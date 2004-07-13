86e9e11ae290e64da700b48df375f1f504cf571b
Kalpit Desai <kvdesai@cs.unc.edu>
2004-07-13 05:06:10 +0000

    2004-07-13  Kalpit Desai <kvdesai@cs.unc.edu> 	* simlissajous.m: Fixed the pole location calculation for 6 pole geometry in  stage coordinate system.

--- magnets/simlissajous/simlissajous.m
+++ magnets/simlissajous/simlissajous.m
@@ -1,4 +1,4 @@
-function data = simlissajous(mags)
+function force = simlissajous(mags)
 %Simulates velocities visualization using Leandra's equations
 %Needs coil currents as input
 TETRA = 0; %Switch to select which geometry to simulate
@@ -9,16 +9,19 @@ if (TETRA)
     pole_locations = [-300, 0 ,300; 300, 0 ,300; 0 , -300, -300; 0,300,-300];
 elseif (HEXA)
     excitation = mags.analogs; %for 6 poles coils current are same as pole currents
-     R = 60; %um
-    H = R*2*cos(pi/6); %um
-    P(1,1:3) = [0, R, H/2];
-    P(2,:) = [-0.866*R, R/2, -H/2];
-    P(3,:) = [-0.866*R, -R/2, H/2];
-    P(4,:) = [0, -R, -H/2];
-    P(5,:) = [0.866*R, -R/2, H/2];
-    P(6,:) = [0.866*R, R/2, -H/2];
+    R = 60; % 0.5*separation between two diagonally opposite poles
+%     P = R.*[-1 0 0; 0 -1 0; 0 0 -1;1 0 0;0 1 0;0 0 1];
+%     pole_locations = rotate_to_vector(P,pi/4,atan(1/sqrt(2)));
+%     
+    H = 2*R/sqrt(3); %separation between two planes
+    r = R*sqrt(2/3); %radius of inner circle described by poles in one plane
+    P(1,1:3) = [0, r, H/2];
+    P(2,:) = [-0.866*r, r/2, -H/2];
+    P(3,:) = [-0.866*r, -r/2, H/2];
+    P(4,:) = [0, -r, -H/2];
+    P(5,:) = [0.866*r, -r/2, H/2];
+    P(6,:) = [0.866*r, r/2, -H/2];
     pole_locations = P;
-%     pole_locations = R.*[-1,0,0;1,0,0;0,-1,0;0,1,0;0,0,-1;0,0,1];
 else
     error('Switch for pole-geometry selection is not defined properly');
 end
@@ -27,6 +30,7 @@ fm = fmatrix(pole_locations);
 disp('Doing force calculations...');
 force = Bforce(fm,excitation);
 disp('Force calculations done...Plotting visualization now!');
+
 data.velx = force(:,1);
 data.vely = force(:,2);
 data.velz = force(:,3);
