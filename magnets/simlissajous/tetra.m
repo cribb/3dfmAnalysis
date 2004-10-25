ddc146048c4071eefc3b132b552d489ecf6fc334
Nanomanipulator Work <nanowork@cs.unc.edu>
2004-10-25 03:39:44 +0000

    2004-10-24  Jeremy Cribb <jcribb@email.unc.edu> 	* removed Bforce.m, fmatrix.m, and tetra.m.  These files are used to simulate the forces via a monopole approximation.  Moved them to a new folder in 'magnets' called 'simforces'

--- magnets/simlissajous/tetra.m
+++
@@ -1,6 +0,0 @@
-function p = tetra(r)
-% tetra(r) -- Returns pole locations of xy-axis aligned tetrahedral pole
-% geometry with pole distances r from the field point at the origin.
-rt13 = sqrt(1/3);
-rt23 = sqrt(2/3);
-p = r.*[rt23,0,-rt13; -rt23,0,-rt13; 0,rt23,rt13; 0,-rt23,rt13];
