342d9a32b9e41385b7577bd5c88145cc7e662a59
Nanomanipulator Work <nanowork@cs.unc.edu>
2004-10-26 18:23:22 +0000

    2004-10.26  Jeremy Cribb <jcribb@email.unc.edu> 	* windiff.m: fixed a bug that incorrectly assigned grid values.  fixed documentation.

--- math/windiff.m
+++ math/windiff.m
@@ -1,20 +1,23 @@
-function [dy,dx] = windiff(y, x, window_size)
+function [newx, dydx] = windiff(y, x, window_size)
 % 3DFM function  
 % Math 
-% last modified 10/23/04 
+% last modified 10/26/04 
 %  
 % This function computes a 'windowed' numerical derivative dy/dx
-% of window-length window_size.
+% of window-length window_size.  It returns the derivative as dydx, and
+% also returns the grid, newx, on which the derivative was calculated.
 %  
-%  v = windiff(x, window_size)
+%  [newx, dydx] = windiff(x, window_size)
 %   
-%  where "x" is a datastream, dx,  (such as bead position) sampled on an even
-%            grid (such as time), dt.
+%  where "x" is a datastream's independent variable
+%        "y" is a datastream's dependent variable
 %        "window_size" is a number of array elements between the two points used to
 %                      compute the derivative.
 %  
 %  10/11/04 - created; jcribb.  
-%  10/23/04 - added the independent variable, x, so that it is computed properly; jcribb
+%  10/23/04 - added the independent variable, x, so that its grid is computed properly; jcribb
+%  10/26/04 - fixed a bug in calculation; jcribb.
+%
 
 	A = y(1:end-window_size,:);
 	B = y(window_size+1:end,:);
@@ -22,5 +25,8 @@ function [dy,dx] = windiff(y, x, window_size)
 	D = x(window_size+1:end,:);
     
     dy = (B-A);
-    dx = mean([C D], 2);
+    dx = (D-C);
+    
+    dydx = dy./dx;
+    newx = mean([C D], 2);
 
