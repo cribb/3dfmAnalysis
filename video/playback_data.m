0e33f76d8ff8451681e0db8d70667afa7459f1fa
Nanomanipulator Work <nanowork@cs.unc.edu>
2004-11-29 17:29:06 +0000

    2004-11-29  Jeremy Cribb <jcribb@email.unc.edu> 	* Added playback_data.m.  Creates .avi video of dataset.  Useful for making animations that describe a bead path (beating cilia, for example). 	* Added raw2avi, which replaces make_pulnix_movie. 	* Removed make_pulnix_movie.

---
+++ video/playback_data.m
@@ -0,0 +1,94 @@
+function playback_data(coords, fps, movfile)
+% 3DFM function  
+% Video 
+% last modified 11/17/2004 
+%  
+% This function will create an AVI file that plays back a 2D or 3D
+% dataset at a desired framerate.  There is no filtering or resampling
+% of the input; it is assumed that such operations are already done.
+%  
+%  playback_data(coords, fps, movfile);  
+%   
+%  where "coords" contains the data for plotting (2D or 3D).
+%        "fps"  is frames per second for the output AVI.
+%        "movfile" is the filename for the output AVI.
+%   
+%  11/17/04 - created; jcribb.
+%  
+%
+
+% coords should be an n x 2 or n x 3 matrix, oriented as [x y z]
+% all resampling, etc, is assumed to have been done before this
+% function is called
+[rows cols] = size(coords);
+if cols > rows
+    coords = coords';
+    [rows cols] = size(coords);
+end
+
+switch cols
+    case 2       
+		x = coords(:,1);
+		y = coords(:,2); 
+    case 3
+		x = coords(:,1);
+		y = coords(:,2);         
+		z = coords(:,3);
+    otherwise
+        error(['playback_data: Method for plotting ' num2str(cols) '-D data is unknown.']);
+    end
+
+[timefig,timetext] = init_timerfig;
+    
+fig = figure;
+set(fig,'DoubleBuffer','on');
+set(fig,'Renderer','zbuffer',...
+        'Resize',  'on');
+    
+mov = avifile(movfile,'compression','none','fps',fps);
+
+for k = 1:rows;
+	tic;
+        
+        switch cols
+            case 2
+                plot(x(1:k), y(1:k), 'r');
+                xlabel('X');
+                ylabel('Y'); 
+				axis([min(x) max(x) min(y) max(y)]);    
+                
+            case 3
+                plot3(x(1:k), y(1:k), z(1:k), 'r');
+                xlabel('X');
+                ylabel('Y'); 
+                zlabel('Z');    
+				axis([min(x) max(x) min(y) max(y) min(z) max(z)]);                    
+        end
+        
+     	axis square;
+		grid on;
+		pretty_plot;
+        drawnow;
+          
+    F = getframe(fig);
+    mov = addframe(mov,F);
+    
+    % reporting time left
+    itertime = toc;
+    if k == 1
+        totaltime = itertime;
+    else
+        totaltime = totaltime + itertime;
+    end    
+    meantime = totaltime / k;
+    timeleft = (rows-k) * meantime;
+    outs = [num2str(timeleft, '%5.0f') ' sec.'];
+    set(timetext, 'String', outs);
+end
+
+
+mov = close(mov);
+
+close(fig);
+close(timefig);
+
---
+++ video/raw2avi.m
@@ -0,0 +1,97 @@
+function v = raw2avi(rawfilein, stride, frame_rate)
+% 3DFM function  
+% Video 
+% last modified 11/19/2004 
+%  
+% This function converts RAW files from the Pulnix camera
+% (via take, GLUItake, etc...) into an AVI file.
+%  
+%  v = raw2avi(rawfilein, movieout, stride, frame_rate);  
+%   
+%  where "rawfilein" is the filename of the input RAW file (wildcards ok)
+%        "stride" takes every <stride> frame (reduces bandwidth, removes timelapse)
+%        "frame_rate" is the frame_rate of the output AVI.
+%   
+%  ??/??/?? - created.  jcribb.
+%  09/07/04 - added documentation and removed a bug that only
+%             allowed one to capture 60 frames.  jcribb.
+%  11/19/04 - added stride and changed name to raw2avi (from 
+%             make_pulnix_movie).  added "time remaining indicator
+%             updated documentation.  removed movieout. output filename
+%             is the input filename with .avi extension; jcribb.
+%  
+
+
+% get input file information
+file = dir(rawfilein);
+
+% frame properties
+rows = 484;
+cols = 648;
+color_depth = 1; % bytes
+frame_size = rows * cols * color_depth;
+
+for f = 1 : length(file)
+    
+    number_of_frames = (file(f).bytes) / frame_size;
+    
+    % setup the input file
+	fid = fopen(file(f).name);
+    
+    % setup the output file name
+    movieout = file(f).name;
+    movieout = [movieout(1:end-3) 'avi'];
+	
+	% setup the output file
+	mov = avifile(movieout,'quality',60,'fps',frame_rate,'keyframe',2.0,'compression','MPG4');
+        % avifile('example.avi','quality',60,'fps',5,'keyframe',2.0,'compression','DIVX')
+
+	% initialize figure and set initial properties
+	fig = figure;
+	set(fig, 'visible','off' ...
+           , 'Position', [1 1 cols rows] ...
+           , 'DoubleBuffer','on' ...
+           , 'Renderer','zbuffer' ...
+           , 'Resize',  'on' ...
+           , 'Colormap', gray(256));
+	
+	% set axes properties
+    ax = gca;
+	set(ax, 'Units', 'Pixels', ...
+            'Position', [1 1 cols rows], ...
+            'Visible', 'off');
+	axis square;
+
+	% set up text-box for 'remaining time' display
+	[timefig,timetext] = init_timerfig;
+	
+	for k=1:stride:number_of_frames
+		tic;  % start timer
+		
+		im = fread(fid, [648,484],'uint8');   % read in the next frame
+		image(im','parent',ax);               % puts image in invisible axes  
+		axis off;
+        drawnow;
+		mov = addframe(mov,ax);               % adds frames to the AVI file 
+		status = fseek(fid, frame_size*k, 'bof');  % advance to next frame
+		
+		% handle timer
+		itertime = toc;
+		if k == 1
+            totaltime = itertime;
+		else
+            totaltime = totaltime + itertime;
+		end    
+		meantime = totaltime / (k / stride);
+		timeleft = (number_of_frames-k)/stride * meantime;
+		outs = [num2str(timeleft, '%5.0f') ' sec.'];
+		set(timetext, 'String', outs);
+	end 
+	
+	mov=close(mov);     % closes the AVI file  
+	close(fig);         % closes the handle to invisible figure
+	close(timefig);
+    
+end    
+    
+v = 0;
