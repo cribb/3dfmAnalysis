b0a9ce6ca2e64e6dc7d851ef1e6e513e93ae4482
Nanomanipulator Work <nanowork@cs.unc.edu>
2004-12-07 03:10:51 +0000

    2004-12-06  Jeremy Cribb <jcribb@email.unc.edu> 	* trigplane.m: prettified code.

--- magnets/simforces/trigplane.m
+++ magnets/simforces/trigplane.m
@@ -1,6 +1,8 @@
 function p = trigplane(r);
-% trigplane(r) -- Returns pole locations of trigonal-plane aligned pole
+% hexaplane(r) -- Returns pole locations of hexagonal-plane aligned pole
 % pieces (jcribb)
 
-p = r .* [0, 1, 0 ; -sin(pi/6), -cos(pi/6), 0 ; sin(pi/6), -cos(pi/6), 0];
+p = r .* [         0,           1,             0 ; ...
+          -sin(pi/6),  -cos(pi/6),             0 ; ...
+           sin(pi/6),  -cos(pi/6),             0 ];
 
