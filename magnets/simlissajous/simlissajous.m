d53be0ad81fbb6fd713be40cb5209503b74780d3
Kalpit Desai <kvdesai@cs.unc.edu>
2005-01-11 07:04:54 +0000

    2004-01-11  Kalpit Desai <kvdesai@email.unc.edu> 	* simlissajous.m: Previous commit was accidentally from wrong directory, This commit has correct coordinate frame.

--- magnets/simlissajous/simlissajous.m
+++ magnets/simlissajous/simlissajous.m
@@ -10,17 +10,18 @@ if (TETRA)
 elseif (HEXA)
     excitation = mags.analogs; %for 6 poles coils current are same as pole currents
     R = 60; % 0.5*separation between two diagonally opposite poles
-    P = R.*[-1 0 0; 0 -1 0; 0 0 -1;1 0 0;0 1 0;0 0 1];
+%     P = R.*[-1 0 0; 0 -1 0; 0 0 -1;1 0 0;0 1 0;0 0 1];
 % %     pole_locations = rotate_to_vector(P,pi/4,atan(1/sqrt(2)));
 % %     
-%     H = 2*R/sqrt(3); %separation between two planes
-%     r = R*sqrt(2/3); %radius of inner circle described by poles in one plane
-%     P(1,1:3) = [0, r, H/2];
-%     P(2,:) = [-0.866*r, r/2, -H/2];
-%     P(3,:) = [-0.866*r, -r/2, H/2];
-%     P(4,:) = [0, -r, -H/2];
-%     P(5,:) = [0.866*r, -r/2, H/2];
-%     P(6,:) = [0.866*r, r/2, -H/2];
+    H = 2*R/sqrt(3); %separation between two planes
+    r = R*sqrt(2/3); %radius of inner circle described by poles in one plane
+    %this gives r = H/sqrt(2)
+    P(1,1:3) = [0, r, H/2];
+    P(2,:) = [-0.866*r, r/2, -H/2];
+    P(3,:) = [-0.866*r, -r/2, H/2];
+    P(4,:) = [0, -r, -H/2];
+    P(5,:) = [0.866*r, -r/2, H/2];
+    P(6,:) = [0.866*r, r/2, -H/2];
     pole_locations = P;
 else
     error('Switch for pole-geometry selection is not defined properly');
