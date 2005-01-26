7ee2d035b6a51e36ad72404eb7ddad000cedf84b
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-01-26 21:17:45 +0000

    2005-01-26  Jeremy Crivv <jcribb@email.unc.edu> 	* get_mips.m: added.  runs and saves single or batches of max(min)imum intensity projection images.  This function calls mip.m and is for batch processing only.

---
+++ video/get_mips.m
@@ -0,0 +1,27 @@
+function get_mips(rawfiles)
+% 3DFM function  
+% Image Analysis 
+% last modified 01/26/05
+%  
+% This function computes and saves a Maximum or Minimum Intensity 
+% Projection image for any RAW file matching <rawfiles>.
+%  
+%  [im] = get_mips(rawfiles);  
+%   
+%  where "rawfiles" is the name of the rawfile (wildcards accepted) 
+%  
+%  Notes:  
+%   
+%  01/26/05 - created; jcribb.
+%   
+%  
+
+rawfiles = dir(rawfiles);
+
+for k = 1 : length(rawfiles)
+    imMIP = mip(rawfiles(k).name, 1, [], 8);
+    
+    new_filename = [rawfiles(k).name(1:end-3) 'MIP.bmp'];
+    
+    imwrite(imMIP, gray, new_filename, 'BMP');    
+end
