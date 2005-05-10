b80c717d25a3c08ddd4979b9949cf6bcb54945bf
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-05-10 04:28:26 +0000

    2005-05-09  Jeremy Cribb <jcribb@email.unc.edu> 	* rows.m:  added.  returns the number or rows in a matrix.

---
+++ utils/rows.m
@@ -0,0 +1,17 @@
+function v = rows(X);
+% 3DFM function  
+% Utilities 
+% last modified 05/09/2005
+%  
+% This function returns the number of rows in a matrix.
+%  
+%  [v] = rows(X);
+%   
+%  where "X" is a matrix.
+%  
+%  05/09/2005 - created; jcribb
+
+
+    [r c p] = size(X);
+
+    v = c;
