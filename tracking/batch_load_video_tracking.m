f177bde21bdab099875ecbcba25d715f4dba44a1
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-06-07 16:16:24 +0000

    2005-06-07  David Hill <dbhill@email.unc.edu>
    * Wrapper program that converts a *.vrpn.mat file into just position and time infromation of the beads in the tracking file. Converts all *.vrpn.mat files in a folder at once

---
+++ tracking/batch_load_video_tracking.m
@@ -0,0 +1,100 @@
+function d = batch_load_video_tracking2(text, conversion, fr, rel)
+% 3DFM function  
+% Rheology 
+% created 12-7-2004 
+%  
+% batch_load_video_tracking
+% by DB Hill
+% 
+% d = batch_load_video_tracking(text, conversion, frame rate, rel);
+%
+%   text == suffix of filenames to load from pix's to UM's;
+%
+%   conversion == 1 or 1.5, depending on how the microscope is set:
+%       note: this is now set to run with the pulnix, 60x and only the
+%       pulnix 60x
+%
+%   rel == a string that sets the postion coordnates to be either relative
+%       (starting from zero) or absolut
+%  
+%  [outputs] = writes file with position vs. time data for multiple beads
+%  tracked in 2-D using video spot tracker. Outputs data in the form:
+%  time x_1 x_2 x_3 .... y_1 y_2 y_3 .... 
+%   
+%  Notes:  
+%   
+%  This function calls all *.mat files from within a given subfolder of
+%  beads tracked in fluorescences with the 1.0x optivar in place on Hercules
+%  or artimus using the Pulnix camera
+%  
+%
+% ***********************************************************************
+
+% create and array with the filenames of a given suffix. For this program,
+% they must include *.mat
+fu = dir(text);
+
+% count the number of files to be loaded
+num_files = size(fu);
+
+% calculate the conversion factor for pixels -- microns
+if conversion == 1.5,
+    con = 0.104;
+elseif conversion == 4,
+    con = 0.039;
+elseif conversion == 6,
+    con = 0.026;
+else con = 0.154;
+end
+
+% determine if we are interested in the absolute or relative motion of
+% beads ( relative == ALL beads start at 0,0
+if rel == 'r'
+   rel = 'relative';
+else rel = 'absolute';
+end
+
+% file loading loop
+for z = 1:num_files,
+    
+    % debugging outputs
+    z
+    filename = fu(z).name
+
+    % call load video tracking to load the data
+    d = load_video_tracking(filename, fr,'um',con,rel,'yes','table');
+
+    % count the number of beads in a given tracking file
+    n_beads = max(d(:,2))+1;
+    
+    % create a data array to fill in with t and position infromation
+    data = zeros(max(d(:,3)+1),2*n_beads+1);
+   
+    % place position and time info into data matrix
+    for j = 0:max(d(:,2)),
+        
+        g = get_bead(d,j);    
+        
+        if j == 0,
+            
+            data(:,1) = g(:,1);
+            data(:,2) = g(:,4);
+            data(:,n_beads+2) = g(:,5);
+            
+        else
+            
+            data(:,j+2) = g(:,4);
+            data(:,j+2+n_beads) = g(:,5);
+            
+        end
+        
+    end
+        
+    % save output
+    filename_save_2 = strcat(filename,'_position.dat');
+    save(filename_save_2, 'data', '-ASCII');
+
+end
+
+% pass data info back
+d = data;
