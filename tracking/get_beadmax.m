f17877820febe3a1e6fcd4b6a72ce738d7261035
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-06-27 14:43:51 +0000

    2005-06-27  Jeremy Cribb  <jcribb@email.unc.edu> 	*  get_beadmax: added.  returns the maximum bead/tracker ID in a tracked-video dataset.

---
+++ tracking/get_beadmax.m
@@ -0,0 +1,24 @@
+function v = get_beadmax(data)
+% 3DFM function  
+% Tracking 
+% last modified 06/27/05
+%  
+% Extracts the maximum tracker ID in a video tracking dataset.
+%  
+%  [v] = get_beadmax(data);  
+%   
+%  where "data" is the output matrix from load_video_tracking
+%   
+%  06/27/05 - created; jcribb.
+%
+
+video_tracking_constants;
+
+% determine whether the input data is in the table or structure of vectors
+% format...
+if isfield(data, 'id')
+    v = max(data.id);
+else
+    v = max(data, ID);
+end
+
