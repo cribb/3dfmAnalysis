0a356c0bd227b782d035f461691d25d5b76c28b6
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-07-19 18:02:32 +0000

    2005-07-19  Kalpit Desai <kvdesai@email.unc.edu> 	* simlissajous.m: added option (hard coded) for simulations with three pole co-planar geometry.

--- magnets/simlissajous/simlissajous.m
+++ magnets/simlissajous/simlissajous.m
@@ -3,6 +3,7 @@ function force = simlissajous(mags)
 %Needs coil currents as input
 TETRA = 0; %Switch to select which geometry to simulate
 HEXA = 1;
+THREEPOLE = 0;
 if (TETRA)
     excitation = coils2poles(mags.analogs); %need only for tetra poles
     %pole_locations = tetra(1);
@@ -22,6 +23,20 @@ elseif (HEXA)
     P(4,:) = [0, -r, -H/2];
     P(5,:) = [0.866*r, -r/2, H/2];
     P(6,:) = [0.866*r, r/2, -H/2];
+    pole_locations = P;
+elseif THREEPOLE;
+    excitation = mags.analogs; %for 6 poles coils current are same as pole currents
+    R = 60; % 0.5*separation between two diagonally opposite poles
+%     P = R.*[-1 0 0; 0 -1 0; 0 0 -1;1 0 0;0 1 0;0 0 1];
+% %     pole_locations = rotate_to_vector(P,pi/4,atan(1/sqrt(2)));
+% %     
+    H = 2*R/sqrt(3); %separation between two planes
+    r = R*sqrt(2/3); %radius of inner circle described by poles in one plane
+    %this gives r = H/sqrt(2)
+    P(1,1:3) = [0, r, H/2];
+    P(2,:) = [-0.866*r, -r/2, H/2];
+    P(3,:) = [0.866*r, -r/2, H/2];
+   
     pole_locations = P;
 else
     error('Switch for pole-geometry selection is not defined properly');
