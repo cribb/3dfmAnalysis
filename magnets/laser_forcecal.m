6ab96ce2273e78e9a7ec242ac4c456a07657dd4d
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-08-11 00:32:32 +0000

    2005-08-10  Jeremy Cribb <jcribb@email.unc.edu> 	* laser_forcecal.m: added drift subtraction.  added documentation.

--- magnets/laser_forcecal.m
+++ magnets/laser_forcecal.m
@@ -1,4 +1,18 @@
 function v = laser_forcecal(tracking_log_file, magnet_log_file, dominant_pole, bead_radius, viscosity)
+% 3DFM function  
+% Magnetics
+% last modified 08/10/05 
+%  
+% Laser force calibration routine for the 3dfm.
+%  
+%  [v] = laser_forcecal(tracking_log_file, magnet_log_file, dominant_pole, bead_radius, viscosity) 
+%   
+%  where "tracking_log_file" is the 3dfm tracking log filename
+%        "magnet_log_file" is the 3dfm magnet log filename
+%		 "dominant_pole" is the identity of the dominant pole in the 3dfm
+%        "bead_radius" is the radius of hte tracked bead in [m]
+%        "viscosity" is the viscosity of the calibrator fluid in [Pa s]
+%  
 
 % handle argument list
 if (nargin < 5) | isempty(viscosity);      viscosity = 1.6;       end;
@@ -23,8 +37,11 @@ magtime = magtime(:,1) + magtime(:,2) * 1e-6;
 mintime = min([beadtime ; magtime]);
 maxtime = max([beadtime ; magtime]);
 
-% compute radial vector for bead position
-r = magnitude(d.beadpos.x, d.beadpos.y, d.beadpos.z);
+% get vector positions and compute radial vector position
+x = d.beadpos.x;
+y = d.beadpos.y; 
+z = d.beadpos.z;
+r = magnitude(x, y, z);
 
 %     % code for separate subplots 
 %     figure;
@@ -44,15 +61,16 @@ r = magnitude(d.beadpos.x, d.beadpos.y, d.beadpos.z);
 	set(get(ax(2),'Ylabel'),'String','dominant coil [V]');
     pretty_plot;
     
-warning off MATLAB:polyfit:RepeatedPointsOrRescale
+warning off MATLAB:polyfit:RepeatedPointsOrRescale;
 
 count = 1;
+drift = [0 0 0];
 
 % Start processing the magnet log entries and create force table.
 for k = 1 : ( length(magtime) - 1 );
 
     % for how long do we need to ignore points due to disynchrony?
-    buffer = 0.1; % seconds
+    buffer = 0; % seconds
     
     idx = find(beadtime >= (magtime(k) + buffer) & beadtime < (magtime(k+1) - buffer));
     
@@ -64,29 +82,53 @@ for k = 1 : ( length(magtime) - 1 );
     
     if (N > thresh)
         
-        fit = polyfit(beadtime(idx), r(idx), 1);
-        bead_velocity = fit(1);
+        dcoil_voltage = m.analogs(k, dominant_pole);
         
-        force = 6 * pi * bead_radius * viscosity * bead_velocity;
-        uncertainty_in_force = 6*pi*bead_radius * viscosity * uncertainty_in_slope(beadtime(idx), r(idx), fit);
+        % approximate velocity on each axis by using slope of linear fit.
+        fitx = polyfit(beadtime(idx), x(idx), 1);
+        fity = polyfit(beadtime(idx), y(idx), 1);
+        fitz = polyfit(beadtime(idx), z(idx), 1);
+        fitr = polyfit(beadtime(idx), r(idx), 1);
+        
+        vel = [ fitx(1) fity(1) fitz(1) ];
+        unc = [ uncertainty_in_slope(beadtime(idx), x(idx), fitx), ... 
+                uncertainty_in_slope(beadtime(idx), y(idx), fity), ...
+                uncertainty_in_slope(beadtime(idx), z(idx), fitz) ] ;
+        
+        if dcoil_voltage == 0
+%             drift = vel;
+        else    
+%             vel = vel - drift;
+        end
+                    
+        bead_velocity = fitr(1);
+        
+        force = 6 * pi * bead_radius * viscosity * vel;
+        uncertainty_in_force = 6 * pi * bead_radius * viscosity * unc;
         
         force_table(count, 1) = min(beadtime(idx)) - mintime;
         force_table(count, 2) = max(beadtime(idx)) - mintime;
         force_table(count, 3) = N;
-        force_table(count, 4) = m.analogs(k, dominant_pole);   % reporting voltage
-        force_table(count, 5) = force;
-        force_table(count, 6) = uncertainty_in_slope(beadtime(idx), r(idx), fit) / bead_velocity;
+        force_table(count, 4) = dcoil_voltage;   % reporting voltage
+        force_table(count, 5) = magnitude(force);
+        force_table(count, 6) = magnitude(uncertainty_in_force);
 
         count = count + 1;
 
         % code for separate subplots
         % plot(beadtime(idx) - mintime, polyval(fit, beadtime(idx)) * 1e6, 'r-');
         
+        % code for single plot with secondary axis
         hold on; 
-            plot(beadtime(idx) - mintime, polyval(fit, beadtime(idx)) * 1e6, 'r-');
+            plot(beadtime(idx) - mintime, polyval(fitr, beadtime(idx)) * 1e6, 'r-');
         hold off;    
-    end
+    end   
+        
 end
+
+    if ~exist('force_table')
+        error('There is no congruence in time for the magnet and tracking files');
+    end
 
 v = force_table;
 
