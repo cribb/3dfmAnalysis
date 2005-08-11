5b3dbf9b8fc09d02fb8ace8f3b499edd662935af
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-08-11 00:11:10 +0000

    2005-08-10  Jeremy Cribb <jcribb@email.unc.edu> 	* percent_recovery.m: updated documentation and fixed an error in the computation.

--- specific/rheology/percent_recovery.m
+++ specific/rheology/percent_recovery.m
@@ -1,14 +1,14 @@
 function v = percent_recovery(x)
 % 3DFM function  
 % Rheology 
-% last modified 07/19/05 (jcribb)
+% last modified 08/10/05 (jcribb)
 %  
 % This function computes the percent recovery of the vector, x.
 %  
 %  [v] = percent_recovery(x);  
 %   
-%  10/30/02 - created (jcribb)  
+%  07/16/05 - created (jcribb) 
 % 
 
-    v = min(x) / max(x) * 100;
+    v = (max(x) - min(x)) / max(x) * 100;
     
