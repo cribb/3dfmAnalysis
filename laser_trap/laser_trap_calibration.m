7ee513f534f8ed9f6f10b8d5f943b0b945abb115
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-08-12 17:08:20 +0000

    2005-08-11  Jeremy Cribb <jcribb@email.unc.edu> 	* LorentzianFit.m: removed calibration code. 	* laser_trap_calibration.m: removed code specific for fitting to Lorentzian.

--- laser_trap/laser_trap_calibration.m
+++ laser_trap/laser_trap_calibration.m
@@ -1,100 +1,25 @@
-function v = laser_trap_analysis(filename);
+function v = laser_trap_calibration(file, eta, a)
+
+if nargin < 3 | isempty(a)
+	a = .5e-6;          % bead radius in m
+end
+
+if nargin < 2 | isempty(eta)
+	eta = .026;         % viscosity in Pa s
+end
+
+% physical constants
+T = 298;
+K = 1.3807e-23;
+gamma = 6*pi*eta*a;
+
+% load dataset
+d = load_lasertrap_tracking(file, [0 0 0]);
 
 
-d = load_vrpn_tracking(filename,'u','zero','yes');  %load in data set
+kx = LorentzianFit(d.psd.f, d.psd.x, eta, a);
+ky = LorentzianFit(d.psd.f, d.psd.y, eta, a);
+kz = LorentzianFit(d.psd.f, d.psd.z, eta, a);
 
-%Set spring constants
-k_x = 40 %244  %pN/um
-k_y = 40 %244 %pn/um
-k_z = 10 %90 %pN/um
+v = [kx ky kz];
 
-%convert to center of trap coords. 
-v.trapcentered.x = d.posError.x - d.posError.x(1);
-v.trapcentered.y = d.posError.y - d.posError.y(1);
-v.trapcentered.z = d.posError.z - d.posError.z(1);
-
-%Convert QPD data to forces
-v.forces.x = k_x * j.trapcentered.x;
-v.forces.y = k_y * j.trapcentered.y;
-v.forces.z = k_z * j.trapcentered.z;
-
-v.position.x = d.beadpos.x;
-v.position.y = d.beadpos.y;
-v.position.z = d.beadpos.z;
-v.time = d.beadpos.time;
-
-% % [px,fx] = mypsd(d.posError.x, 10000, 1, 'rectangle');
-% % [py,fy] = mypsd(d.posError.y, 10000, 1, 'rectangle');
-% % [pz,fz] = mypsd(d.posError.z, 10000, 1, 'rectangle');
-% % 
-% % figure(1);subplot(2,1,1),plot(d.beadpos.time,j.position.x)
-% % title('X Position (microns) vs. Time (seconds)')
-% % xlabel('seconds')
-% % ylabel('microns')
-% % subplot(2,1,2),plot(d.beadpos.time,j.forces.x)
-% % title('X Position vs. Force')
-% % xlabel('time')
-% % ylabel('pN')
-% % 
-% % figure(2);subplot(2,1,1),plot(d.beadpos.time,j.position.y)
-% % title('Y Position (microns) vs. Time (seconds)')
-% % xlabel('seconds')
-% % ylabel('microns')
-% % subplot(2,1,2),plot(d.beadpos.time,j.forces.y)
-% % title('Y Position vs. Force')
-% % xlabel('time')
-% % ylabel('pN')
-% % 
-% % figure(3);subplot(2,1,1),plot(d.beadpos.time,j.position.z)
-% % title('Z Position (microns) vs. Time (seconds)')
-% % xlabel('seconds')
-% % ylabel('microns')
-% % subplot(2,1,2),plot(d.beadpos.time,j.forces.z)
-% % title('Z Position vs. Force')
-% % xlabel('time')
-% % ylabel('pN')
-% % 
-% % figure(4);plot(abs(j.position.x),j.forces.x)
-% % title('X Force vs. Extension')
-% % xlabel('Position (microns)')
-% % ylabel('Force (pN)')
-% % 
-% % figure(5);plot(abs(j.position.y),j.forces.y)
-% % title('Y Force vs. Extension')
-% % xlabel('Position (microns)')
-% % ylabel('Force (pN)')
-% % 
-% % figure(6);plot(abs(j.position.z),j.forces.z)
-% % title('Z Force vs. Extension')
-% % xlabel('Position (microns)')
-% % ylabel('Force (pN)')
-% % 
-% % % figure(4);loglog(fx,px)
-% % % title('X PSD')
-% % % xlabel('log frequency (Hz)')
-% % % ylabel('Power (um^2/Hz')
-% % % 
-% % % figure(5);loglog(fy,py)
-% % % title('Y PSD')
-% % % xlabel('log frequency (Hz)')
-% % % ylabel('Power (um^2/Hz')
-% % % 
-% % % figure(6);loglog(fz,pz)
-% % % title('Z PSD')
-% % % xlabel('log frequency (Hz)')
-% % % ylabel('Power (um^2/Hz')
-% % 
-% % figure;subplot(2,1,1),loglog(fx,px)
-% % title('X PSD')
-% % ylabel('Power (um^2/Hz')
-% % subplot(2,1,2),loglog(fy,py)
-% % title('Y PSD')
-% % xlabel('log frequency (Hz)')
-% % ylabel('Power (um^2/Hz)')
-% % 
-% % % subplot(3,1,3),loglog(fz,pz)
-% % % title('Z PSD')
-% % % xlabel('log frequency (Hz)')
-% % % ylabel('Power (um^2/Hz')
-% % 
-% % 
