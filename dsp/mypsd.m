3de4000818abe4712db1ab252aa77cc780103d3a
Kalpit Desai <kvdesai@cs.unc.edu>
2005-09-13 21:21:19 +0000

    2005-09-13  Kalpit Desai <kvdesai@cs.unc.edu> 	* mypsd.m: 	- Made to exit gracefully and report some plain english errors when input parameters are inconsistent. 	- Fixed some comments.

--- dsp/mypsd.m
+++ dsp/mypsd.m
@@ -1,7 +1,7 @@
 function varargout = mypsd(s, rate, res, window_type, style, calculateId)
 % 3DFM function  
 % DSP 
-% last modified 09/08/05 - kvdesai
+% last modified 09/11/05 - kvdesai
 %  
 % This function computes the power spectral density for input, s  
 %  
@@ -19,14 +19,14 @@ function varargout = mypsd(s, rate, res, window_type, style, calculateId)
 %        integrated displacement upto the frequency, defaulted to 'no'
 %  
 %  Notes:  
-%  - Resolution cannot exceed 1/endtime  
+%  - Resolution cannot exceed 1/time-span
 %   
 %   
 %  ??/??/?? - created by gb.  
 %  05/06/04 - added documentation; jcribb/kvdesai
 %  05/07/04 - added integrated displacement option; kvdesai
 %  
-
+varargout = cell(nargout); %initialize outputs with empty cells
 if(nargin < 6 | isempty(calculateId))
     calculateId = 'no';
 end
@@ -37,11 +37,15 @@ if(nargin < 4 | isempty(window_type))
    	window_type = 'blackman';
 end
 if(nargin < 3 | isempty(res))
-  res = 1;
+  res = ceil(size(s,1)/rate);%the finest possible resolution with given data
 end
 
-
 nw = fix(rate/(2*res))*2;
+if (nw > size(s,1))
+    disp('mypsd ERROR: Not enough data for requested freuqncy-resolution.');
+    disp('Time span of data should atleast be 1/resolution');
+    return;   
+end
 win = zeros(nw,1);
 switch window_type
 	case 'blackman'
@@ -53,16 +57,13 @@ switch window_type
     case 'hann'
         win = hann(nw);
     otherwise
-        disp('ERROR: mypsd:: Unrecoginzed window type');
-        varargout = cell(nargout);
+        disp('ERROR: mypsd:: Unrecoginzed window type');        
         return;       
 end
-	
 
 for i=1:size(s,2)
-   % [Pxx,f]=pwelch(X, WINDOW,NOVERLAP,NFFT)
-   % [p(:,i) f] = pwelch(s(:,i), blackman(nw), nw/2, nw, rate);  %original gbcode
-   [p(:,i) f] = pwelch(s(:,i), win, nw/2, nw, rate);          %jac-hack
+   % [Pxx,f]=pwelch(X, WINDOW,NOVERLAP,NFFT)   
+   [p(:,i) f] = pwelch(s(:,i), win, nw/2, nw, rate); 
    if(findstr(calculateId,'y'))
        Id(1,i) = sqrt(p(1,i)*f(1));
        for j = 2:length(p(:,i))
