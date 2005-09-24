e9c221801b1c2e4d0c47c4c11fa94e1082df27ea
Kalpit Desai <kvdesai@cs.unc.edu>
2005-09-24 18:24:05 +0000

    2005-09-24  Kalpit Desai <kvdesai@cs.unc.edu> 	* processpeak.m: Made interactive.

--- dsp/processpeak.m
+++ dsp/processpeak.m
@@ -1,17 +1,20 @@
-function peak = processpeak(pp, pf,f)
+function peak = processpeak(pp, pf,f,interactive)
 % 3DFM function  
 % DSP 
-% Created 01/20/05 - kvdesai. Last modified 01/20/05 - kvdesai
-% Usage: peak = processpeak(pp, pf,fsearch)
+% Created 01/20/05 - kvdesai. 
+% Last modified 09/23/05 - kvdesai
+% Usage: peak = processpeak(pp, pf,fsearch,interactive)
 %   pp = output of psd in units of [power/frequency] or [disp^2/frequency]
 %   pf = frequency vector corresponding to pp
 %   fsearch = search frequency
+%   interactive = 0 or 1, select frquencies using cross-hair window.
+%           if 1, then fsearch ignored.
 % peak.p = power at fsearch
 % peak.ampli = amplitude of sine wave at fsearch
 % peak.fsearch = fsearch
-
+fres = mean(diff(pf));%the step size in frequencies
+if (~interactive)
     NBIN = 3; %Number of bins to be integrated on both sides to calculate total power
-    fres = mean(diff(pf));%the step size in frequencies
     i = find(abs(pf - f) < fres/2); %find the index of pf that has value nearest to search frquency
     peak.p = 0;
     if    isempty(i)
@@ -34,6 +37,22 @@ function peak = processpeak(pp, pf,f)
     end
     peak.ampli = sqrt(peak.p*2);%conversion from power to amplitude of sine wave
     peak.fsearch = f;
-
+else %interactively choose frequencies
+    [x y] = ginput(2);
+    dbstop if error
+    fmin = x(1,1); fmax = x(2,1);
+    
+    imin = max(find(pf <= fmin));
+    imax = min(find(pf >= fmax));
+    peak.p = 0;
+    for k = imin:imax
+        peak.p = peak.p + pp(k)*(pf(k) - pf(k-1));        
+    end   
+    peak.f = pf(imin:imax);
+    peak.fsearch = mean(peak.f);
+    peak.ampli = sqrt(peak.p*2);%conversion from power to amplitude of sine wave
+end
+    
+    
 
       
