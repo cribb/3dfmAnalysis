716f48c62e1eb7114954f638feeec14ed6c36239
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-10-27 18:40:07 +0000

    2005-10-27  Jeremy Cribb <jcribb@email.unc.edu> 	* get_mips.m: added stride parameter for convenience.

--- video/get_mips.m
+++ video/get_mips.m
@@ -1,27 +1,34 @@
-function get_mips(rawfiles)
+function get_mips(rawfiles, stride)
 % 3DFM function  
 % Image Analysis 
 % last modified 01/26/05
 %  
 % This function computes and saves a Maximum or Minimum Intensity 
-% Projection image for any RAW file matching <rawfiles>.
+% Projection image for any RAW file matching "rawfiles".
 %  
-%  [im] = get_mips(rawfiles);  
+%  [im] = get_mips(rawfiles, stride);  
 %   
 %  where "rawfiles" is the name of the rawfile (wildcards accepted) 
-%  
-%  Notes:  
+%        "stride" is the number of frames to jump between "start" and "stop"
 %   
 %  01/26/05 - created; jcribb.
 %   
 %  
 
+if nargin < 2 | isempty(stride);
+    stride = 8;
+end
+
+if nargin < 1 | isempty(rawfiles);
+    rawfiles = '*.raw';
+end
+
 rawfiles = dir(rawfiles);
 
 for k = 1 : length(rawfiles)
-    imMIP = mip(rawfiles(k).name, 1, [], 8, 'min');
+    imMIP = mip(rawfiles(k).name, 1, [], stride, 'min');
     
     new_filename = [rawfiles(k).name(1:end-3) 'MIP.bmp'];
     
-    imwrite(imMIP, gray, new_filename, 'BMP');    
+    imwrite(imMIP, colormap(gray(256)), new_filename, 'BMP');    
 end
