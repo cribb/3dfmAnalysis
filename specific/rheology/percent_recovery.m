6a1911bda0e42c139df992b8b8c0cc1166d8be6d
Nanomanipulator Work <nanowork@cs.unc.edu>
2005-11-20 19:04:55 +0000

    2005-11-20  Jeremy Cribb <jcribb@email.unc.edu> 	* percent_recovery.m: added output variables that report the maximum distance traveled, the distance recovered, as well as the percent recovery.

--- specific/rheology/percent_recovery.m
+++ specific/rheology/percent_recovery.m
@@ -1,15 +1,28 @@
-function v = percent_recovery(x)
+function varargout = percent_recovery(x)
 % 3DFM function  
 % Rheology 
 % last modified 08/10/05 (jcribb)
 %  
 % This function computes the percent recovery of the vector, x.
 %  
-%  [v] = percent_recovery(x);  
+%  [pct_rec, xmax, xrec] = percent_recovery(x);
 %   
 %  07/16/05 - created (jcribb) 
 % 
 
-    v = (max(x) - x(end)) / (max(x) - x(1)) * 100;
+	xrec = max(x) - x(end);    
+    xmax = max(x) - x(1);
+    pct_rec = (xrec) / (xmax) * 100;
     
+    switch nargout
+        case 1
+            varargout{1} = pct_rec;
+        case 2
+            varargout{1} = pct_rec;
+            varargout{2} = xmax;
+        case 3
+            varargout{1} = pct_rec;
+            varargout{2} = xmax;
+            varargout{3} = xrec;
+    end    
     
