68e6518df0821a9521868296967d084990af2ba8
Kalpit Desai <kvdesai@cs.unc.edu>
2005-12-23 05:06:21 +0000

    2005-12-22  Kalpit Desai <kvdesai@cs.unc.edu> 	* mypsd.m: 	- Calculation of 'integrated displacement' is much efficient now. 	- Last argument, flag for calculating id, is now obsolete and ignored. 	- Improved documentation

--- dsp/mypsd.m
+++ dsp/mypsd.m
@@ -1,35 +1,46 @@
-function varargout = mypsd(s, rate, res, window_type, style, calculateId)
+function varargout = mypsd(s, rate, res, window_type, style, obsoletearg)
 % 3DFM function  
 % DSP 
-% last modified 09/11/05 - kvdesai
+% last modified 12/22/05 - kvdesai
 %  
-% This function computes the power spectral density for input, s  
-%  
-%  [p,f] = mypsd(signal, rate, res, window_type, style, calculateId);
-%  OR
-%  [p,f,Id] = mypsd(signal, rate, res, window_type, style, calculateId);
+% This function computes the power spectral density for input.
+% Function signature:
+%  [p, f, id] = mypsd(signal, rate,res, window_type, style, obsoletearg);
 %   
-%  where "signal" is a matrix (columns denote different datasets)
-%        "rate" is the sampling rate in units of [Hz] 
+%  where "signal" is a matrix (each column is a different signal)
+%        "rate" is the sampling rate in units of [Hz] [required argument] 
 %		 "res"  is the desired PSD resolution in [Hz]
+%               default value is the maximum that could be achieved
+%               for given length of data.
 %        "window_type" couldbe any of
 %               'blackman' 'rectangle' 'hamming' 'hann', defaulting to 'blackman'
 %        "style" is matlab graph-style string
-%        "calculateId" is 'yes' or 'no': a switch for calculating
-%        integrated displacement upto the frequency, defaulted to 'no'
-%  
-%  Notes:  
+%        "obsoletearg" is what used to be the flag for 'calculateId'. 
+%               This argument is now obsolete and ignored. Id is calculated when
+%               number of output arguments are more than 2. (see Usage below) 
+%               The argument is kept as a  placeholder so that old dependent 
+%               code supplying 6 args doesn't break.  
+%        "p" is the computed power-density [power/hz] at frequencies in "f"
+%        "id" is the sqrt of area under the curve upto frequency "f"
+%               'id' stands for 'integrated displacement'
+%  Various Usages:
+%           mypsd(signal, rate, ...);
+%                Calculates psd (p and f) and plots it on loglog axes.
+%       p = mypsd(signal, rate, ...);
+%                Calculates psd and returns power-density (p).
+%   [p,f] = mypsd(signal, rate, ...);
+%                Calculates psd and returns p as well as frequencies (f).
+%[p,f,id] = mypsd(signal, rate, ...);
+%                Calculates psd as well as accumulated displacement 
+%                and returns all: power-density(p), frequencies(f), accum. disp. (id)
+%%  Notes:  
 %  - Resolution cannot exceed 1/time-span
-%   
-%   
+%    
 %  ??/??/?? - created by gb.  
 %  05/06/04 - added documentation; jcribb/kvdesai
 %  05/07/04 - added integrated displacement option; kvdesai
 %  
 varargout = cell(nargout); %initialize outputs with empty cells
-if(nargin < 6 | isempty(calculateId))
-    calculateId = 'no';
-end
 if(nargin < 5 | isempty(style))
 	style = '-';
 end
@@ -64,28 +75,23 @@ end
 for i=1:size(s,2)
    % [Pxx,f]=pwelch(X, WINDOW,NOVERLAP,NFFT)   
    [p(:,i) f] = pwelch(s(:,i), win, nw/2, nw, rate); 
-   if(findstr(calculateId,'y'))
-       Id(1,i) = sqrt(p(1,i)*f(1));
-       for j = 2:length(p(:,i))
-           Id(j,i) = sqrt(Id(j-1,i)^2 + p(j,i)*(f(j)-f(j-1)));%discrete integration
-       end
-   end
 end
 
 switch nargout
-	case 1
-       varargout{1} = p;
-	case 2
-       varargout{1} = p;
-       varargout{2} = f;
-   case 3
-       varargout{1} = p;
-       varargout{2} = f;
-       varargout{3} = Id;
-	otherwise
-       loglog(f, p, style);
-       zoom on;
-       grid on;
+    case 1
+        varargout{1} = p;
+    case 2
+        varargout{1} = p;
+        varargout{2} = f;
+    case 3
+        varargout{1} = p;
+        varargout{2} = f;
+        % Integrated displacement = sqrt(area under psd)
+        varargout{3} = sqrt(cumsum(p)*mean(diff(f)));
+    otherwise
+        loglog(f, p, style);
+        zoom on;
+        grid on;
 end
 
       
