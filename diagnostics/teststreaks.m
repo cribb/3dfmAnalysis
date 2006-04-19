5311bd80a65c1f3d8af178f9717641a2170b93e4
Nanomanipulator Work <nanowork@cs.unc.edu>
2006-04-19 15:51:47 +0000

    2006-04-19	Kalpit Desai <kvdesai@email.unc.edu> 	* teststreaks.m: Removed the code that asked to save the data. It wasn't being used ever, and was annoying. Fixed documentation.

--- diagnostics/teststreaks.m
+++ diagnostics/teststreaks.m
@@ -1,13 +1,14 @@
-function data =  teststreaks(amp_um, f, Nrepeat)
+function data =  teststreaks(amp_um, f, Nseconds)
 % A stand-alone function used to test streaks on camera by moving MCL stage
-% in circles.
-% Last modified 3 October 05 by: kvdesai
+% in circles. This lets us test various specifications and settings of the
+% camera.
+% Last modified 19 April 06 by: kvdesai
 % Usage:
-%   data =  teststreaks(radius_um, Hz, Nrepeat)
+%   data =  teststreaks(radius_um, Hz, Nseconds)
 %   Where,
 %   radius_um = desired radius of circle in microns
 %   Hz = Frequency of the circular motion
-%   Nrepeat = How many time do you want to do circles?
+%   Nseconds = duration in number of seconds
 AOname = 'PCI-6733';   %id = 1
 AIname = 'PCI-6052E';  %id = 2
 sampleRate = 10000; 
@@ -39,7 +40,7 @@ end
 AO = analogoutput('nidaq', AOid);
 set(AO, 'SampleRate', sampleRate);
 Ochan = addchannel(AO, 0:1); % X==0, Y==1
-set(AO, 'RepeatOutput', Nrepeat);
+set(AO, 'RepeatOutput', Nseconds);
 % set up the input
 AI = analoginput('nidaq', AIid);
 set(AI, 'InputType', 'Differential');
@@ -62,7 +63,11 @@ set(AI, 'SamplesPerTrigger', window);
 %of those positions.
 % setVolts = [1, 2, 3];
 putsample(AO,[5 5]); %put the stage at the center
-input('Stage just put at the center, wait couple of seconds and press any key to proceed');
+% stage_current = getsample(AI);
+
+disp('Stage was just told to go at the center.');
+disp('If this is the first run, wait a couple of seconds and press any key to proceed');
+input('If this is a re-try, then just press any key to proceed');
 putdata(AO, drive);
 start(AO);
 start(AI);
@@ -73,31 +78,30 @@ xlabel('X position [microns]');
 ylabel('Y position [microns]');
 legend('Commanded','Measured');
 
-asksave = input('See plots and press [Y] if data is to be saved OR press [N] if you want to discard this data','s');
+% asksave = input('See plots and press [Y] if data is to be saved OR press [N] if you want to discard this data','s');
 
-%     data.info.user_input = info;
-%     data.info.dateNtime = datestr(now,30);
     data.info.sampleRate = sampleRate;
     data.info.seconds = seconds;
     data.stageDrive = drive;
     data.stageSensor = dacIn(:,1:3);
-if(strncmpi(asksave,'y',1))
-        %     info = input(['Type any information you \n', ...
-%             'want to be associated with this noise scan \n',...
-%             'Press Enter only when you are done\n'],'s');
+% if(strncmpi(asksave,'y',1))
+%         %     info = input(['Type any information you \n', ...
+% %             'want to be associated with this noise scan \n',...
+% %             'Press Enter only when you are done\n'],'s');
+% %     
+% 
+%     [filename, pathname] = uiputfile('*.mat', 'Save streak data as');
 %     
-
-    [filename, pathname] = uiputfile('*.mat', 'Save streak data as');
-    
-    fullname = horzcat(pathname,'\',filename);
-    save(fullname,'data');
-    disp(['The noise data is saved as ', fullname]);
-else
-    disp('Data was discarded...Waiting for drive to complete');
-%     data = [];
-end
-% putsample(AO,[0 0 0 0]);%Return stage to the origin
-waittilstop(AO,seconds*Nrepeat);
+%     fullname = horzcat(pathname,'\',filename);
+%     save(fullname,'data');
+%     disp(['The noise data is saved as ', fullname]);
+% else
+%     disp('Data was discarded...'); 
+% %     data = [];
+% end
+disp('Waiting for buffered drive to be complete..');
+% putsample(AO,[0 0 0 0]);%Return stage to the origin DONT
+wait(AO,seconds*Nseconds);
 stop(AO);
 stop(AI);
 delete(AI);
@@ -105,4 +109,4 @@ delete(AO);
 clear AI;
 clear AO;
 daqreset;
-disp('Finished cleaning up! Good bye!');
+disp('Finished. Cleaned up! Good bye!');
