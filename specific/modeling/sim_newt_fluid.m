dce49504c6df57cb14176577d315096182e437cb
Nanomanipulator Work <nanowork@cs.unc.edu>
2006-04-24 16:50:04 +0000

    2006-04-24  Jeremy Cribb <jcribb@email.unc.edu> 	* sim_newt_fluid.m: added.  simulates Newtonian fluid random walk in any dimension. 	* sim_newt_fluid_1D.m: removed. 	* sim_newt_fluid_2D.m: removed. 	* sim_newt_fluid_3D.m: removed.

---
+++ specific/modeling/sim_newt_fluid.m
@@ -0,0 +1,41 @@
+function x = sim_newt_fluid(viscosity, bead_radius, sampling_rate, duration, temp, dim)
+% 3DFM function  
+% Rheology 
+% last modified 04/24/2006 (jcribb)
+%  
+% This function outputs the stochastic displacement over time for a bead in
+% a Newtonian fluid for a temperature in Kelvin. The viscosity of the fluid, 
+% the radius of the bead, the sampling_rate, and the duration of the data 
+% collection are all needed for the approximation.  To get a theoretical power 
+% spectrum for the same fluid (where sampling_rate -> inf and duration -> inf) look
+% at sim_newt_ps.
+%  
+%  [displacement] = sim_newt_fluid(viscosity, bead_radius, sampling_rate, duration, temp, dim);  
+%   
+%  where "viscosity" is in [Pa sec] 
+%        "bead_radius" is in [m] 
+%		 "sampling_rate" is in [Hz] 
+%        "duration" is in [s]
+%        "temp" is in [K]
+%        "dim" is the dimension of diffusion (usually 1D, 2D, 3D)
+%  
+%  Notes:  
+%   
+%  - For results that truly approximate the bulk response of the material,
+%  you need lots of repeats and averaging.
+%   
+
+    
+	% randomize state of random number generator
+	rand('state',sum(100000*clock));
+
+    time_step = 1 / sampling_rate;    
+    k = 1.3806e-23;   % boltzmann's constant
+    
+    gamma = 6 * pi * viscosity * bead_radius;        
+    D = k * temp / gamma;
+    A = sqrt(2 * D * time_step);
+    v = A * randn(sampling_rate * duration, dim);
+
+    x =  cumsum(v);
+    
