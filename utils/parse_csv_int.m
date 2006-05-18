6ad941f6495c944f6e9ea7888d8eac481f23ce06
Kalpit Desai <kvdesai@cs.unc.edu>
2006-05-18 07:56:57 +0000

    2006-05-18  Kalpit Desai <kvdesai@email.unc.edu> 	* parse_csv_int.m: Fixed a bug where multidigit numbers in the string weren't recognized correctly.

--- utils/parse_csv_int.m
+++ utils/parse_csv_int.m
@@ -12,10 +12,11 @@ function numerVec = parse_csv_int(str)
 % '1x 2 yz : x1y 3 c : 4 e' is parsed as [2:3:4]
 % 
 % Created: ??/??/2005     -kvdesai
-% Last Modified: 05/17/06 -kvdesai
+% Last Modified: 05/18/06 -kvdesai
 
 numerVec = [];
-inum = find(isstrprop(str,'digit') ~= 0 );
+digflags = isstrprop(str,'digit');
+inum = find(digflags ~= 0 );
 if isempty(inum)
     disp('Warning: No numeric character was found in the string supplied to parse_csv_int.');    
     return;
@@ -23,16 +24,16 @@ end
 
 if ~isempty(strfind(str,':'))
     icolon = strfind(str,':');
-    imin = inum(max(find(inum < icolon(1))));
+    imin = getneighbors(inum(max(find(inum < icolon(1)))),digflags);    
     dmin = str2num(str(imin));        
     if length(icolon) > 1
-        istep = inum(max(find(inum < icolon(2) & inum > icolon(1))));
+        istep = getneighbors(inum(max(find(inum < icolon(2) & inum > icolon(1)))),digflags);
         dstep = str2num(str(istep));
-        imax = inum(min(find(inum > icolon(2))));
+        imax = getneighbors(inum(min(find(inum > icolon(2)))),digflags);
         dmax = str2num(str(imax));
     else % length(icolon) = 1        
         dstep = 1;
-        imax = inum(min(find(inum > icolon(1))));
+        imax = getneighbors(inum(min(find(inum > icolon(1)))),digflags);
         dmax = str2num(str(imax));
     end
 %     disp(icolon); disp(inum); disp([dmin dstep dmax]);
@@ -42,6 +43,31 @@ if ~isempty(strfind(str,':'))
     end
 else   
    for c = 1:length(inum)
-       numerVec(c) = str2num(str(inum));
+       thisnum = str2num(str(getneighbors(inum(c),digflags)));
+       if any(numerVec == thisnum)
+           continue; % This is a duplication caused because of considering neighbors
+       else
+           numerVec = [numerVec,thisnum];
+       end
    end
 end
+%---------------
+function ingh = getneighbors(i,digflags)
+ingh = [];
+if digflags(i) == 0
+    error('Array parsing code is messed up. Tell some programmer to fix');
+    return;
+end
+ibef = max(find(digflags(1:i-1) == 0)); 
+if isempty(ibef)
+    ibef = 1;
+else
+    ibef = ibef + 1;
+end
+iaft = min(find(digflags(i+1:end) == 0));
+if isempty(iaft)
+    iaft = length(digflags) - i;
+else
+    iaft = iaft - 1;
+end
+ingh = ibef:i+iaft;
