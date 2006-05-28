999509d5d07baf8440e48bd28e170b6a6f1fa946
Kalpit Desai <kvdesai@cs.unc.edu>
2006-05-28 21:15:11 +0000

    2006-05-29  Kalpit Desai <kvdesai@email.unc.edu> 	* myspectrogram.m: 	- Replaced the input argument 'frequency resolution' by 'time resolution' because time resolution is more intuitive. 	- Instead of finest possible resolution, the default is now set to achieve best tradeoff between time resolution and frequency resolution. 	- Exposed one more output argument: PSD for each time segment

--- dsp/myspectrogram.m
+++ dsp/myspectrogram.m
@@ -1,4 +1,4 @@
-function [s, f, t, p] = myspectrogram(sig, rate, fres, window_type, clrmap)
+function varargout = myspectrogram(sig, rate, tres, window_type, clrmap)
 % 3DFM function  
 % DSP 
 % last modified 05/04/06 - kvdesai
@@ -8,19 +8,22 @@ function [s, f, t, p] = myspectrogram(sig, rate, fres, window_type, clrmap)
 % Function signature:
 %  [s, f, t] = myspectrogram(signal, rate, fres, window_type,clrmap);  
 %   
-%  where "signal" is a matrix (each column is treated independently)
-%        "rate" is the sampling rate in units of [Hz] [required argument] 
-%		 "fres"  is the desired PSD resolution in [Hz]
-%               default value is the maximum that could be achieved
-%               for a given length of data.
-%        "window_type" couldbe any of
-%               'blackman' 'rectangle' 'hamming' 'hann', defaulting to 'blackman'
-%        "clrmap" could be any of the matlab recognized colormaps.
-%        Default is gray.
-%          
-%        "s" is the estimated short-term, time-localized frequency content of signal
-%        "f" is the frequencies at which the spectrogram is computed
-%        "t" is the vector of times at which the spectrogram is computed%        
+%  where 
+%    "signal" is a one column vector
+%    "rate" is the sampling rate in units of [Hz] [required argument] 
+%    "tres"  is the desired time resolution in seconds
+%         default value is the the one that gives equal number of points on
+%         time and frequency axis.
+%    "window_type" could be any of
+%         'blackman' 'rectangle' 'hamming' 'hann', defaulting to 'blackman'
+%    "clrmap" could be any of the matlab recognized colormaps.
+%         Default is gray.
+% 
+%    "s" is the estimated short-term, time-localized frequency content 
+%     of signal (Nf x Nt) 
+%    "f" is the frequencies at which the spectrogram is computed (Nf x 1) 
+%    "t" is the time-vector at which the spectrogram is computed (1 x Nt)
+%    "p" is the psd of each segment (Nf x Nt)    
 %  Various Usages:
 %           myspectrogram(signal, rate, ...);
 %                Calculates spectrogram and plots it on semilogx surface.
@@ -35,14 +38,29 @@ function [s, f, t, p] = myspectrogram(sig, rate, fres, window_type, clrmap)
 varargout = cell(nargout); %initialize outputs with empty cells
 if(nargin < 5 | isempty(clrmap))  	clrmap = gray; end
 if(nargin < 4 | isempty(window_type))  	window_type = 'blackman'; end
-if(nargin < 3 | isempty(fres))
-  fres = ceil(rate/size(sig,1));%the finest possible resolution with given data
+if(nargin < 3 | isempty(tres))
+    % here we don't want the finest possible resolution in frequency,
+    % because that will result in psd (only 1 bin for time) and not spectrogram.
+    % Instead, we want frequency resolution that is the best trade, i.e. 
+    % it should result in equal points on frqeuncy axis and time axis. 
+    % It could be easily shown that if 
+    % Nt = number of points on time axis and
+    % Nf = number of points on frequency axis then
+    % Nt*Nf = total number of points in the data. 
+    %               (Nt+1)*(Nf-1) = N, to be precise, but we ignore 1 
+    % Then for the condition Nt = Nf,  Nf = sqrt(N) = Nt.
+    % Apply this to tres = span/Nt and fres = srate/(2*Nf);
+    tres = (size(sig,1)/rate)/ceil(sqrt(size(sig,1)));  
+%     fres = rate/(2*floor(sqrt(size(sig,1))));
 end
 
-nw = fix(rate/(2*fres))*2; %length of fft
+% fres = 1/(2*tres);
+% nw = fix(rate/(2*fres))*2;
+
+nw = fix(rate*tres)*2; %length of fft, make it even
+
 if (nw > size(sig,1))
-    disp('myspectrogram ERROR: Not enough data for requested frequency-resolution.');
-    disp('Time span of data should atleast be 1/resolution');
+    disp('myspectrogram ERROR: Time resolution can not be greater than time span');
     return;   
 end
 win = zeros(nw,1);
@@ -60,10 +78,8 @@ switch window_type
         return;       
 end
 
-for c=1:size(sig,2)
-%    [s,f,t] = spectrogram(x,window,noverlap,nfft,fs) 
-   [s(:,:,c) f t p] = spectrogram(sig(:,c), win, nw/2, nw, rate); 
-end
+%    [s,f,t] = spectrogram(x,window,noverlap,nfft,fs)
+[s f t p] = spectrogram(sig(:,1), win, nw/2, nw, rate);
 
 switch nargout
     case 1
@@ -72,6 +88,11 @@ switch nargout
         varargout{1} = s;
         varargout{2} = f;        
         varargout{3} = t;
+    case 4
+        varargout{1} = s;
+        varargout{2} = f;        
+        varargout{3} = t;
+        varargout{4} = p;
     otherwise
         args = {f,t,10*log10(abs(p')+eps)};         
         surf(args{:},'EdgeColor','none');
