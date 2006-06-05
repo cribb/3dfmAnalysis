fa4d74f11a4967dd012710b5e10c048a5db8e303
Nanomanipulator Work <nanowork@cs.unc.edu>
2006-06-05 14:23:06 +0000

    2006-06-05  Jeremy Cribb <jcribb@email.unc.edu> 	- ve.m: corrected documentation and added w to output structure.

--- specific/rheology/msd/ve.m
+++ specific/rheology/msd/ve.m
@@ -44,16 +44,22 @@ MYgamma = gamma(1 + alpha);
 % to delete the last row of f, tau, and msd values computed.
 msd = msd(1:end-1,:);
 tau = tau(1:end-1,:);
-gstar = (2/3) * (k*T) ./ (pi * bead_radius .* msd .* MYgamma);
 
+% get frequencies all worked out from timing (tau)
 f = 1 ./ tau;
 w = 2*pi*f;
 
+% compute shear and viscosity
+gstar = (2/3) * (k*T) ./ (pi * bead_radius .* msd .* MYgamma);
 gp = gstar .* cos(pi/2 .* alpha);
 gpp= gstar .* sin(pi/2 .* alpha);
+nstar = gstar .* tau;
 np = gpp .* tau;
 npp= gp  .* tau;
 
+%
+% setup very detailed output structure
+%
 % setting up axis transforms for the figures plotted below.  You cannot plot
 % errorbars on a loglog plot, it seems, so we have to set them up here.
 logtau = log10(tau);
@@ -120,18 +126,22 @@ ste_lognpp= nanstd(lognpp') ./ sqrt(N');
 	grid on;
 	pretty_plot;
 
+
 v.raw.f = f;
+v.raw.w = w;
 v.raw.tau = tau;
 v.raw.msd = msd;
 v.raw.alpha = alpha;
 v.raw.gamma = MYgamma;
 v.raw.gstar = gstar;
 v.raw.gp = gp;
-v.raw.gpp=gpp;
+v.raw.gpp = gpp;
+v.raw.nstar = nstar;
 v.raw.np = np;
 v.raw.npp = npp;
 
 v.mean.f = nanmean(f')';
+v.mean.w = nanmean(w')';
 v.mean.tau = nanmean(tau')';
 v.mean.msd = nanmean(msd')';
 v.mean.alpha = nanmean(alpha')';
@@ -139,10 +149,12 @@ v.mean.gamma = nanmean(MYgamma')';
 v.mean.gstar = nanmean(gstar')';
 v.mean.gp = nanmean(gp')';
 v.mean.gpp = nanmean(gpp')';
+v.mean.nstar = nanmean(nstar')';
 v.mean.np = nanmean(np')';
 v.mean.npp = nanmean(npp')';
 
 v.error.f = (nanstd(f') ./ sqrt(N'))';
+v.error.w = (nanstd(w') ./ sqrt(N'))';
 v.error.tau = (nanstd(tau') ./ sqrt(N'))';
 v.error.msd = (nanstd(msd') ./ sqrt(N'))';
 v.error.alpha = (nanstd(alpha') ./ sqrt(N'))';
@@ -150,8 +162,11 @@ v.error.gamma = (nanstd(MYgamma') ./ sqrt(N'))';
 v.error.gstar = (nanstd(gstar') ./ sqrt(N'))';
 v.error.gp = (nanstd(gp') ./ sqrt(N'))';
 v.error.gpp = (nanstd(gpp') ./ sqrt(N'))';
+v.error.nstar = (nanstd(nstar') ./ sqrt(N'))';
 v.error.np = (nanstd(np') ./ sqrt(N'))';
 v.error.npp = (nanstd(npp') ./ sqrt(N'))';
 
 v.n = N;
 
+% plot output
+plot_ve(v);
