9f10455ed9d0aa4f1b64e2ad1a8f3d64eea3593d
Nanomanipulator Work <nanowork@cs.unc.edu>
2006-07-20 21:30:32 +0000

    2006-07-20  Jeremy Cribb <jcribb@email.unc.edu> 	* forces2d.m: added documentation.

--- magnets/forces2d.m
+++ magnets/forces2d.m
@@ -1,8 +1,26 @@
-function [newxy,F] = forces2d(video_tracking_data, viscosity, bead_radius, calib_um, window_size);
+function [newxy,F] = forces2d(video_tracking_table, viscosity, bead_radius, calib_um, window_size);
+% 3DFM function  
+% Magnetics 
+% last modified 07/20/06 
+%  
+% This function computes 2d forces on beads in Newtonian fluid using Stokes
+% drag.
+%  
+%  [newxy,F] = forces2d(video_tracking_table, viscosity, bead_radius, calib_um, window_size);
+%   
+%  where "newxy" are the x and y positions on the new grid
+%        "F" is the computed force in [N]
+%        "video_tracking_table" is the data table returned from load_video_tracking
+%        "viscosity" of the Newtonian standard solution in [Pa s]
+%        "bead_radius" in [m]
+%        "calib_um" is the length calibration factor in [microns/pixel]
+%        "window_size" is an integer describing the derivative's time-step, tau
+%   
+
 
     video_tracking_constants;
     
-    temp = video_tracking_data;    
+    temp = video_tracking_table;    
 
     if(length(temp)>window_size) % avoid taking the derivative for any track 
                                  % whose number of points is less than the 
@@ -16,3 +34,5 @@ function [newxy,F] = forces2d(video_tracking_data, viscosity, bead_radius, calib
         newxy = [];
         F = [];
     end
+    
+    
