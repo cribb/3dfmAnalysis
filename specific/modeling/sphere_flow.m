e9ff52751dc359828abc35be1ad2677f578e0b12
Nanomanipulator Work <nanowork@cs.unc.edu>
2007-03-19 14:59:32 +0000

    2007-03-19  Jeremy Cribb <jcribb@email.unc.edu> 	* sphere_flow.m: clearer code.  Added quiver plot to convey full vector rather than just magnitude.  perhaps streamlines later.

--- specific/modeling/sphere_flow.m
+++ specific/modeling/sphere_flow.m
@@ -27,28 +27,51 @@ function vel = sphere_flow(vd, theta, a, r);
     % sphere's center, i.e. [0 0].  The velocity at this point is a vector
     % with two components:  along the radial component of the coordinate system 
     % is vr, while the azimuthal component is vtheta.
-	vr = -vd * cos(theta) * (1 - (3*a)./(2*r) + (a.^3)./(2*r.^2));
-	vtheta = vd * sin(theta) * (1 - (3*a)./(4*r) - (a.^3)./(4*r.^2));
+	vr = -vd * cos(theta) * (1 - (3*a)./(2*r) + (a.^3)./(2*r.^3));
+	vtheta = vd * sin(theta) * (1 - (3*a)./(4*r) - (a.^3)./(4*r.^3));
+    
+    % plot the results     
+    if length(theta) > 1 & length(r) > 1
+ 
+        MXtheta = repmat(theta, size(r));
+        MXr = repmat(r, size(theta));
+        
+        r = MXr;
+        theta = MXtheta;
+        
+        % extract the locations of the measurements in the cartesian coordinate 
+        % system for plotting purposes    
+		X =  r .* cos(theta);
+		Y =  r .* sin(theta);
+        U = vr .* cos(theta) - vtheta .* sin(theta);
+        V = vr .* sin(theta) + vtheta .* cos(theta);
+ 
+        Z = sqrt(vr.^2 + vtheta.^2);
+        
+        h=figure(100);
+%         surface(X, Y, Z);
+% 
+%         colormap(hot(256));
+%         colorbar;
+%         % this turns off mesh lines on surface (they may get into the way here)
+%         set(get(gca, 'Children'), 'EdgeColor', 'flat')
+%         hold on;       
+
+        [sx, sy] = meshgrid([min(min(Y)):(max(max(Y))-min(min(Y)))/10:max(max(Y))]' , max(max(X)));
+        quiver(X, Y, U, V, 0.4);
+%         streamlines(X,Y,U,V);
+
+    end
     
     % The velocity vector results in a component that is parallel
     % to the unit vector, vr, and a component that is normal to the unit
     % vector, vtheta.  The unit vector in this case is aligned with the
     % input, r, which is the radius of the measurement location.
-    vel = vr + j*vtheta;
+    vel.theta = theta;
+    vel.r     = r;
+    vel.vr    = vr;
+    vel.vtheta= vtheta;
+%     vel.U = U;
+%     vel.V = V;
     
-    % plot the results     
-    if length(theta) > 1 & length(r) > 1
- 
-        % extract the locations of the measurements in the cartesian coordinate 
-        % system for plotting purposes    
-		X = r'*cos(theta');
-		Y = r'*sin(theta');
-        
-        h=figure;
-        surf(X, Y, abs(vel'));
-        colormap(winter(256));
-        colorbar;
-
-        % this turns off mesh lines on surface (they may get into the way here)
-        set(get(gca, 'Children'), 'EdgeColor', 'flat')
-    end
+    
