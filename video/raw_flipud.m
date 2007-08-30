68e7dccead4e63dc79e3ebc6f7e9c20e80ad0262
Nanomanipulator Work <nanowork@cs.unc.edu>
2007-08-30 20:41:15 +0000

    2007-08-30  Jeremy Cribb <jcribb@email.unc.edu> 	* raw_flipud.m: added.  vertically flips rawfile.

---
+++ video/raw_flipud.m
@@ -0,0 +1,49 @@
+function v = raw_flipud(rawfilein, rawfileout)
+tic;
+
+% get input file information
+file = dir(rawfilein);
+
+% setup the input file
+fid = fopen(rawfilein);
+fod = fopen(rawfileout,'w');
+
+% frame properties
+rows = 484;
+cols = 648;
+color_depth = 1; % bytes
+frame_size = rows * cols * color_depth;
+number_of_frames = (file.bytes) / frame_size;
+
+% set up text-box for 'remaining time' display
+[timefig,timetext] = init_timerfig;
+	
+for k=1:number_of_frames    
+    tic;
+    
+    im = fread(fid, [648,484],'uint8');   % read in the next frame    
+
+    im = im';
+    im = flipud(im);
+    im = im';
+    
+    fwrite(fod, im, 'uint8');
+    
+    % handle timer
+    itertime = toc;
+    if k == 1
+        totaltime = itertime;
+    else
+        totaltime = totaltime + itertime;
+    end    
+    meantime = totaltime / k;
+    timeleft = (number_of_frames-k) * meantime;
+    outs = [num2str(timeleft, '%5.0f') ' sec.'];
+    set(timetext, 'String', outs);
+
+    drawnow;
+end
+
+fclose('all');
+
+v = 0;
