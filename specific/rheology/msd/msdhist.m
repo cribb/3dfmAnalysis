e4ac55f2e016538c2a08145599df4fde22c220fc
Nanomanipulator Work <nanowork@cs.unc.edu>
2008-02-26 20:53:28 +0000

    2008-02-26  Jeremy Cribb <jcribb@email.unc.edu> 	- msdhist.m: removed plotting code. 	- plot_msdhist.m: removed computations.

--- specific/rheology/msd/msdhist.m
+++ specific/rheology/msd/msdhist.m
@@ -1,7 +1,7 @@
-function v = msdhist(tau, msd)
+function v = msdhist(d, num_bins)
 % 3DFM function
 % Rheology
-% last modified 07/06/07
+% last modified 02/24/08
 %
 % This function plots the graph of mean square displacement versus tau for an aggregate number
 % of beads.
@@ -16,12 +16,8 @@ function v = msdhist(tau, msd)
 %      to plot the cumulative msd histogram
 %
 
-if nargin < 3 || isempty(optstring)
-    optstring = 'c';
-end
-
-if nargin < 2 || isempty(h)
-    h = figure;
+if nargin < 2 || isempty(num_bins)
+    num_bins = 51;
 end
 
 % renaming input structure
@@ -30,11 +26,11 @@ r2  = d.r2;
 msd = d.msd;
 
 if isfield(d, 'n');
-    sample_count = d.n;
+    n = d.n;
 else
-    sample_count = sum(~isnan(msd),2);
+    n = sum(~isnan(msd),2);
     idx = find(sample_count > 0);
-    sample_count = sample_count(idx);
+    n = n(idx);
 end
 
 logr = log10(r2);
@@ -44,7 +40,6 @@ logr = logr(~isinf(logr));
 minr = min(logr(:));
 maxr = max(logr(:));
 
-num_bins = 51;
 ranger = (maxr - minr) / num_bins;
 
 mybins = [minr : ranger : (maxr - ranger)];
@@ -57,76 +52,29 @@ logmsd = log10(msd);
 mean_logtau = nanmean(logtau');
 mean_logmsd = nanmean(logmsd');
 
-ste_logtau = nanstd(logtau') ./ sqrt(sample_count');
-ste_logmsd = nanstd(logmsd') ./ sqrt(sample_count');
+% ste_logtau = nanstd(logtau') ./ sqrt(sample_count');
+% ste_logmsd = nanstd(logmsd') ./ sqrt(sample_count');
 
-
-
-% creating the plot
-figure(h);
-
-if strcmpi(optstring, 'a')
     
-    % window, beadID, frame
-    for k = 1:size(r2,1)
-        data = log10(r2(k, :, :));
-        data = data(:);
-        data = data(~isnan(data));
-        data = data(~isinf(data));
+% window, beadID, frame
+for k = 1:size(r2,1)
+    data = log10(r2(k, :, :));
+    data = data(:);
+    data = data(~isnan(data));
+    data = data(~isinf(data));
 
 
-        [myhist(:,k), bins] = hist(data, num_bins);
-    %     pause;
-    end
-
-    myhist = myhist ./ repmat(d.n', size(myhist,1),1);
-    mytau = mean(tau,2);
-    
-%     plot(bins, myhist);
-%     xlabel('sq. dist');
-%     ylabel('count');
-    
-%     imagesc(log10(mytau), mybins, (myhist));
-    surf(log10(mytau), mybins, (myhist));
-    set(gca, 'YDir', 'normal');
-    colormap(hot(256));
-
-    xlabel('log_{10}(\tau [s])');
-    ylabel('log_{10}(disp^2)');
-    colorbar;
-
-elseif strcmpi(optstring, 'c')
-    
-    for k = 1:size(r2,2)
-        data = log10(r2(:, k, :));
-        data = data(:);
-        data = data(~isnan(data));
-        data = data(~isinf(data));
-
-
-        [myhist(:,k), bins] = hist(data, num_bins);
-    %     pause;
-    end
-
-    plot(bins, myhist, '.-');
-
-elseif strcmpi(optstring, 'ac')
-%     plot(logtau, logmsd, 'b', mean_logtau, mean_logmsd, 'k.-');
+    [myhist(:,k), bins] = hist(data, num_bins);
 end
 
-% xlabel('log_{10}(\tau) [s]');
-% ylabel('log_{10}(MSD) [m^2]');
-grid on;
-pretty_plot;
-refresh(h);    
+myhist = myhist ./ repmat(n', size(myhist,1),1);
+mytau = mean(tau,2);
+
+d.hist = myhist;
+d.htau = mytau;
+d.bins = mybins;
+
+v = d;
 
 return;
-
-
-% taubins = mean(tau, 2);
-
-% v = hist(msd(1,:));
-
-% return
-
 
--- specific/rheology/msd/plot_msdhist.m
+++ specific/rheology/msd/plot_msdhist.m
@@ -12,8 +12,8 @@ function plot_msdhist(d, h, optstring)
 %       value (window size), and the number of beads in a given window.
 %       "h" is the figure handle in which to put the plot.  If h is not
 %       used, a new figure is generated.
-%      "optstring" is a string containing 'a' to plot overlaid individual histograms, 'c'
-%      to plot the cumulative msd histogram
+%      "optstring" is a string containing 's' to plot surface histogram, 'p'
+%      to plot histogram as scaled image. 
 %
 
 if nargin < 3 || isempty(optstring)
@@ -24,98 +24,42 @@ if nargin < 2 || isempty(h)
     h = figure;
 end
 
-% renaming input structure
-tau = d.tau;
-r2  = d.r2;
-msd = d.msd;
-
-if isfield(d, 'n');
-    sample_count = d.n;
-else
-    sample_count = sum(~isnan(msd),2);
-    idx = find(sample_count > 0);
-    sample_count = sample_count(idx);
-end
-
-logr = log10(r2);
-logr = logr(~isnan(logr));
-logr = logr(~isinf(logr));
-
-minr = min(logr(:));
-maxr = max(logr(:));
-
-num_bins = 51;
-ranger = (maxr - minr) / num_bins;
-
-mybins = [minr : ranger : (maxr - ranger)];
-
-% setting up axis transforms for the figure plotted below.  You cannot plot
-% errorbars on a loglog plot, it seems, so we have to set it up here.
-logtau = log10(tau);
-logmsd = log10(msd);
-
-mean_logtau = nanmean(logtau');
-mean_logmsd = nanmean(logmsd');
-
-ste_logtau = nanstd(logtau') ./ sqrt(sample_count');
-ste_logmsd = nanstd(logmsd') ./ sqrt(sample_count');
-
-
+mytau = d.htau;
+myhist = d.hist;
+mybins = d.bins;
 
 % creating the plot
 figure(h);
 
-if strcmpi(optstring, 'a')
+if strcmpi(optstring, 's')
     
-    % window, beadID, frame
-    for k = 1:size(r2,1)
-        data = log10(r2(k, :, :));
-        data = data(:);
-        data = data(~isnan(data));
-        data = data(~isinf(data));
-
-
-        [myhist(:,k), bins] = hist(data, num_bins);
-    %     pause;
-    end
-
-    myhist = myhist ./ repmat(d.n', size(myhist,1),1);
-    mytau = mean(tau,2);
-    
-%     plot(bins, myhist);
-%     xlabel('sq. dist');
-%     ylabel('count');
-    
-%     imagesc(log10(mytau), mybins, (myhist));
-    surf(log10(mytau), mybins, (myhist));
+    surf(log10(mytau), mybins, log10(myhist));
     set(gca, 'YDir', 'normal');
     colormap(hot(256));
 
     xlabel('log_{10}(\tau [s])');
-    ylabel('log_{10}(disp^2)');
+    ylabel('log_{10}(r^2)');
+    zlabel('count');
     colorbar;
 
-elseif strcmpi(optstring, 'c')
-    
-    for k = 1:size(r2,2)
-        data = log10(r2(:, k, :));
-        data = data(:);
-        data = data(~isnan(data));
-        data = data(~isinf(data));
+elseif strcmpi(optstring, 'p')
 
+    imagesc(log10(mytau), mybins, (myhist));
+    set(gca, 'YDir', 'normal');
+    colormap(hot(256));
 
-        [myhist(:,k), bins] = hist(data, num_bins);
-    %     pause;
-    end
-
-    plot(bins, myhist, '.-');
+    xlabel('log_{10}(\tau [s])');
+    ylabel('log_{10}(r^2)');
+    zlabel('count');
+    colorbar;
 
 elseif strcmpi(optstring, 'ac')
-%     plot(logtau, logmsd, 'b', mean_logtau, mean_logmsd, 'k.-');
 end
 
+% plot(bins, myhist, '.-');
 % xlabel('log_{10}(\tau) [s]');
 % ylabel('log_{10}(MSD) [m^2]');
+
 grid on;
 pretty_plot;
 refresh(h);    
