476cedd70db63959746d18f6f92486b1004e1008
Nanomanipulator Work <nanowork@cs.unc.edu>
2008-09-10 19:37:05 +0000

    2008-09-10  Jeremy Cribb <jcribb@email.unc.edu> 	* carreau_model_fit.m: added.  performs fit for the Carreau-Yasuda model, a non-linear model for shear thinning/thickening in macroscale rheology. 	* carreau_model_fun.m: added.  contains fitting function for Carreau-Yasuda model. 	* cross_model_fit.m: adjusted options parameters for better fitting and removed log-space representation of function. 	* cross_model_fun.m: removed log-space representation of function.

--- specific/rheology/models/carreau_model_fit.m
+++ specific/rheology/models/carreau_model_fit.m
@@ -1,4 +1,4 @@
-function [eta_zero, eta_inf, lambda, m, R_square] = cross_model_fit(gamma_dot, eta_apparent, report)
+function [eta_zero, eta_inf, lambda, m, n, R_square, eta_apparent_fit] = carreau_model_fit(gamma_dot, eta_apparent, report)
 % 3DFM function  
 % Rheology
 % last modified 2008.03.19
@@ -7,7 +7,7 @@ function [eta_zero, eta_inf, lambda, m, R_square] = cross_model_fit(gamma_dot, e
 % a materials apparent viscosity as a function of shear rate. The fitting parameters
 % as well as an R_square fitness value are provided as outputs.
 %  
-%  [eta_zero, eta_inf, lambda, m, R_square] = cross_model_fit(gamma_dot, eta_apparent, report);  
+%  [eta_zero, eta_inf, lambda, m, n, R_square, eta_fit] = carreau_model_fit(gamma_dot, eta_apparent, report);  
 %   
 %  where "gamma_dot" contains the input shear rates [s^-1].
 %        "eta_apparent" contains the apparent viscosity at the input shear rate.
@@ -17,31 +17,34 @@ function [eta_zero, eta_inf, lambda, m, R_square] = cross_model_fit(gamma_dot, e
 %        "eta_inf" is the damper parallel to the spring in [N s m^-1] 
 %        "lambda" is time constant for switch to thinning from non-thinning
 %        regime
-%        "m" is powerlaw slope of thinning regime
+%        "m" is index that controls the width of the thinning regime
+%        "n" is  related to the power law such that (n-1) is equal to
+%        powerlaw slope.
 %        "R_square" is the a measure of fitness.
 %
 
     % set parameters for the options structure sent to lsqcurvefit.
-	options = optimset('MaxFunEvals', 10000, ...
+	options = optimset('MaxFunEvals', 100000, ...
                        'Diagnostics', 'on', ...
-                       'TolFun', 1e-12, ...
-                       'MaxIter', 50000, ...
-                       'TolX', 1e-7, ...
+                       'TolFun', 1e-17, ...
+                       'MaxIter', 500000, ...
+                       'TolX', 1e-9, ...
                        'ShowStatusWindow', 'on');
 
 	% initial guess
-    m        = -0.8;
-    lambda   = 0.05;
+    m        = 0.68;
+    n        = 0.2;
+    lambda   = 20;
     eta_zero = 10;
-    eta_inf  = 0.01;
-
+    eta_inf  = 0.016;
+    
 % % %     % normalize t and x
 % % %     t = t - min(t);    
 % % %     x = x - min(x);
     
-    init_cond = [m lambda eta_zero eta_inf];
+    init_cond = [m n lambda eta_zero eta_inf];
     
-	[fit, resnorm, residuals] = lsqcurvefit('cross_model_fun', init_cond, gamma_dot, eta_apparent,[],[],options);
+	[fit, resnorm, residuals] = lsqcurvefit('carreau_model_fun', init_cond, gamma_dot, eta_apparent, [], [], options);
 
     % standard deviation of measurements == rms of the residuals
 	rms_residuals = rms(residuals);
@@ -53,13 +56,14 @@ function [eta_zero, eta_inf, lambda, m, R_square] = cross_model_fit(gamma_dot, e
 	R_square = 1 - sse/sst;
 	    
     m        = fit(1);
-	lambda   = fit(2);
-	eta_zero = fit(3);
-	eta_inf  = fit(4);
-
-    % go to town.  this is our fitting function
-    eta_apparent_fit = (eta_zero - eta_inf) ./ ( (1+lambda*gamma_dot) .^ m) + eta_inf;
+    n        = fit(2);
+	lambda   = fit(3);
+	eta_zero = fit(4);
+	eta_inf  = fit(5);
     
+    % go to town.  this is our fitting function
+    eta_apparent_fit = (eta_zero - eta_inf) .* ( (1+lambda*gamma_dot) .^ ((n-1)/m)) + eta_inf;
+
     if findstr(report, 'y')
         figure;
         loglog(gamma_dot, eta_apparent, '.', gamma_dot, eta_apparent_fit, 'r-');
--- specific/rheology/models/carreau_model_fun.m
+++ specific/rheology/models/carreau_model_fun.m
@@ -1,11 +1,11 @@
-function x = cross_model_fun(init_cond, gamma_dot, eta_apparent)
+function x = carreau_model_fun(init_cond, gamma_dot, eta_apparent)
 % 3DFM function  
 % Rheology
 % last modified 2008.03.19
 %  
 % This is the fitting function for cross_model_fit. 
 %  
-%  x = cross_model_fun(init_cond, gamma_dot, eta_apparent);
+%  x = carreau_model_fun(init_cond, gamma_dot, eta_apparent);
 %   
 %  where "init_cond" contains the parameters passed from lsqcurvefit.
 %        "gamma_dot" contains the input shear rates [s^-1].
@@ -16,11 +16,13 @@ function x = cross_model_fun(init_cond, gamma_dot, eta_apparent)
 
     % set guesses for fitting parameters
     m        = init_cond(1);
-    lambda   = init_cond(2);
-    eta_zero = init_cond(3);
-    eta_inf  = init_cond(4);
+    n        = init_cond(2);
+    lambda   = init_cond(3);
+    eta_zero = init_cond(4);
+    eta_inf  = init_cond(5);
 
     % go to town.  this is our fitting function
-    x = (eta_zero - eta_inf) ./ ( (1+lambda*gamma_dot) .^ m) + eta_inf;
+    x = (eta_zero - eta_inf) .* ( (1+lambda*gamma_dot) .^ ((n-1)/m)) + eta_inf;
     
     
+
