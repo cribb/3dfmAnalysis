1b2762389398221fc49ab1b2f6b8000a8eb5910c
Nanomanipulator Work <nanowork@cs.unc.edu>
2008-10-14 17:07:27 +0000

    2008-10-14  Jeremy Cribb <jcribb@email.unc.edu> 	* plotxx.m: added.  obtained from http://www.mathworks.com/matlabcentral/fileexchange/loadFile.do?objectId=317

--- external/circle.m
+++ external/circle.m
@@ -1,8 +1,17 @@
-function h = circle(x,y,r,linespec)
+function h = circle(x,y,r,linespec, fillspec)
+
+if nargin < 5 || isempty(fillspec)
+    fillspec = 'n';
+end
 
 hold on
-th = 0:pi/50:2*pi;
+th = 0:pi/256:2*pi;
 xunit = r * cos(th) + x;
 yunit = r * sin(th) + y;
 h = plot(xunit, yunit,linespec);
+
+if fillspec =='y'
+    h = fill(xunit, yunit, linespec);
+end
+   
 hold off
