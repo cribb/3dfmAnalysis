007e820bd25353cec823a3c6d8bcdec9176da849
Nanomanipulator Work <nanowork@cs.unc.edu>
2008-10-30 18:02:33 +0000

    2008-10-30  Jeremy Cribb <jcribb@email.unc.edu> 	* select_data_in_box.m: added awareness of several series rather than just selecting the first one.

--- utils/select_data_in_box.m
+++ utils/select_data_in_box.m
@@ -20,11 +20,60 @@ function [xout, yout, idx, serh] = select_data_in_box(fig)
 	y = get(serh, 'YData');
 	
 	if iscell(x)
-		x = x{end};
-		y = y{end};
-	end
-	
-	idx = find(x > xlo & x < xhi & y > ylo & y < yhi);
-	xout = x(idx);
-	yout = y(idx);
+% 		x = x{end};
+% 		y = y{end};
+        for k = 1:length(x)
+            idx = find(x{k} > xlo & x{k} < xhi & y{k} > ylo & y{k} < yhi);
+            x{k} = x{k}(idx);
+            y{k} = y{k}(idx);
+        end
+        
+        for k = length(x):-1:1
+            if isempty(x{k})
+                x(k) = [];
+                y(k) = [];
+            end
+        end
+        
+        if length(x) == 1
+            xout = x{1};
+            yout = y{1};
+        else
+            logentry('Only keeping first series on which you clicked.');
+            xout = x{1};
+            yout = y{1};
+            % This function typically assumes you've clicked on only one
+            % dataset.  The simplest thing to do would be to extract just
+            % the data in the first cell position.  Ideally you would want
+            % to be able to extract all the information, or ask which
+            % series, and what happens if the data aren't identically
+            % sampled? oy
+        end
+    elseif isnumeric(x)
+            idx = find(x > xlo & x < xhi & y > ylo & y < yhi);
+
+            xout = x(idx);
+            yout = y(idx);
+    else
+        error('Unknown behavior.');
+    end
+    
+    return;
+    
+    
+function logentry(txt)
+
+    logtime = clock;
+    logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
+                   num2str(logtime(2),        '%02i') '.' ...
+                   num2str(logtime(3),        '%02i') ', ' ...
+                   num2str(logtime(4),        '%02i') ':' ...
+                   num2str(logtime(5),        '%02i') ':' ...
+                   num2str(round(logtime(6)), '%02i') ') '];
+     headertext = [logtimetext 'select_data_in_box: '];
+     
+     fprintf('%s%s\n', headertext, txt);
+     
+     return;
+     
     
