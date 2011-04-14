52f2df511e1608cd602090c93ddf0ae5d7257507
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-04-14 20:53:21 +0000

    2011-04-14  Jeremy Cribb <jcribb@email.unc.edu> 	* SetFPSandMinFrames.m:  fixed bug where saving directory list corrupted output.

--- tracking/SetFPSandMinFrames.m
+++ tracking/SetFPSandMinFrames.m
@@ -1,4 +1,4 @@
-function outfilelist = SetFPSandMinFrames(filemask, frameRate, minFrames, minPixels,maxPixelRange, tcrop, xycrop)
+function outfilelist = SetFPSandMinFrames(filemask, frameRate, minFrames, minPixels, tcrop, xycrop)
 
 %USAGE:
 %     SetFPSandMinFrames(filemask, frameRate, minFrames, minPixels, tcrop,
@@ -12,27 +12,34 @@ video_tracking_constants;
 files = dir(filemask);
 
 count = 1;
+
 for i = 1:length(files)
     filename = files(i).name;
     
     fprintf('Loading %g of %g \n', i, length(files));
-    d = load_video_tracking(filename, frameRate, [], [], 'absolute', 'no', 'table', minFrames, minPixels,maxPixelRange, tcrop, xycrop);
+    d = load_video_tracking(filename, frameRate, [], [], 'absolute', 'no', 'table', minFrames, minPixels, tcrop, xycrop);
     
     if(~isempty(d))
         tracking.spot3DSecUsecIndexFramenumXYZRPY = d;
 
-        fprintf('\t%g trackers in ', length(unique(d(:,ID))));
-        fprintf(filename);
+        fprintf('\t%g trackers in %s. \n', length(unique(d(:,ID))), filename);
 
         append = 'evt.mat';
         outfile = [filename(1:end-3) append];
         save(outfile, 'tracking');
-        
-        outfilelist(count) = dir(outfile);
+               
+        if count <= 1
+            outfilelist = dir(outfile);
+        else
+            outfilelist(count) = dir(outfile);
+        end
+            
         count = count + 1;
     else
         fprintf('No trackers. No .evt.mat created.');
     end
     fprintf('\n');
+    
+    
 end
 
