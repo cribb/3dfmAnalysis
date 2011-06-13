79e7432a096453685992322ccb71b65d74c84550
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-06-13 17:27:27 +0000

    2011-06-13  Jeremy Cribb <jcribb@email.unc.edu> 	* plot_msd_2pt.m:  added on behalf of Thomas Sayre_McCord.  plots the 2pt MSD as a  function of time scale. 	* plot_ve_2pt.m:  added on behalf of Thomas Sayre_McCord.  plots the 2pt Generalized  stokes-einstein relation as a function of frequency.

---
+++ specific/rheology/msd/plot_msd_2pt.m
@@ -0,0 +1,89 @@
+function plot_msd_2pt(d, h, optstring)
+% PLOT_MSD plots the graph of mean square displacement versus tau for an aggregate number of beads 
+%
+% 3DFM function
+% specific\rheology\msd
+% last modified 11/20/08 (krisford)
+%
+%
+% plot_msd(d)
+%
+% where "d" is the input matrix and should contain the mean square displacement, the tau 
+%       value (window size), and the number of beads in a given window.
+%       "h" is the figure handle in which to put the plot.  If h is not
+%       used, a new figure is generated.
+%      "optstring" is a string containing 'm'
+%      to plot the mean msd function, and 'e' to include errorbars on mean.
+%
+
+if nargin < 3 || isempty(optstring)
+    optstring = 'me';
+end
+
+if nargin < 2 || isempty(h)
+    h = figure;
+end
+
+% renaming input structure
+tau = d(:,1,1);
+msd = d(:,4,1);
+msd = msd*10^-12; %converting to meters
+% counts = d.ns;
+
+% if isfield(d, 'n');
+%     sample_count = d.n;
+% else
+%     sample_count = sum(~isnan(msd),2);
+%     idx = find(sample_count > 0);
+%     sample_count = sample_count(idx);
+% end
+% 
+% clip = find(sum(~isnan(counts),2) <= 1);
+% tau(clip,:) = [];
+% msd(clip,:) = [];
+% counts(clip,:) = [];
+% sample_count(clip,:) = [];
+
+% setting up axis transforms for the figure plotted below.  You cannot plot
+% errorbars on a loglog plot, it seems, so we have to set it up here.
+logtau = log10(tau);
+logmsd = log10(msd);
+
+% numbeads = size(logmsd,2);
+% 
+% % weighted mean for logmsd
+% weights = counts ./ repmat(nansum(counts,2), 1, size(counts,2));
+% 
+% mean_logtau = nanmean(logtau');
+% mean_logmsd = nansum(weights .* logmsd, 2);
+% 
+% % computing error for logmsd
+% Vishmat = nansum(weights .* (repmat(mean_logmsd, 1, numbeads) - logmsd).^2, 2);
+% msderr =  sqrt(Vishmat ./ sample_count);
+
+msderr = d(:,7,1);
+% creating the plot
+figure(h);
+if strcmpi(optstring, 'a')
+    plot(logtau, logmsd, 'b');
+elseif strcmpi(optstring, 'm')
+%     plot(mean_logtau, mean_logmsd, 'k.-');
+plot(logtau,logmsd,'k.-');
+elseif strcmpi(optstring, 'am')
+    plot(logtau, logmsd, 'b', mean_logtau, mean_logmsd, 'k.-');
+elseif strcmpi(optstring, 'me') || strcmpi(optstring, 'e')
+    errorbar(logtau, logmsd, msderr, 'k.-');
+elseif strcmpi(optstring, 'ame')
+    plot(logtau, logmsd, 'b-');
+    hold on;
+        errorbar(mean_logtau, mean_logmsd, msderr, 'k.-');
+    hold off;
+end
+
+xlabel('log_{10}(\tau) [s]');
+ylabel('log_{10}(MSD) [m^2]');
+grid on;
+pretty_plot;
+refresh(h);    
+
+return;
---
+++ specific/rheology/msd/plot_ve_2pt.m
@@ -0,0 +1,170 @@
+function plot_ve_2pt(v, freq_type, h, optstring)
+% PLOT_VE plots the viscoelastic moduli using the ve output structure 
+%
+% 3DFM function
+% specific\rheology\msd
+% last modified 11/20/08 (krisford)
+%  
+%
+%  plot_ve(v, freq_type);  
+%   
+%  where "v" is the output structure of ve.
+%        "freq_type" is 'f' for [Hz] or 'w' for [rad/s], default is [rad/s]
+%  
+
+if nargin < 4 || isempty(optstring)
+    optstring = 'Gn';
+end
+
+if nargin < 3 || isempty(h)
+    h = figure;
+end
+
+if nargin < 2 || isempty(freq_type)
+    freq_type = 'w';
+end
+
+
+% pull out the variables we need for the plot from the ve data structure.
+f   = v.f;
+w   = v.w;
+tau = v.tau;
+% msd = v.msd;
+gp  = v.gp;
+gpp = v.gpp;
+np  = v.np;
+npp = v.npp;
+% N   = v.n;
+
+% gperr = v.error.gp;
+% gpperr= v.error.gpp;
+% nperr = v.error.np;
+% npperr= v.error.npp;
+gperr = 1;
+gpperr = 1;
+nperr = 1;
+nperr = 1;
+
+% setting up axis transforms for the figures plotted below.  You cannot plot
+% errorbars on a loglog plot, it seems, so we have to set them up here.
+logtau = log10(tau);
+logf   = log10(f);
+logw   = log10(w);
+
+loggp  = log10(gp);
+loggpp = log10(gpp);
+lognp  = log10(np);
+lognpp = log10(npp);
+
+% loggperr  = log10(gp+gperr) - log10(gp);
+% loggpperr = log10(gpp+gpperr) - log10(gpp);
+% lognperr  = log10(np + nperr) - log10(np);
+% lognpperr = log10(npp + npperr) - log10(npp);
+
+
+% select the desired depiction of frequency
+switch freq_type
+    case 'f'
+        plot_freq = logf;
+        freq_label = 'log_{10}(f) [Hz]';
+        gp_label = 'G''(f)';
+        gpp_label = 'G''''(f)';
+        np_label = '\eta''(f)';
+        npp_label = '\eta''''(f)';
+    case 'w'
+        plot_freq = logw;
+        freq_label = 'log_{10}(\omega) [rad/s]';
+        gp_label = 'G''(\omega)';
+        gpp_label = 'G''''(\omega)';
+        np_label = '\eta''(\omega)';
+        npp_label = '\eta''''(\omega)';
+end
+
+
+figure(h);
+clf(h);
+
+if strcmp(optstring, 'G')
+	errorbar(plot_freq, real(loggp), real(loggperr), 'b');
+    y_label = 'log_{10}(G'') [Pa]';
+    leg = gp_label;
+elseif strcmp(optstring, 'g')
+	errorbar(plot_freq, real(loggpp), real(loggpperr), 'b--');
+    y_label = 'log_{10}(G'''') [Pa]';
+    leg = gpp_label;
+elseif strcmp(optstring, 'N')
+	errorbar(plot_freq, real(lognp), real(lognperr), 'r');
+    y_label = 'log_{10}(\eta'') [Pa s]';
+    leg = np_label;
+elseif strcmp(optstring, 'n') 
+	errorbar(plot_freq, real(lognpp), real(lognpperr), 'r--');
+    y_label = 'log_{10}(\eta'''') [Pa s]';
+    leg = npp_label;
+elseif strcmp(optstring, 'Gg')
+    hold on;
+	errorbar(plot_freq, real(loggp), real(loggperr), 'b');
+	errorbar(plot_freq, real(loggpp), real(loggpperr), 'b--');
+    hold off;
+    y_label = 'log_{10}(G'',G'''') [Pa]';
+    leg = {gp_label, gpp_label};
+elseif strcmp(optstring, 'GgN')
+    hold on;
+	errorbar(plot_freq, real(loggp), real(loggperr), 'b');
+	errorbar(plot_freq, real(loggpp), real(loggpperr), 'b--');
+	errorbar(plot_freq, real(lognp), real(lognperr), 'r');
+    hold off;
+    y_label = 'log_{10}(G'',G'''') [Pa], log_{10}(\eta'')';
+    leg = {gp_label, gpp_label, np_label};
+elseif strcmp(optstring, 'gn')
+    hold on;
+	errorbar(plot_freq, real(loggpp), real(loggpperr), 'b--');
+	errorbar(plot_freq, real(lognpp), real(lognpperr), 'r--');
+    hold off;
+    y_label = 'log_{10}(G'''') [Pa], log_{10}(\eta'''')';
+    leg = {gpp_label, npp_label};
+elseif strcmp(optstring, 'Nn')
+    hold on;
+	errorbar(plot_freq, real(lognp), real(lognperr), 'r');
+	errorbar(plot_freq, real(lognpp), real(lognpperr), 'r--');
+    hold off;
+    y_label = 'log_{10}(\eta'',\eta'''') [Pa s]';
+    leg = {np_label, npp_label};
+elseif strcmp(optstring, 'GN')
+    hold on;
+	errorbar(plot_freq, real(loggp), real(loggperr), 'b');
+	errorbar(plot_freq, real(lognp), real(lognperr), 'r');
+    hold off;
+    y_label = 'log_{10}(G'') [Pa], log_{10}(\eta'') [Pa s]';
+    leg = {gp_label, np_label};
+elseif strcmp(optstring, 'GgNn')
+    hold on;
+plot(plot_freq,real(loggp),'b')
+plot(plot_freq,real(loggpp),'b--')
+plot(plot_freq,real(lognp),'r')
+plot(plot_freq,real(lognpp),'r--')
+    hold off;
+    y_label = 'log_{10}(G'',G'''') [Pa], log_{10}(\eta'',\eta'''') [Pa s]';
+    leg = {gp_label, gpp_label, np_label, npp_label};
+else
+    error('No code for printing that one.');
+end
+
+
+  xlabel(freq_label);
+  ylabel(y_label);
+  h = legend(leg, 0);
+  grid on;
+  pretty_plot;
+% % 	
+% % 	figure;
+% %     hold on;
+% % 	errorbar(plot_freq, real(mean_lognp), real(ste_lognp), 'b');
+% % 	errorbar(plot_freq, real(mean_lognpp), real(ste_lognpp), 'r');
+% %     hold off;
+% % 	xlabel(freq_label);
+% % 	ylabel('log_{10}(\eta'',\eta'''') [Pa sec]');
+% % 	legend(np_label, npp_label, 0);
+% % 	grid on;
+% % 	pretty_plot;
+% %     
+    
