6f26c47e1285236f8f4199d0a5d9e8e7c24c2b52
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-08-10 13:59:04 +0000

    2011-08-09  Jeremy Cribb <jcribb@email.unc.edu> 	* save_msdfile.m:  added switch for ignoring squared-displacements, if desired.

--- specific/rheology/save_msdfile.m
+++ specific/rheology/save_msdfile.m
@@ -1,7 +1,12 @@
-function outfile = save_msdfile(filename, data) %#ok<INUSD>
-    
+function outfile = save_msdfile(filename, data, r2switch) 
+
+
     outfile = [filename(1:end-3) 'msd.mat'];
 
+    if strcmp(r2switch, 'n')
+        data = rmfield(data, 'r2out');   %#ok<NASGU>
+    end
+    
     save(outfile, '-STRUCT', 'data');
 
 return;
