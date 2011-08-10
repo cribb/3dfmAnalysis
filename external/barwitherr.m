c7c491eec5cd0e1f9e7a55aa57b842a9d1418db2
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-08-10 16:19:19 +0000

    2011-08-10  Jeremy Cribb <jcribb@email.unc.edu> 	* barwitherr.m: added.  generates bar plots *with* errorbars. 	* cprintf.m: added.  generates color-definable text output to stdout, stderr.

---
+++ external/barwitherr.m
@@ -0,0 +1,98 @@
+%**************************************************************************
+%
+%   This is a simple extension of the bar plot to include error bars.  It
+%   is called in exactly the same way as bar but with an extra input
+%   parameter "errors" passed first.
+%
+%   Parameters:
+%   errors - the errors to be plotted
+%   varargin - parameters as passed to conventional bar plot
+%   See bar and errorbar documentation for more details.
+%
+%   Example:
+%   y = randn(3,4);         % random y values (3 groups of 4 parameters) 
+%   errY = 0.1.*y;          % 10% error
+%   barwitherr(errY, y);    % Plot with errorbars
+%
+%   set(gca,'XTickLabel',{'Group A','Group B','Group C'})
+%   legend('Parameter 1','Parameter 2','Parameter 3','Parameter 4')
+%   ylabel('Y Value')
+%
+%   Note: Ideally used for group plots with non-overlapping bars because it
+%   will always plot in bar centre (so can look odd for over-lapping bars) 
+%   and for stacked plots the errorbars will be at the original y value is 
+%   not the stacked value so again odd appearance as is.
+%
+%   24/02/2011  Created     Martina F. Callaghan
+%
+%**************************************************************************
+
+function barwitherr(errors,varargin)
+
+% Check how the function has been called based on requirements for "bar"
+if nargin < 3
+    % This is the same as calling bar(y)
+    values = varargin{1};
+else
+    % This means extra parameters have been specified
+    if isscalar(varargin{2}) || ischar(varargin{2})
+        % It is a width / property so the y values are still varargin{1}
+        values = varargin{1};
+    else
+        % x-values have been specified so the y values are varargin{2}
+        values = varargin{2};
+    end
+end
+
+% Check that the size of "errors" corresponsds to the size of the y-values:
+if any(size(values) ~= size(errors))
+    error('The values and errors have to be the same length')
+end
+
+[nRows nCols] = size(values);
+handles.bar = bar(varargin{:}); % standard implementation of bar fn
+hold on
+
+if nRows > 1
+    for col = 1:nCols
+        % Extract the x location data needed for the errorbar plots:
+        x = get(get(handles.bar(col),'children'),'xdata');
+        % Use the mean x values to call the standard errorbar fn; the
+        % errorbars will now be centred on each bar:
+        errorbar(mean(x,1),values(:,col),errors(:,col), '.k')
+    end
+else
+    x = get(get(handles.bar,'children'),'xdata');
+    errorbar(mean(x,1),values,errors,'.k')
+end
+
+hold off
+
+
+
+% LICENSE.TXT
+%
+% Copyright (c) 2011, Martina Callaghan
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are 
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright 
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright 
+%       notice, this list of conditions and the following disclaimer in 
+%       the documentation and/or other materials provided with the distribution
+%       
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE.
