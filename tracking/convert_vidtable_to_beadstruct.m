1aeb11308a541da3ef7e3c174193f7c1d8c4180e
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-09-08 17:38:36 +0000

    2011-09-08  Jeremy Cribb <jcribb@email.unc.edu> 	* convert_vidtable_to_beadstruct.m:  added.  converts a load_video_tracking 'table' type output to a bead struct array that other folks find useful (same format as that outputted by "export all beads" functionality in evt_GUI.

---
+++ tracking/convert_vidtable_to_beadstruct.m
@@ -0,0 +1,32 @@
+function beadstruct = convert_vidtable_to_beadstruct(vidtable)
+
+
+video_tracking_constants;
+
+beadID = vidtable(:,ID);
+
+for k = 0:max(beadID)
+
+    idx = find(beadID == k);
+
+    beadstruct(k+1).t      = vidtable(idx,TIME) - min(vidtable(:,TIME));
+    beadstruct(k+1).x      = vidtable(idx,X);
+    beadstruct(k+1).y      = vidtable(idx,Y);
+    beadstruct(k+1).yaw    = vidtable(idx,YAW);
+
+end
+    
+    
+
+%     beadID = handles.table(:,ID);
+% 
+%     for k = 0:max(beadID)
+%     
+%         idx = find(beadID == k);
+% 
+%         bead(k+1).t      = handles.table(idx,TIME) - min(handles.table(:,TIME));
+%         bead(k+1).x      = handles.table(idx,X);
+%         bead(k+1).y      = handles.table(idx,Y);
+%         bead(k+1).yaw    = handles.table(idx,YAW);
+%         
+%     end
