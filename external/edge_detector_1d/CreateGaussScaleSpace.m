7e0b5937f56ce734de4c933476d3b76ca6455149
Nanomanipulator Work <nanowork@cs.unc.edu>
2011-09-27 21:04:58 +0000

    2011-09-27  Jeremy Cribb <jcribb@email.unc.edu> 	* edge_detector_1d:  fixed whitespace issues.

--- external/edge_detector_1d/CreateGaussScaleSpace.m
+++ external/edge_detector_1d/CreateGaussScaleSpace.m
@@ -14,24 +14,27 @@ function [ space ] = CreateGaussScaleSpace( data, deriv, scales )
 if (nargin < 3)
     scales = 1:20;
 end
+
 if (nargin < 2)
     deriv = 0;
 end
 
 for i = 1:length(scales)
     scale = scales(i);
-    
+
     % Find the gaussian kernel, convolve
     g = GaussianKernel1D(scale, deriv);
-    
+ 
     % we have to pad the data to avoid the derivative blowing up at the
     % boundaries
     padData = [data(1)*ones(length(g),1); data; data(end)*ones(length(g),1)];
     fData = conv(padData, g);
-    
+
     % Resize the filtered data
     offset = (length(fData) - length(data)) / 2;
     fData = fData(offset:offset+length(data)-1);
     space(:,i) = fData;
+
 end;
+
     
