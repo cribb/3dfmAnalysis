cbf6580f67e95ab3873b56ff120de98c57698bcf
Jeremy Cribb <jcribb@email.unc.edu>
2013-09-15 14:15:58 -0400

    - now provides conversion factors (um/pixel) for the 12 cameras in Panoptes ONLY for MCU parameters equal to zero.

--- panoptes/pan_MCU2um.m
+++ panoptes/pan_MCU2um.m
@@ -1,4 +1,4 @@
-function calibum = pan_MCU2um(MCUparam)
+function [calibum, calibum_err] = pan_MCU2um(MCUparam, systemid, wellid)
 % PAN_MCU2UM  Converts Panoptes's MCU parameter to microns per pixel
 %
 
@@ -6,17 +6,45 @@ function calibum = pan_MCU2um(MCUparam)
 %   calibum = (0.0009192384 * MCUparam + 0.1338995737);     % (range from 31 to 76) (062011 by Jeremy)
 %    calibum = (0.0012402498 * MCUparam + 0.1089275278);       % (range from 46 to 96) (091911 by Jeremy)
    
-   calibum = (2.292106704e-6 * MCUparam + 0.1105463685);     % (range from 21000 to 60000) (112812 by Jeremy)
-   idx = find(MCUparam < 21000);
-   if ~isempty(idx)
-       calibum(idx) = 0.157; % um/pixel 
-   end
-   
-   idx = find(MCUparam > 60000);
-   if ~isempty(idx)
-       logentry('The input MCU parameter is out of range.  Setting to NaN');
-       calibum(idx) = NaN;
-   end
+    if length(MCUparam) ~= length(wellid)
+        error('Mismatch in number of MCU values and length of wells listed.');
+    end
+    
+    if strcmp(systemid, 'panoptes')
+        channelid = pan_get_channel_id(wellid);
+    elseif strcmp(systemid, 'monoptes')
+        channelid = 0;
+    else
+        error('Unknown system id, or none defined');
+    end
+    
+%     channelid = 0;
+
+    if channelid == 0     % case for monoptes
+           calibum = (2.292106704e-6 * MCUparam + 0.1105463685);     % (range from 21000 to 60000) (112812 by Jeremy)
+           idx = find(MCUparam < 21000);
+           if ~isempty(idx)
+               calibum(idx) = 0.157; % um/pixel 
+           end
+
+           idx = find(MCUparam > 60000);
+           if ~isempty(idx)
+               logentry('The input MCU parameter is out of range.  Setting to NaN');
+               calibum(idx) = NaN;
+           end
+
+           calibum_err = NaN; % don't know this error
+    elseif channelid > 0 % case for panoptes
+
+        % calibrations for Panoptes channels 1-12 given an MCU parameter of zero.
+        MCU_zero_calibum = [0.151447, 0.149707, 0.148812, 0.148333, 0.149083, 0.147706, 0.148374, 0.149008, 0.149312, 0.149215, 0.149227, 0.144805];
+        MCU_zero_calibum_error = [0.00179751, 0.00373752, 0.00371864, 0.00218959, 0.00180049, 0.00124988, 0.00339845, 0.00153736, 0.00232281, 0.00248024, 0.000841523, 0.00206237];
+        
+        calibum = MCU_zero_calibum( channelid );
+        calibum_err = MCU_zero_calibum_error( channelid );
+
+    end
+    
     
 %     
 %     for k = 1:length(MCUparam)
