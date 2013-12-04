173d3f479990808f213edd60cd5eecbff0cb13b1
Jeremy Cribb <jcribb@email.unc.edu>
2013-12-04 13:04:14 -0500

    Moves TA Instruments data files into unique directories for unhindered processing.

---
+++ specific/rheology/cone_and_plate/TA_naming_script.m
@@ -0,0 +1,11 @@
+fl = dir('*.rsl');
+
+fprintf('handling %i file(s).\n', length(fl));
+
+for k = 1:length(fl)
+    
+    myname = strrep(fl(k).name, '.rsl', '');
+    mkdir(myname);
+    movefile([myname '.rsl'], [myname '/' myname '.rsl']);
+    movefile([myname ' exp.txt'], [myname '/' myname ' exp.txt']);
+end
