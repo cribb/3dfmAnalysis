2c639688631383f280325edb1353137566851664
Jeremy Cribb <jcribb@email.unc.edu>
2014-05-23 16:01:02 -0400

    Script adjusted for operating on a different data set. Soon this will be an input rather than a 'magic number'.

--- specific/rheology/cone_and_plate/run_tareport_on_all_files.m
+++ specific/rheology/cone_and_plate/run_tareport_on_all_files.m
@@ -1,6 +1,8 @@
-rootdir = 'K:\data\material\mucus_HBE\2013.11.12__CAP_HBE_mucus_5pct';
+rootdir = 'K:\data\material\mucus_HBE\2014.03.06__CAP_HBE_mucus_3p6pct';
+cd(rootdir);
 
-rsl_file_list = 
+rsl_file_list = importdata([rootdir '\rsl_file_list.txt']);
+
 for k = 1:length(rsl_file_list)
     cd(rootdir);
     
@@ -8,7 +10,7 @@ for k = 1:length(rsl_file_list)
     
     cd(pathstr);
     
-    cap = tareport([filename ' exp.txt'], '5% HBE mucus');
+    cap = tareport([filename ' exp.txt'], '3.6% HBE mucus');
     
 end
 
