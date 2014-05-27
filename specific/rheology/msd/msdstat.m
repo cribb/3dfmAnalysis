e3af7cbb351e782249bb0b28dfc23918734ed3ab
Jeremy Cribb <jcribb@email.unc.edu>
2014-05-27 17:02:44 -0400

    Added rms-displacement as fields in the msd structure.

--- specific/rheology/msd/msdstat.m
+++ specific/rheology/msd/msdstat.m
@@ -1,37 +1,48 @@
 function msdout = msdstat(msdin)
 
-if nargin < 1 || isempty(msdin)
+if nargin < 1 || isempty(msdin) || ~isfield(msdin, 'tau') || isempty(msdin.tau)
+    
+    if exist(msdin)
+        msdout = msdin;
+    end
+    
     msdout.logtau = NaN;
     msdout.logmsd = NaN;
     msdout.mean_logtau = NaN;
     msdout.mean_logmsd = NaN;
     msdout.msderr = NaN;
     
-    return;
-end
-
-if isempty(msdin.tau)
     logentry('Error:  No data to perform statistics on.  Exiting now.');
     
-    msdout = msdin;
-    msdout.logtau = NaN(size(msdin.window));
-    msdout.logmsd = NaN(size(msdin.window));
-    msdout.mean_logtau = NaN(size(msdin.window));
-    msdout.mean_logmsd = NaN(size(msdin.window));
-    msdout.msderr = NaN(size(msdin.window));
-    
     return;
 end
 
-% renaming input structure
+% if isempty(msdin.tau)
+%     
+%     
+%     msdout = msdin;
+%     msdout.logtau = NaN(size(msdin.window));
+%     msdout.logmsd = NaN(size(msdin.window));
+%     msdout.mean_logtau = NaN(size(msdin.window));
+%     msdout.mean_logmsd = NaN(size(msdin.window));
+%     msdout.msderr = NaN(size(msdin.window));
+%     
+%     return;
+% end
+
+% renaming input structure. the 'ns' field refers to the number of
+% estimates (differences) of MSD that exist within a given timescale. The 
+% 'n' field refers to the number of trackers available within that particular
+% timescale.
 tau    = msdin.tau;
-msd    = msdin.msd;
+mymsd  = msdin.msd;
 counts = msdin.ns;
 
+% compatibility issues with old msd.m
 if isfield(msdin, 'n');
     sample_count = msdin.n;
 else
-    sample_count = sum(~isnan(msd),2);
+    sample_count = sum(~isnan(mymsd),2);
     idx = find(sample_count > 0);
     sample_count = sample_count(idx);
     msdin.n = sample_count;
@@ -39,27 +50,37 @@ end
 
 clip = find(sum(~isnan(counts),2) <= 1);
 tau(clip,:) = [];
-msd(clip,:) = [];
+mymsd(clip,:) = [];
 counts(clip,:) = [];
 sample_count(clip,:) = [];
 
-if isempty(tau) && isempty(msd) && isempty(counts) && isempty(sample_count)
+% did we delete out everything? if so, return empty set
+if isempty(tau) && isempty(mymsd) && isempty(counts) && isempty(sample_count)
     msdout = msdin;
     msdout.logtau = NaN(size(msdin.window));
     msdout.logmsd = NaN(size(msdin.window));
     msdout.mean_logtau = NaN(size(msdin.window));
     msdout.mean_logmsd = NaN(size(msdin.window));
     msdout.msderr = NaN(size(msdin.window));
+    msdout.rmsdisp = NaN(size(msdin.window));
+    msdout.logrmsdisp = NaN(size(msdin.window));
+    msdout.mean_logrmsdisp = NaN(size(msdin.window));
+    msdout.rmsdisp_err = NaN(size(msdin.window));    
     return;
 end
 
 
+
+
+% compute rmsdisplacement 
+rmsdisp = sqrt(mymsd);
+
 % setting up axis transforms for the figure plotted below.  You cannot plot
 % errorbars on a loglog plot, it seems, so we have to set it up here.
-logtau = log10(tau);
-logmsd = log10(msd);
-
-numbeads = size(logmsd,2);
+logtau     = log10(tau);
+logmsd     = log10(mymsd);
+logrmsdisp = log10(rmsdisp);
+numbeads   = size(logmsd,2);
 
 % weighted mean for logmsd
 baseline = repmat(nansum(counts,2), 1, size(counts,2));
@@ -67,14 +88,22 @@ weights = counts ./ baseline;
 
 mean_logtau = nanmean(logtau,2);
 mean_logmsd = nansum(weights .* logmsd, 2);
+mean_logrmsdisp = nansum(weights .* logrmsdisp, 2);
 
 % if the mean_logmsd is exactly equal to zero, then assume that every entry
 % in that row for logmsd (computed just above) was zero and the 'nansum' was then zero
 mean_logmsd( mean_logmsd == 0) = NaN;
+mean_logrmsdisp( mean_logrmsdisp == 0) = NaN;
 
 % computing error for logmsd
 Vishmat = nansum(weights .* (repmat(mean_logmsd, 1, numbeads) - logmsd).^2, 2);
 msderr =  sqrt(Vishmat ./ sample_count);
+Vishmat_rmsdisp = nansum(weights .* (repmat(mean_logrmsdisp, 1, numbeads) - logrmsdisp).^2, 2);
+rmsdisp_err = sqrt(Vishmat_rmsdisp ./ sample_count);
+
+%d.logrmsdisp = d.logmsd;
+%d.logrmsdisp = sqrt(10.^mymsd);
+%rmsdisp_err = sqrt(10.^(mymsd+mymsd_err)) - rmsdisp;\
 
 % generating output structure
 msdout = msdin;
@@ -104,20 +133,31 @@ else
     msdout.msderr = msderr;
 end
 
+if isempty(rmsdisp)
+    msdout.rmsdisp = NaN(size(rmsdisp,1),1);
+else    
+    msdout.rmsdisp = rmsdisp;
+end
+
+if isempty(logrmsdisp)
+    msdout.logrmsdisp = NaN(size(logrmsdisp,1),1);
+else    
+    msdout.logrmsdisp = logrmsdisp;
+end
+
+if isempty(mean_logrmsdisp)
+    msdout.mean_logrmsdisp = NaN(size(mean_logrmsdisp,1),1);
+else    
+    msdout.mean_logrmsdisp = mean_logrmsdisp;
+end
+    
+if isempty(rmsdisp_err)
+    msdout.rmsdisp_err = NaN(size(rmsdisp_err,1),1);
+else    
+    msdout.rmsdisp_err = rmsdisp_err;
+end
+
 return;
 
 
-% function for writing out stderr log messages
-function logentry(txt)
-    logtime = clock;
-    logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
-                   num2str(logtime(2),        '%02i') '.' ...
-                   num2str(logtime(3),        '%02i') ', ' ...
-                   num2str(logtime(4),        '%02i') ':' ...
-                   num2str(logtime(5),        '%02i') ':' ...
-                   num2str(floor(logtime(6)), '%02i') ') '];
-     headertext = [logtimetext 'msdstat: '];
-     
-     fprintf('%s%s\n', headertext, txt);
-     
-     return;
+
