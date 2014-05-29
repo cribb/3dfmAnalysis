845a7ab826e0b23daf58095b6aca483efde81162
Jeremy Cribb <jcribb@email.unc.edu>
2014-05-29 11:08:54 -0400

    rotates vector from the camera orientation to ludl stage orientation

---
+++ panoptes/pan_rot2stage.m
@@ -0,0 +1,51 @@
+function newxy = pan_rot2stage(xy, wellid, theta)
+% PAN_ROT2STAGE rotates a vector to ludl stage orientation
+%
+% Panoptes function 
+% 
+% This function is used by the Panoptes analysis code to rotate a velocity
+% vector inplace to coordinate directionality from the camera frame to the
+% stage frame. The lower left corner of the stage (in the vicinity of
+% optics channel 7) is considered the origin for both the X and Y
+% coordinates.
+% 
+% newxy = pan_rot2stage(xy, wellid, theta) 
+%
+% where "newxy" is the 
+%       "xy" is the 
+%       "wellid" is the
+%       "theta" is the
+%
+% Notes:
+% - This function is designed to work within the PanopticNerve software
+% chain but can also be used manually from the matlab command line interface.
+%
+
+% First, set up default values for input parameters in case they are empty 
+% or otherwise do not exist.
+
+    if ~exist('theta') || isempty(theta)
+        theta = 90;
+    end
+
+    if ~exist('wellid') || isempty(wellid)
+        error('No well ID defined.');
+    end 
+
+    if ~exist(xy) || isempty(xy)
+        error('No input coords defined');
+    end
+
+    channelid = pan_get_channel_id(wellid);
+
+    % do the initial rotation
+    R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+    rotxy = R * xy;
+
+    if mod(channelid,2) %even
+        newxy = [-rotxy(:,1)  rotxy(:,2)];
+    else % odd
+        newxy = [ rotxy(:,1) -rotxy(:,2)];    
+    end
+
+return;
