c4a16515510b748a0376a087a59fbfe7659ce980
Jeremy Cribb <jcribb@email.unc.edu>
2015-06-19 13:32:15 -0400

    Added condition for no well or pass data within file name.

--- panoptes/pan_wellpass.m
+++ panoptes/pan_wellpass.m
@@ -11,14 +11,17 @@ function [mywell, mypass] = pan_wellpass(filenamein)
 
     wchunk  = regexp(filenamein, '_well[0-9]*', 'match');
     mywell = regexp(wchunk, '[0-9]*', 'match');
-    mywell = str2double(mywell{:}); 
+    if isempty(mywell)
+        mywell = {'0'};
+    end
     
     pchunk = regexp(filenamein, '_pass[0-9]*', 'match');
     mypass = regexp(pchunk, '[0-9]*', 'match');
     if isempty(mypass)
-        mypass = {'-1'};
+        mypass = {'0'};
     end
     
+    mywell = str2double(mywell{:}); 
     mypass = str2double(mypass{:});
        
   return;
