d5faad79a5dba81c105b3cb214ac3204dcacc6b8
Jeremy Cribb <jcribb@email.unc.edu>
2015-07-13 16:32:24 -0400

    Code cleanup.

--- specific/rheology/msd/plot_alphadist.m
+++ specific/rheology/msd/plot_alphadist.m
@@ -1,4 +1,4 @@
-function h = plot_alphadist(d, h, optstring)
+function h = plot_alphadist(alpha, h)
 % PLOT_ALPHAVSTAU plots the graph of slope (alpha) of mean square displacement values versus tau for an aggregate number of beads 
 %
 % 3DFM function
@@ -12,10 +12,7 @@ function h = plot_alphadist(d, h, optstring)
 %       value (window size), and the number of beads in a given window.
 %       "h" is the figure handle in which to put the plot.  If h is not
 %       used, a new figure is generated.
-%      "optstring" is a string containing 'a' to plot all individual paths, 'm'
-%      to plot the mean msd function, and 'e' to include errorbars on mean,
-%      'u' to plot msd in units of um^2
-%
+
 
 
 % % % 
@@ -33,7 +30,10 @@ function h = plot_alphadist(d, h, optstring)
 % % % % calculate statistical measures for msd and plant into data structure
 % % % d = msdstat(d);
 
-alpha = d;
+if nargin < 2 || isempty(h);
+    h = figure;
+end
+
 
 % creating the plot
 figure(h);
@@ -53,18 +53,4 @@ refresh(h);
 
 return;
 
-
-% function for writing out stderr log messages
-function logentry(txt)
-    logtime = clock;
-    logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
-                   num2str(logtime(2),        '%02i') '.' ...
-                   num2str(logtime(3),        '%02i') ', ' ...
-                   num2str(logtime(4),        '%02i') ':' ...
-                   num2str(logtime(5),        '%02i') ':' ...
-                   num2str(floor(logtime(6)), '%02i') ') '];
-     headertext = [logtimetext 'plot_msd: '];
-     
-     fprintf('%s%s\n', headertext, txt);
-     
-     return;    
+ 
