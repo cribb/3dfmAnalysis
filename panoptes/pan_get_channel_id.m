7dd52727d3b51b11d14e071e0fbb02c6e0226a17
Jeremy Cribb <jcribb@email.unc.edu>
2015-08-18 14:59:38 -0400

    Added support for multiple dimensioned inputs (i.e. now also works for 8x12 platemap representaions)

--- panoptes/pan_get_channel_id.m
+++ panoptes/pan_get_channel_id.m
@@ -1,37 +1,39 @@
 function channelid = pan_get_channel_id(wellid)
 % PAN_GET_CHANNEL_ID Extracts the optical channel used to measure the bead trajectories
 
-N = length(wellid);
+N = size(wellid);
 
-channelid = NaN(1,N);
+channelid = NaN(N);
 
-for k = 1:N
+for k = 1:prod(N)
     switch wellid(k)
         case {1,2,13,14,25,26,37,38}
-            channelid(1,k) = 1;
+            channelid(k) = 1;
         case {3,4,15,16,27,28,39,40}
-            channelid(1,k) = 2;
+            channelid(k) = 2;
         case {5,6,17,18,29,30,41,42}
-            channelid(1,k) = 3;
+            channelid(k) = 3;
         case {7,8,19,20,31,32,43,44}
-            channelid(1,k) = 4;
+            channelid(k) = 4;
         case {9,10,21,22,33,34,45,46}
-            channelid(1,k) = 5;
+            channelid(k) = 5;
         case {11,12,23,24,35,36,47,48}
-            channelid(1,k) = 6;
+            channelid(k) = 6;
         case {49,50,61,62,73,74,85,86}
-            channelid(1,k) = 7;
+            channelid(k) = 7;
         case {51,52,63,64,75,76,87,88}
-            channelid(1,k) = 8;
+            channelid(k) = 8;
         case {53,54,65,66,77,78,89,90}
-            channelid(1,k) = 9;
+            channelid(k) = 9;
         case {55,56,67,68,79,80,91,92}
-            channelid(1,k) = 10;
+            channelid(k) = 10;
         case {57,58,69,70,81,82,93,94}
-            channelid(1,k) = 11;
+            channelid(k) = 11;
         case {59,60,71,72,83,84,95,96}
-            channelid(1,k) = 12;
+            channelid(k) = 12;
         otherwise
-            channelid(1,k) = 0;
+            channelid(k) = NaN;
     end
 end
+
+return;
