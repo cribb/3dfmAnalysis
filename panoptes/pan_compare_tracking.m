ad1d4ff3231abaee45961073b5d72d7c7593bb54
Jeremy Cribb <jcribb@email.unc.edu>
2015-08-18 14:50:24 -0400

    Updated to check for mismatches in ID/dist.

--- panoptes/pan_compare_tracking.m
+++ panoptes/pan_compare_tracking.m
@@ -23,25 +23,41 @@ MEANDIFFY = 11;
 STDDIFFX = 12;
 STDDIFFY = 13;
 
-filelistA = dir([pathA filesep '*TRACKED' '.' filetype]);
-filelistB = dir([pathB filesep '*TRACKED.vrpn' '.' filetype]);
+
+metadataA = pan_load_metadata(pathA, 'panoptes', '96well');
+metadataB = pan_load_metadata(pathB, 'panoptes', '96well');
+
+fpsA = metadataA.instr.fps_imagingmode;
+fpsB = metadataB.instr.fps_imagingmode;
+
+if fpsA ~= fpsB
+    error('ERROR! Frame rates between the two datasets are not identical!');
+end
+
+filelistA = dir([pathA filesep '*TRACKED.' filetype]);
+% filelistA = dir([pathA filesep '*TRACKED.vrpn.mat']);
+filelistB = dir([pathB filesep '*TRACKED.vrpn.' filetype]);
 
 if length(filelistA) ~= length(filelistB)
     error('the same files do not exist in both folders');
 end
 
-table = [];
+table = ones(0,13);
 
 for k = 1:length(filelistA)
 
     fileA = [pathA filesep filelistA(k).name];
     fileB = [pathB filesep filelistB(k).name];
     
-    ct = compare_tracking(fileA, fileB, 1, 0.1, filteryn);
+    id_error_thresh = 1; % pixel distance between Atracker and the closest Btracker
+    xy_error_thresh = 0.1; % on average s
+    
+    ct = compare_tracking(fileA, fileB, id_error_thresh, xy_error_thresh, filteryn);
 
     if strcmpi(filteryn, 'y')
-        %function outfile = save_evtfile(filename, tracking_in, xyzunits, calib_um, fps)
-        save_evtfile(filtered_file, ct.filtered, 'pixels', 1, );
+%         filtered_filename = [filelistB(k).name(1:end-3) 'evt.mat'];
+        filtered_filename = fileB;
+        save_evtfile(filtered_filename, ct.filteredB, 'pixels', 1, fpsA, 'csv');
     end
     
     [mywell, mypass] = pan_wellpass(filelistA(k).name);
@@ -56,10 +72,17 @@ for k = 1:length(filelistA)
         table = [table ; tmp];
     end
     
-    outs.results  = table;
-    outs.mismatch = table( isnan(table(:, MATCHERR_AB)), : );
-    outs.filtered = table(~isnan(table(:, MATCHERR_AB)), : );
-    outs.maxerrXY = max(outs.filtered(:,MAXDIFFX:MAXDIFFY),[],1);
 end
 
+% set up outputs
+failed_matches_NaN = isnan(table(:, MATCHERR_AB));
+failed_matches_ID  = table(:,MATCHERR_AB) >= id_error_thresh;
+
+failed_matches = or(failed_matches_NaN, failed_matches_ID);
+
+outs.results  = table;
+outs.mismatch = table( failed_matches, : );
+outs.filtered = table(~failed_matches, : );
+outs.maxerrXY = max(outs.filtered(:,MAXDIFFX:MAXDIFFY),[],1);
+    
 return;
