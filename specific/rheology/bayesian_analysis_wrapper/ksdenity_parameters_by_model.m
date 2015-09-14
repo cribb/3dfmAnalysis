92acb503ef6e26e3fb76cf2e49ca9f3d2e380569
Jeremy Cribb <jcribb@email.unc.edu>
2015-09-14 10:48:36 -0400

    code cleanup.

--- specific/rheology/bayesian_analysis_wrapper/ksdenity_parameters_by_model.m
+++ specific/rheology/bayesian_analysis_wrapper/ksdenity_parameters_by_model.m
@@ -50,75 +50,64 @@ DAV_V=[];
 
 if N_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,N)>=0.5))
-        idx = outs_bayes.idlist(i)
-% d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         
-        N_N = [N_N outs_bayes.N(i,N)] %records the N-parameter value if this was the 'N' model had probability higher than 50%.
+        idx = outs_bayes.idlist(i); 
+        N_N = [N_N outs_bayes.N(i,N)]; %records the N-parameter value if this was the 'N' model had probability higher than 50%.
     end
 end
 
 if D_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,D)>=0.5))
-        idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(idx, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');         
-        D_D = [D_D outs_bayes.D(i,D)]
+        idx = outs_bayes.idlist(i);
+        D_D = [D_D outs_bayes.D(i,D)];
     end
 end
 
 if V_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,V)>=0.5))
-    	idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         
-         V_V = [V_V outs_bayes.V(i,V)]
+    	idx = outs_bayes.idlist(i);         
+        V_V = [V_V outs_bayes.V(i,V)];
     end
 end
 
 if DV_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,DV)>=0.5))
-        idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         
-         DV_D = [DV_D outs_bayes.D(i,DV)]
-         DV_V = [DV_V outs_bayes.V(i,DV)]
+        idx = outs_bayes.idlist(i);
+         DV_D = [DV_D outs_bayes.D(i,DV)];
+         DV_V = [DV_V outs_bayes.V(i,DV)];
     end
 end
 
 if DR_count/num_trackers> 0.15
      for i = transpose(find(outs_bayes.prob(:,DR)>=0.5))
-         idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         DR_D = [DR_D outs_bayes.D(i,DR)]
-         DR_R = [DR_R outs_bayes.Rc(i,DR)]
+         idx = outs_bayes.idlist(i);
+         DR_D = [DR_D outs_bayes.D(i,DR)];
+         DR_R = [DR_R outs_bayes.Rc(i,DR)];
     end
 end
 
 if DA_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,DA)>=0.5))
-        idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         DA_D = [DA_D outs_bayes.D(i,DA)]
-         DA_A = [DA_A outs_bayes.A(i,DA)]
+        idx = outs_bayes.idlist(i);
+        DA_D = [DA_D outs_bayes.D(i,DA)];
+        DA_A = [DA_A outs_bayes.A(i,DA)];
     end
 end
 
 if DRV_count/num_trackers> 0.15
     for  i = transpose(find(outs_bayes.prob(:,DRV)>=0.5))
-         idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         DRV_D = [DRV_D outs_bayes.D(i,DRV)]
-         DRV_R = [DRV_R outs_bayes.Rc(i,DRV)]
-         DRV_R = [DRV_V outs_bayes.Rc(i,DRV)]
+         idx = outs_bayes.idlist(i);
+         DRV_D = [DRV_D outs_bayes.D(i,DRV)];
+         DRV_R = [DRV_R outs_bayes.Rc(i,DRV)];
+         DRV_R = [DRV_V outs_bayes.Rc(i,DRV)];
     end
 end
 
 if DAV_count/num_trackers> 0.15
     for i = transpose(find(outs_bayes.prob(:,DAV)>=0.5))
-        idx = outs_bayes.idlist(i)
-%      d = load_video_tracking(['single_curve_ID'  num2str(i, '%03u') '_' num2str(filename)], 55, 'm', 0.152, 'absolute', 'no', 'table');
-         DAV_D = [DAV_D outs_bayes.D(i,DAV)]
-         DAV_A = [DAV_A outs_bayes.A(i,DAV)]
-         DAV_V = [DAV_V outs_bayes.V(i,DAV)]
+        idx = outs_bayes.idlist(i);
+        DAV_D = [DAV_D outs_bayes.D(i,DAV)];
+        DAV_A = [DAV_A outs_bayes.A(i,DAV)];
+        DAV_V = [DAV_V outs_bayes.V(i,DAV)];
     end
 end
 
