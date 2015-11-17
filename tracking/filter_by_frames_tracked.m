083fe0779eb40a3abc7e9629d5881450dfb6e030
Alison Schaefer <amschaef@live.unc.edu>
2015-11-17 17:41:12 -0500

    changed frame filter to have a frame number input

--- tracking/filter_by_frames_tracked.m
+++ tracking/filter_by_frames_tracked.m
@@ -1,4 +1,4 @@
-function [data]= filter_by_frames_tracked(filepath)
+function [data]= filter_by_frames_tracked(filepath,numframes)
 
 if isempty(filepath)
     error('No file found, please enter a vide to filter');
@@ -9,7 +9,7 @@ data = load_video_tracking(filepath, [], 'pixels', 1, 'absolute', 'no', 'table')
 
 freqstats=tabulate(data(:,ID));
 for (i=1:length(freqstats(:,2)))
-    if(freqstats(i,2)<5)
+    if(freqstats(i,2)<numframes)
      TF1= data(:,ID)==freqstats(i);
      data(TF1,:)=[];
     end
