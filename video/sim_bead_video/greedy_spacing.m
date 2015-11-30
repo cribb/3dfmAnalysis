7323d5e9197d6df6530f1f5938917be3723bd053
Phoebe Lee <ptlee@live.unc.edu>
2015-11-30 13:48:07 -0500

    updated to work with odd numbers

--- video/sim_bead_video/greedy_spacing.m
+++ video/sim_bead_video/greedy_spacing.m
@@ -1,6 +1,17 @@
 function [allframes] = greedy_spacing(numpaths,frame_width,frame_height,numframes)
 
 %if isprime???
+if numpaths == 1
+
+    x = frame_width/2;
+    y = frame_height/2;
+    
+    [xcoords,ycoords] = meshgrid(x,y);
+    pairs = [xcoords(:) ycoords(:)];
+
+    allframes = repmat(pairs,1,1,numframes);
+    allframes = permute(allframes,[3 2 1]);
+return;
 
 if mod(numpaths,2) == 0
     numcolumns = numpaths;
@@ -32,14 +43,14 @@ x_spacing = frame_width/numcolumns;
 y_spacing = frame_height/numrows;
 
 x = zeros(numcolumns,1);
-y = zeros(numrows,1);
-
 for xpoints = 1:numcolumns
     x(xpoints) = (x_spacing/2) + (x_spacing*(xpoints-1));
 end
 
+
+y = zeros(numrows,1);
 for ypoints = 1:numrows
-    y(ypoints) = (y_spacing/2) + (y_spacing*(ypoints-1));
+    y(ypoints) = (y_spacing/2) + (y_spacing*(ypoints-1));  
 end
 
 [xcoords,ycoords] = meshgrid(x,y);
