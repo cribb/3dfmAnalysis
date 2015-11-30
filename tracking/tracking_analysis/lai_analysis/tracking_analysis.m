23929871c1aa30e3ecf703f6ef8bdfb53d16938b
Phoebe Lee <ptlee@live.unc.edu>
2015-11-30 13:47:43 -0500

    updated to include more information

--- tracking/tracking_analysis/lai_analysis/tracking_analysis.m
+++ tracking/tracking_analysis/lai_analysis/tracking_analysis.m
@@ -8,33 +8,39 @@ matched_traj = zeros(numframes,1);
 track_eff = zeros(numframes,1);
 extraA = zeros(numframes,1);
 extraB = zeros(numframes,1);
+fraction_untracked = zeros(numframes,1);
 artifactor = zeros(numframes,1);
 errorx = zeros(numframes,1);
 errory = zeros(numframes,1);
+matched_traj_overall = 0;
+total_traj_man_overall = 0;
 
-for fr = 1:numframes+1
+for fr = 1:numframes %took away +1
     matched_traj_fr = 0;
     extraA_fr = 0;
     extraB_fr = 0;
     for n = 1:length(data)
-        if data(n,1) == fr-1 && ~isnan(data(n,2)) && ~isnan(data(n,3))
+        if data(n,1) == fr && ~isnan(data(n,2)) && ~isnan(data(n,3)) %changed fr-1 to fr for these statements
             matched_traj_fr = matched_traj_fr + 1;
-        elseif data(n,1) == fr-1 && isnan(data(n,3)) %if B has no matching location, there is an extra one in A
+        elseif data(n,1) == fr && isnan(data(n,3)) %if B has no matching location, there is an extra one in A
             extraA_fr = extraA_fr + 1;
-        elseif data(n,1) == fr-1 && isnan(data(n,2)) %if A has no matching location, there is an extra one in B
+        elseif data(n,1) == fr && isnan(data(n,2)) %if A has no matching location, there is an extra one in B
             extraB_fr = extraB_fr + 1; 
         end
     end
     
     matched_traj(fr) = matched_traj_fr;
+    matched_traj_overall = matched_traj_overall + matched_traj_fr;
     extraA(fr) = extraA_fr;
     extraB(fr) = extraB_fr; 
     total_traj_man_fr = matched_traj_fr + extraA_fr; 
+    total_traj_man_overall = total_traj_man_overall + total_traj_man_fr;
     
     track_eff(fr) = matched_traj_fr/total_traj_man_fr;
+    fraction_untracked(fr) = extraA_fr/total_traj_man_fr;
     artifactor(fr) = extraB_fr/total_traj_man_fr;
     
-    thesepoints = data(:,1) == fr-1;
+    thesepoints = data(:,1) == fr;
     errorx_vector = data(thesepoints,12);
     errorx_vector = errorx_vector(~isnan(errorx_vector));
     errory_vector = data(thesepoints,13);
@@ -73,10 +79,14 @@ end
 
 track.track_eff = track_eff;
 track.avg_track_eff = mean(track_eff);
+track.overall_track_eff = matched_traj_overall/total_traj_man_overall;
 track.extraA = extraA;
 track.extraAtotal = sum(extraA(:));
 track.extraB = extraB;
 track.extraBtotal = sum(extraB(:));
+track.fraction_untracked = fraction_untracked;
+track.avg_untracked = mean(fraction_untracked);
+track.overall_untracked = (total_traj_man_overall - matched_traj_overall)/total_traj_man_overall;
 track.artifactor = artifactor;
 track.avg_artifactor = mean(artifactor);
 track.errorx = errorx;
