d21f71af346f96a61796856654e68a6c16b1c79f
Jeremy Cribb <cribb@cs.unc.edu>
2016-11-04 16:27:15 -0400

    Fixed typo in documentation, updated for new sample.

--- specific/rheology/cone_and_plate/run_tareport_on_all_files.m
+++ specific/rheology/cone_and_plate/run_tareport_on_all_files.m
@@ -1,4 +1,4 @@
-rootdir = 'K:\data\material\mucus_HBE\2015.04.08__CAP_HBE_mucus_2pct_EBFDAC';
+rootdir = 'K:\data\material\PEO\2016.07__PEO_Study\2016.07.18__CAP_PEO_5MD_0p91pct_dH2O';
 cd(rootdir);
 
 rsl_file_list = importdata([rootdir '\rsl_file_list.txt']);
@@ -9,11 +9,11 @@ for k = 1:length(rsl_file_list)
     %[pathstr filename ext vrsn] = fileparts(rsl_file_list{k});
     
     %File parts doesn't output a vrsn number so I got an error. vrsn is not
-    %used so i droped it. RJ 2015.02.25
+    %used so i dropped it. RJ 2015.02.25
     [pathstr filename ext] = fileparts(rsl_file_list{k});
     cd(pathstr);
     
-    cap = tareport([filename ' exp.txt'], '2% HBE mucus Spec-EBFDAC');
+    cap = tareport([filename ' exp.txt'], 'PEO 5MD 0.91%');
     
 end
 
