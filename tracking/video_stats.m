4e387dd8570a5966bc953461e18567926ed7a774
Jeremy Cribb <cribb@cs.unc.edu>
2017-08-09 11:41:01 -0400

    cleaned up the code. removed useless variable names, etc.

--- tracking/video_stats.m
+++ tracking/video_stats.m
@@ -1,79 +1,76 @@
-function [stats_table,num_pix] = video_stats(files)
+function outs = video_stats(files, outstyle, report)
 % must be in the directory of the video you want to analyze 
 % files is what all the filenames of the frames have in common, must be a string
+if nargin < 3 || isempty(report)
+    report = 'no';
+end
 
-list = dir([files '*']);
+if nargin < 2 || isempty(outstyle)
+    outstyle = 'dataset';
+end
+
+list = dir(files);
 firstframe = imread(list(1).name);
 
-bits = bitdepth(list(1).name);
+bits = class(firstframe);
 num_frames = length(list);
 
 
-if bits == 8
+if isa(firstframe, 'uint8');
     maximum_I = 255;
     logentry('8-bit images.');
-elseif bits == 12
+elseif isa(firstframe, 'uint12')
     maximum_I = 4095;
     logentry('12-bit images.');
-elseif bits == 16
+elseif isa(firstframe, 'uint16')
     maximum_I = 65535;
     logentry('16-bit images.');
+else
+    error('unknown file type');
 end
 
 
-stats = zeros(num_frames,6);
-num_pix = zeros(1,num_frames);
+stats = zeros(num_frames,7);
 
 for i = 1:num_frames
     frame = imread(list(i).name);
     
-    mean_I = mean(frame(:));
-    stats(i,1) = mean_I;
-    
-    median_I = median(frame(:));
-    stats(i,2) = median_I;
-    
-    mode_I = mode(frame(:));
-    stats(i,3) = mode_I;
-    
-    stdev_I = std(double(frame(:)));
-    stats(i,4) = stdev_I;
-    
-    max_I_frame = max(frame(:));
-    stats(i,5) = max_I_frame;
-    
-    min_I = min(frame(:));
-    stats(i,6) = min_I;
-    
-    bleached = (frame == maximum_I);
-    count = sum(sum(bleached));
-    num_pix(i) = count;
-end
+    stats(i,1) = mean(frame(:));
+    stats(i,2) = median(frame(:));
+    stats(i,3) = mode(frame(:));
+    stats(i,4) = std(double(frame(:)));
+    stats(i,5) = max(frame(:));
+    stats(i,6) = min(frame(:));
+    stats(i,7) = sum(sum(frame == maximum_I));
 
-if count == 0
-    logentry('No photobleaching occurs.');
 end
 
 stats_table = mat2dataset(stats);
-stats_table.Properties.VarNames = {'Mean' 'Median' 'Mode' 'Standard_Deviation' 'Maximum' 'Minimum'};
+stats_table.Properties.VarNames = {'Mean' 'Median' 'Mode' 'Standard_Deviation' 'Maximum' 'Minimum' 'Bleach_Count'};
+stats_struct = dataset2struct(stats_table, 'AsScalar', true);
 
-folder = pwd;
-[path, currentfolder, ~] = fileparts(folder);
+[~, currentfolder, ~] = fileparts(pwd);
 
-figure;
-plot(stats);
-title(['Stats for ' currentfolder]);
-xlim([0 num_frames]);
-xlabel('Frame Number');
-legend('Mean','Median','Mode','Standard Deviation','Maximum','Minimum');
+if strcmp(report, 'y')
+    figure;
+    plot(stats);
+    title(['Stats for ' currentfolder], 'Interpreter', 'none');
+    xlim([0 num_frames]);
+    xlabel('Frame Number');
+    legend('Mean','Median','Mode','Standard Deviation','Maximum','Minimum');
+    grid on;
+end
 
-figure;
-plot(num_pix);
-title(['Overexposed pixels in ' currentfolder]);
-xlabel('Frame Number');
-ylabel('Number of Overexposed Pixels');
-axis([0 num_frames 0 (max(num_pix)+10)]);
+if sum(stats_table.Bleach_Count) == 0
+    logentry('No photobleaching occurs.');
+end
 
 logentry('Finished analysis.')
+
+if strcmp(outstyle, 'struct')
+    outs = stats_struct;
+else
+    outs = stats_table;
+end
 
 end
