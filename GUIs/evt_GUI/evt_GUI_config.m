bb7604d664534dcd95e38489b10520d3f27dd1b9
Jeremy Cribb <cribb@cs.unc.edu>
2017-11-13 14:11:48 -0500

    Beginning of refactoring evt_GUI. THIS DOES NOT YET WORK and needs to be worked on any further on a branch.

---
+++ GUIs/evt_GUI/evt_GUI_config.m
@@ -0,0 +1,22 @@
+% set default sizes and locations
+imageX = 648 % [px]
+imageY = 484 % [px]
+plotXY = [posx posy width height];
+plotXT = [posx posy width height];
+plotAUX = [posx posy width height];
+plotXYlock = yes
+plotXTlock = yes
+plotAUXlock = yes
+
+
+% set default values for UI controls
+fps = 120 % [frames/sec]
+auxplot = none
+drift = common
+calibum = 0.157  % [um/px]
+minframes = off, 120
+min_pixel = off, 0
+max_pixel = off, 0
+tcrop = off, 0
+xycrop = off, 0
+
