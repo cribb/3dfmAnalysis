0fc989f0a5a9440e35fd72f51da8250b8e8b337c
Jeremy Cribb <cribb@cs.unc.edu>
2018-03-12 11:34:33 -0400

    further updates, but still doesn't work

--- panoptes/pan_gen_filelists.m
+++ panoptes/pan_gen_filelists.m
@@ -1,38 +1,57 @@
-function outfl = pan_gen_filelist(metadata, wells, passes)
+function outfl = pan_gen_filelists(filepath, systemid, plate_type)
+% PAN_GEN_FILELISTS generate and check list of panoptes data files
+%
+% Generates lists of files expected from a Panoptes run's wells.txt file
+% and checks against what's present and reports on what's missing.
 %
 
-if nargin < 3 || isempty(passes) 
-    passes = unique(metadata.pass_list)';
-end
-   
-if nargin < 2 || isempty(wells)
-    wells = unique(metadata.well_list)';
-end
+metadata = pan_load_metadata(filepath, systemid, plate_type);
 
-% % if ischar(wells)    
-% %     filtparam_name = wells;
-% %     filtparam      = evalin('caller', filtparam_name);    
-% %     
-% %     paramlist = unique(filtparam);
-% %       % filter out empty strings 
-% %       paramlist = paramlist( ~strcmp(paramlist, '') );
-% % end
+Wells_txt_file   = [metadata.instr.experiment ...
+                    '_ExperimentConfig' ... % + some timestamp
+                    ];
+
+Layout_file      = '*_WELL_LAYOUT.csv';
+
+MCU_file         = [metadata.instr.experiment ...
+                    '_MCUparams.txt'];
+
+findcfg_file     = [];
+trackcfg_file    = [];
+        
+exptname = metadata.instr.experiment;
+
+pass_list = 1:size(metadata.instr.offsets,1);
+well_list = metadata.instr.wells;
+
+fidmax = length(pass_list) * length(well_list);
 
 count = 1;
 for p = 1:length(passes)
     for w = 1:length(wells)
         mypass = passes(p);
         mywell = wells(w);
+
+
+
         
-        root_filestring = [metadata.instr.experiment ...
-                          '_video' ...
-                          '_pass' num2str(mypass) ...
-                          '_well' num2str(mywell) ...
-                          '_TRACKED'];
-         
+        burst_filestring = [metadata.instr.experiment ...
+                           '_Flburst' ...
+                           '_pass' num2str(mypass) ...
+                           '_well' num2str(mywell)];        
+        video_filestring = [metadata.instr.experiment ...
+                           '_video' ...
+                           '_pass' num2str(mypass) ...
+                           '_well' num2str(mywell) ...
+                           '_TRACKED'];
+                       
+%         vrpnmat_file
+%         trackcsv_file
+%         vrpnevt_file      
+
         file_type = '.vrpn.evt.mat';
         
-        my_filename = [root_filestring file_type];
+        my_filename = [video_filestring file_type];
         q = dir( my_filename );
               
         if ~isempty(q)
@@ -47,6 +66,9 @@ end
 if ~exist('outfl');
     outfl = [];
 end
+
+return;
+
 
 % function for writing out stderr log messages
 function logentry(txt)
