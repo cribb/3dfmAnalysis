9dcc9c769d9f5d94241f0b2d6ac7e4180860379d
Jeremy Cribb <cribb@cs.unc.edu>
2018-04-09 15:14:42 -0400

    Now saves trackersummary image outputs in png fig and svg formats

--- panoptes/pan_tracker_report.m
+++ panoptes/pan_tracker_report.m
@@ -17,12 +17,21 @@ cs = combine_tracker_image_stacks(bead_stacks_NF);
 
 scs = sort_tracker_images(cs, 'sens');
 hs = plot_tracker_images(scs, 'sens', mytitle);
+gen_pub_plotfiles('trackerimages_unscaled.sens.fig', hs(1), 'normal');
+gen_pub_plotfiles('trackerimages_scaled.sens.fig', hs(2), 'normal');
 
 sca = sort_tracker_images(cs, 'area');
 ha = plot_tracker_images(sca, 'area', mytitle);
+gen_pub_plotfiles('trackerimages_unscaled.area.fig', ha(1), 'normal');
+gen_pub_plotfiles('trackerimages_scaled.area.fig', ha(2), 'normal');
+
 % 
 % scna = sort_tracker_images(cs, 'newarea');
 % hna = plot_tracker_images(scna, 'newarea', mytitle);
+
+
+
+
 
 sens = scs.sorted_vals;
 area = sca.sorted_vals;
@@ -46,5 +55,6 @@ histfig = figure;
 %     hist(newarea, 50);
 %     xlabel('new_area');
 %     ylabel('counts');
+    gen_pub_plotfiles('tracker-report-histogram', histfig, 'normal');
     
 outs = cs;
