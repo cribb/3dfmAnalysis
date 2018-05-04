5dd0560a05b00b6a3f1da05681fc86321a18ade9
Jeremy Cribb <cribb@cs.unc.edu>
2018-05-04 11:49:01 -0400

    Added Xo and Yo columns in anticipation of filtering and drift subtraction. Reworked outputted variable units into something more robust. Added hack to rearrange table columns. When upgrading to Matlab 2018a, use the new movevars matlab function.

--- tracking/load_vst_tracking.m
+++ tracking/load_vst_tracking.m
@@ -6,14 +6,13 @@ function TrackingTable = load_vst_tracking(VidTable)
 %  
 % Loads and aggregates tracking from a dataset defined by the input, VidTable
 % 
-%  pan = panoptes_combine_data(metadata, filtparam_name) 
+%  TrackingTable = load_vst_tracking(VidTable) 
 %   
 % 'VidTable' is a specifically formatted Matlab table. Use 'mk_video_table'
 % function for help in generating this.
 %
 
-filelist = VidTable.File;
-
+filelist = VidTable.TrackingFiles;
 
 % pre-initialize the output so Matlab doesn't have a cow.
 TrackingTable = table;
@@ -21,13 +20,15 @@ TrackingTable = table;
 % Matlab disapproves some of the column-headings in Spot Tracker's 
 % csv-outfile. This turns off the warning.
 
-warning('off', 'MATLAB:table:ModifiedVarnames');
-fid = unique(VidTable.Fid);
+warning('off', 'MATLAB:table:ModifiedAndSavedVarnames');
+
+fid = 1:size(VidTable,1);
+
 for f = 1:length(fid)    
     
     % Handle the incoming data for whatever type it is...
     % First, check if it's a csv file
-    myfile = fullfile(VidTable.Path{f}, VidTable.File{f});
+    myfile = fullfile(VidTable.Path{f}, VidTable.TrackingFiles{f});
     
     dd = readtable(myfile);
 
@@ -37,9 +38,9 @@ for f = 1:length(fid)
     % rename the headers to something more easily coded/read
     dd.Properties.VariableNames{'FrameNumber'} = 'Frame';
     dd.Properties.VariableNames{'SpotID'}      = 'ID';
-
-    logentry(['Loaded *' filelist{f} '* which contains ' num2str(length(unique((dd.ID)))) ' initial trackers.']);
     
+    logentry(['Loaded *' filelist{f} '* which contains ' num2str(length(unique((dd.ID)))) ' initial trackers.']);
+
     % Z data and remaining values are nonexistant (These can be added back if needed in a
     % later version
     dd.Z = [];
@@ -50,18 +51,41 @@ for f = 1:length(fid)
     dd.MeanBackground_FIONA_ = [];
     dd.SummedValue_forFIONA_ = [];
 
+    % Copy X and Y into "original" X and Y columns to Xo and Yo them use X and
+    % Y as filtered and/or drift-corrected dataset. This creates a bifurcation 
+    % in the plausible output (filter vs. unfiltered) and doubles the number of 
+    % MSD computations needed if original MSDs are desired. Don't really see 
+    % any other way around it, though.
+    % 
+    dd.Xo = dd.X;
+    dd.Yo = dd.Y;
+    
     % tack on the Fid value
     dd = [Fid dd];
 
     TrackingTable = [TrackingTable;dd];
 end
-warning('on', 'MATLAB:table:ModifiedVarnames');
+warning('on', 'MATLAB:table:ModifiedAndSavedVarnames');
 
-% There should be a better way than this. I don't like how it's unhinged
-% from the columnTitles.
-TrackingTable.Properties.VariableUnits = {'', '', '', 'pixels', 'pixels', 'pixels', 'Intensity-16-bit', 'pixels^2', '', 'pixels^2'};
-    
-        
+TrackingTable.Properties.VariableUnits{'Fid'} = '';
+TrackingTable.Properties.VariableUnits{'Frame'} = '';
+TrackingTable.Properties.VariableUnits{'ID'} = '';
+TrackingTable.Properties.VariableUnits{'X'} = 'pixels';
+TrackingTable.Properties.VariableUnits{'Y'} = 'pixels';
+TrackingTable.Properties.VariableUnits{'Xo'} = 'pixels';
+TrackingTable.Properties.VariableUnits{'Yo'} = 'pixels';
+TrackingTable.Properties.VariableUnits{'Radius'} = 'pixels';
+TrackingTable.Properties.VariableUnits{'CenterIntensity'} = 'Intensity-16-bit';
+TrackingTable.Properties.VariableUnits{'RegionSize'} = 'pixels^2';
+TrackingTable.Properties.VariableUnits{'Sensitivity'} = '';
+TrackingTable.Properties.VariableUnits{'ForegroundSize'} = 'pixels^2';
+
+
+% This hacks the Xo and Yo columns to be next to the X and Y columns. 
+% XXX TODO: Upgrade to >2018a and use new addvars or movevars table functions
+TrackingTable = TrackingTable(:,[1:5 11 12 6:end-2]);
+
+
 
     
 
