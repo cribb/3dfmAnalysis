4bf30cac9353714b6482c48718517f651e890ad8
Jeremy Cribb <cribb@cs.unc.edu>
2018-05-04 16:19:22 -0400

    Computes end-to-end distance and angle for a vector of positions.

---
+++ math/eedistangle.m
@@ -0,0 +1,16 @@
+function [d,a] = eedistangle(XYcoords)
+
+    if isempty(XYcoords)
+        d = NaN;
+        a = NaN;
+    elseif size(XYcoords,1) == 1
+        d = 0;
+        a = NaN;
+    else
+        d = sqrt( (XYcoords(end,1) - XYcoords(1,1)) .^2 + ...
+                  (XYcoords(end,2) - XYcoords(1,2)) .^2 );
+        a = atan2( XYcoords(end,2) - XYcoords(1,2), ...
+                   XYcoords(end,1) - XYcoords(1,1) );
+    end
+
+    return;
