f4bcc17b4c8c61d280168bb09032ebe0ca6f43fe
Jeremy Cribb <cribb@cs.unc.edu>
2018-05-14 11:21:25 -0400

    Added function that collects imaging information from first frames and intensity projection files for a video spot tracker dataset.

---
+++ tracking/vst_mk_ImageTable.m
@@ -0,0 +1,17 @@
+function outs = vst_collect_images(VidTable)
+
+N = size(VidTable,1);
+
+video_files = fullfile(VidTable.Path, VidTable.VideoFiles);
+fframe_files = fullfile(VidTable.Path, VidTable.FirstFrameFiles);
+mip_files = fullfile(VidTable.Path, VidTable.MipFiles);
+
+for f = 1:N
+    
+        Fid(f,1) = VidTable.Fid(f);
+        FirstFrames{f,1} = imread(fframe_files{f});
+        Mips{f,1} = imread(mip_files{f});
+        
+end
+
+outs = table(Fid, FirstFrames, Mips);
