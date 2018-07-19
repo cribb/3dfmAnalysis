d453ceee5415b1dd91eab5544a417c92fd69bce2
Jeremy A. Cribb <cribb@cs.unc.edu>
2018-07-19 12:53:20 -0400

    Updates to file handling. Still doesn't work.

--- video/bin2pgm.m
+++ video/bin2pgm.m
@@ -1,4 +1,29 @@
-function outs = bin2pgmstack(filename, outputpath)
+function outs = bin2pgm(filename, outputpath)
 
-
+    filename_suffix = regexpi(filename, '_\d+x\d+x\d+_uint\d+.bin', 'match');
+    filename_suffix = filename_suffix{1};
+    
+    [s,e] = regexpi(filename_suffix, '^_\d+x');
+    width = str2num(filename_suffix(s+1:e-1));   
+    filename_suffix(s:e) = [];
+    
+    [s,e] = regexpi(filename_suffix, '^\d+x');
+    height = str2num(filename_suffix(s:e-1));
+    filename_suffix(s:e) = [];
+    
+    [s,e] = regexpi(filename_suffix, '^\d+_');
+    frames = str2num(filename_suffix(s:e-1));
+    filename_suffix(s:e) = [];
+    
+    depth = regexpi(filename_suffix, '^uint\d+', 'match');
+    depth = depth{1};
+    
+    fid = fopen(filename, 'r');
+    for k = 1:frames
+%         myframe = fread(fid, width*height, depth);
+        framename = ['frame' num2str(k,'%04u') '.pgm'];
+%         imwrite(myframe, framename);
+    end
+    
+    outs = {width, height, frames, depth};
 return
--- video/save_bin_on_nvme.m
+++ video/save_bin_on_nvme.m
@@ -1,39 +1,49 @@
 function outs = save_bin_on_nvme(data, basefilename)
+% SAVE_BIN_ON_NVME should be used for saving video data as bin file
+%
+%  outs = save_bin_on_nvme(data, basefilename)
+%
+%  where data = video data in XxYx1xFRAMES shape
 
-   [rows, cols, rgb, frames] = size(data);
-   datatype = class(data);
+    machinefmt = 'ieee-le';
+    
+    [rows, cols, rgb, frames] = size(data);
+    datatype = class(data);
    
-   if rgb ~= 1
-       error('Incorrect format for input. Must be X x Y x RGB x FRAMES, RGB = 1.');
-   end
+    if rgb ~= 1
+        error('Incorrect format for input. Must be X x Y x RGB x FRAMES, RGB = 1.');
+    end
    
-   switch datatype
-       case 'uint8'
-           depth = '8';
-       case 'uint16'
-           depth = '16';
-       otherwise
-           error('Type not found.');
-   end
+    switch datatype
+        case 'uint8'
+            depth = '8';
+            encodingOut = 'US-ASCII';
+        case 'uint16'
+            depth = '16';
+            encodingOut = '';
+        otherwise
+            error('Type not found.');
+    end 
    
-   [mypath, myname, myext] = fileparts(basefilename);
+    [mypath, myname, myext] = fileparts(basefilename);
    
-   filename = strcat([myname, '_', ...
-                      num2str(cols), 'x', ...
-                      num2str(rows), 'x', ...
-                      num2str(frames), 'x', ...
-                      depth, '-bit', ...
-                      '.bin']);
-   
-   rootpath = pwd;
-   filepath = cd(mypath);
+    rootpath = pwd;
+    filepath = cd(mypath);
                   
-   fid = fopen(filename, 'w+');
-   fwrite(fid, data);
-   fclose(fid);
- 
-   disp(['Saved ' mypath filesep filename '.']);
-   cd(rootpath);
-   outs = filename;
+    filename = strcat([myname, '_', ...
+                       num2str(cols), 'x', ...
+                       num2str(rows), 'x', ...
+                       num2str(frames), '_', ...
+                       datatype, ...
+                       '.bin']);
+
+    fid = fopen(filename, 'w');
+
+    fwrite(fid, data, datatype);
+    fclose(fid);
+   
+    disp(['Saved ' mypath filesep filename '.']);
+    cd(rootpath);
+    outs = filename;
 
 return
