9d8379c221265e02650842fac5a62d4faaa77a58
Jeremy Cribb <cribb@cs.unc.edu>
2018-10-15 11:16:23 -0400

    Pulled in X and Y tracking data to supplement Z. Now reads evt files.

--- specific/adhesion/bead_force_stokes_jac.m
+++ specific/adhesion/bead_force_stokes_jac.m
@@ -1,28 +1,32 @@
-function d = bead_force_stokes_jac(filelist, fps, calibum, bead_diameter_um, std_viscosity_Pas, width, height, lolim, hilim)
+function d = bead_force_stokes_jac(filelist, fps, calibum, zstep, bead_diameter_um, std_viscosity_Pas, width, height, lolim, hilim)
 % how this works
 
-if nargin < 9 || isempty(hilim)
+if nargin < 10 || isempty(hilim)
     hilim = 250;
 end
 
-if nargin < 8 || isempty(lolim)
+if nargin < 9 || isempty(lolim)
     lolim = 52;
 end
 
-if nargin < 7 || isempty(height)
+if nargin < 8 || isempty(height)
     height = NaN;
 end
 
-if nargin < 6 || isempty(width)
+if nargin < 7 || isempty(width)
     width = NaN;
 end
 
-if nargin < 5 || isempty(std_viscosity_Pas)
+if nargin < 6 || isempty(std_viscosity_Pas)
     std_viscosity_Pas = 0;
 end
 
-if nargin < 4 || isempty(bead_diameter_um)
+if nargin < 5 || isempty(bead_diameter_um)
     bead_diameter_um = 24;   
+end
+
+if nargin < 4 || isempty(zstep)
+    error('No Zstep calibration defined.');
 end
 
 if nargin < 3 || isempty(calibum)
@@ -43,6 +47,7 @@ N = length(filelist);
 fpslist = repmat(fps, N, 1);
 calibumlist = repmat(calibum, N, 1);
 
+Zstep = repmat(zstep, N, 1);
 BeadDiameter = repmat(bead_diameter_um, N, 1);
 StdViscosity = repmat(std_viscosity_Pas, N, 1);
 VidWidth = repmat(width, N, 1);
@@ -51,15 +56,22 @@ VidHeight = repmat(height, N, 1);
 d.VidTable = mk_video_table(filelist, fpslist, calibumlist, width, height, [], []);
 
 % AUGMENT standard table
-Augs = table(BeadDiameter, StdViscosity, VidWidth, VidHeight);
+Augs = table(Zstep, BeadDiameter, StdViscosity, VidWidth, VidHeight);
 d.VidTable = [d.VidTable Augs];
+d.VidTable.Properties.VariableUnits{'Zstep'} = 'um';
 d.VidTable.Properties.VariableUnits{'BeadDiameter'} = 'um';
 d.VidTable.Properties.VariableUnits{'StdViscosity'} = 'Pa s';
 
 d.TrackingTable = vst_load_tracking(d.VidTable);
 
-jTable = join(d.TrackingTable(:,{'Fid', 'Frame', 'ID', 'Z', 'Radius', 'CenterIntensity', 'Sensitivity'}), ...
+jTable = join(d.TrackingTable(:,{'Fid', 'Frame', 'ID', 'X', 'Y', 'Z', 'Radius', 'CenterIntensity', 'Sensitivity'}), ...
               d.VidTable(:,{'Fid', 'Fps', 'Calibum'}));
+
+jTable.X = jTable.X .* jTable.Calibum; % [xPixels] -> [um]
+jTable.Properties.VariableUnits{'X'} = 'um';
+
+jTable.Y = jTable.Y .* jTable.Calibum; % [yPixels] -> [um]
+jTable.Properties.VariableUnits{'Y'} = 'um';
 
 jTable.Z = jTable.Z .* jTable.Calibum; % [stepsize] -> [um]
 jTable.Properties.VariableUnits{'Z'} = 'um';
@@ -69,9 +81,11 @@ jTable.Properties.VariableUnits{'Time'} = 's';
 
 [g, grpTable] = findgroups(jTable(:,{'Fid', 'ID'}));
 
-tmpVels = splitapply(@(x1,x2,x3)calcZvel_easy(x1, x2, x3, lolim, hilim), ...
+tmpVels = splitapply(@(x1,x2,x3,x4,x5) calcZvel_xyz(x1, x2, x3, x4, x5, lolim, hilim), ...
                     jTable.Time, ...
                     jTable.ID, ...
+                    jTable.X, ...
+                    jTable.Y, ...
                     jTable.Z, ...
                     g);
 
@@ -118,7 +132,7 @@ ylabel('Z velocity [um/s], s=8');
 
 return
 
-function outs = calcZvel_jac(frames, trackerid, zpos, lowlim, highlim)
+function outs = calcZvel_jac(frames, trackerid, xpos, ypos, zpos, lowlim, highlim)
 
 %     first = find(zpos >  lowlim, 1);
 %     last  = find(zpos > highlim, 1);
@@ -216,7 +230,7 @@ function outs = calcZvel_jac(frames, trackerid, zpos, lowlim, highlim)
 return
 
 
-function outs = calcZvel(frames, trackerid, zpos, lowlim, highlim)
+function outs = calcZvel(frames, trackerid, xpos, ypos, zpos, lowlim, highlim)
 
 %     first = find(zpos >  lowlim, 1);
 %     last  = find(zpos > highlim, 1);
@@ -304,17 +318,20 @@ function outs = calcZvel(frames, trackerid, zpos, lowlim, highlim)
 return
 
 
-function outs = calcZvel_easy(frames, trackerid, zpos, lowlim, highlim)
-    first = 1;
-    last = length(zpos);
-        
+function outs = calcZvel_easy(frames, trackerid, xpos, ypos, zpos, lowlim, highlim)
+
     myID = unique(trackerid);
+    
+    % Where is the minimum value
+    [~, minidx] = min(zpos);
+    
+    zpos = zpos(minidx:end);
+    frames = frames(minidx:end);
+    
     
     % Choose how long the dataset needs to be before we consider it. If
     % it's too short, return NaN for this tracker.
-    if length(zpos) > 100
-        initZ = max(zpos(1:10));
-    else
+    if length(zpos) < 10
         outs = NaN(1,7);
         disp('Not enough points to run algorithm.');
     end
@@ -332,7 +349,7 @@ function outs = calcZvel_easy(frames, trackerid, zpos, lowlim, highlim)
     % is moving down, it's not one we want to measure velocity for.
     % Consider drawing out this part of the process into a different
     % function.    
-    [~, maxidx] = max(zpos);
+    [maxz, maxidx] = max(zpos);
     [minz, ~] = min(zpos);    
     
     if maxidx < ceil(length(zpos)*0.1)
@@ -348,11 +365,19 @@ function outs = calcZvel_easy(frames, trackerid, zpos, lowlim, highlim)
     end
     
     
-    midrange = ceil(range(zpos(zpos>=initZ))/2 + minz);   % Middle of the range
+%     midrange = ceil(range(zpos(zpos>minz))/2 + minz);   % Middle of the range
+    midrange = ceil(range(zpos(zpos>minz))/2 + minz);   % Middle of the range
     [midz, mid_zidx] = min(abs(zpos - midrange));
     lowerflat = mode(zpos(1:mid_zidx));    
     
+    max_extent = length(zpos) - mid_zidx;
+    
     extent = 3;
+    
+    if extent > max_extent
+        extent = max_extent;
+    end
+    
     dist_far_edge = length(zpos) - (mid_zidx+extent);
     while dist_far_edge < 0
         extent = extent - 1;
@@ -405,3 +430,127 @@ function outs = calcZvel_easy(frames, trackerid, zpos, lowlim, highlim)
     title(['trackerID = ' num2str(myID), ', CVerr= ', num2str(CVerr,'%2.2f')]);
     xlim([min(frames_for_fitting)-40*mean(diff(frames)) max(frames_for_fitting)+mean(diff(frames))]);
 return
+
+function outs = calcZvel_xyz(frames, trackerid, xpos, ypos, zpos, lowlim, highlim)
+
+    myID = unique(trackerid);
+    
+%     [dx, dy, dz] = deal(diff(xpos), diff(ypos), diff(zpos));
+
+    dxyz = diff([xpos(:), ypos(:), zpos(:)]);
+    df = diff(frames(:));
+    df = repmat(df, 1, 3);
+    
+    dxyzdf = dxyz./df;
+    
+    
+     
+    % Where is the minimum value
+    [~, minidx] = min(zpos);
+    
+    zpos = zpos(minidx:end);
+    frames = frames(minidx:end);
+    
+    
+    % Choose how long the dataset needs to be before we consider it. If
+    % it's too short, return NaN for this tracker.
+    if length(zpos) < 10
+        outs = NaN(1,7);
+        disp('Not enough points to run algorithm.');
+    end
+    
+
+%     if isempty(last)
+%         frames = frames(first:end);
+%         zpos = zpos(first:end);
+%     else
+%         frames = frames(first:last(1)-1);
+%         zpos = zpos(first:last(1)-1);
+%     end
+
+    % Next, where is the maximum z for this tracker located? If a tracker
+    % is moving down, it's not one we want to measure velocity for.
+    % Consider drawing out this part of the process into a different
+    % function.    
+    [maxz, maxidx] = max(zpos);
+    [minz, ~] = min(zpos);    
+    
+    if maxidx < ceil(length(zpos)*0.1)
+        outs = NaN(1,7);
+        disp('This tracker does not appear to leave the substrate.');
+    end
+    
+    % Is the maximum z-value the last value in the trajectory? If not, make
+    % it so.
+    if maxidx ~= length(zpos)
+        zpos = zpos(1:maxidx);
+        frames = frames(1:maxidx);
+    end
+    
+    
+%     midrange = ceil(range(zpos(zpos>minz))/2 + minz);   % Middle of the range
+    midrange = ceil(range(zpos(zpos>minz))/2 + minz);   % Middle of the range
+    [midz, mid_zidx] = min(abs(zpos - midrange));
+    lowerflat = mode(zpos(1:mid_zidx));    
+    
+    max_extent = length(zpos) - mid_zidx;
+    
+    extent = 3;
+    
+    if extent > max_extent
+        extent = max_extent;
+    end
+    
+    dist_far_edge = length(zpos) - (mid_zidx+extent);
+    while dist_far_edge < 0
+        extent = extent - 1;
+        dist_far_edge = length(zpos) - (mid_zidx+extent);
+    end
+    
+    if length(zpos) >= 2*extent + 1
+        Nframes = 2*extent + 1;
+        zpos_for_fitting = zpos(mid_zidx-extent:mid_zidx+extent);
+        frames_for_fitting = frames(mid_zidx-extent:mid_zidx+extent);
+    else zpos_for_fitting = zpos;
+        frames_for_fitting = frames;
+        Nframes = length(zpos);
+    end
+    
+    order = 1;
+
+    if length(frames_for_fitting) < order + 2
+        outs = NaN(1,7);
+        return;
+    end  
+    
+    fit = polyfit(frames_for_fitting, zpos_for_fitting, order);
+    fitZ = polyval(fit, frames_for_fitting);
+        
+    firstframe = fit(1) * lowerflat + fit(2);
+    Zvel = fit(1); % [stepsize/frame]
+    ZfitRmse = sqrt(sum((zpos_for_fitting-fitZ).^2));
+    ZposMean = mean(zpos_for_fitting);
+    ZposRange = range(zpos_for_fitting);
+    CVerr = ZfitRmse / ZposMean;
+    
+    outs = [firstframe, Zvel, ZfitRmse, ZposMean, ZposRange, CVerr, Nframes];    
+    
+    smooth_dzdt = CreateGaussScaleSpace(zpos, 1, 4) ./ mean(diff(frames));
+    [maxdzdt, maxdzdt_idx] = max(smooth_dzdt);
+    
+    figure;
+    hold on;
+    plot(frames,             zpos,             'Marker', 'o', 'MarkerEdgeColor', [0.75 0.75 0.75], 'MarkerFaceColor', 'none', 'LineStyle', 'none');
+    plot(frames_for_fitting, zpos_for_fitting, 'Marker', 'o', 'MarkerEdgeColor', 'k', 'MarkerFaceColor', 'k', 'LineStyle', 'none');
+    plot(frames_for_fitting, fitZ, 'Marker', 'x', 'MarkerEdgeColor', 'r', 'LineStyle', '-', 'Color', 'r');
+    plot(frames(maxdzdt_idx), zpos(maxdzdt_idx), 'Marker', '^', 'MarkerSize', 16);
+    plot(frames(mid_zidx), midrange, 'Marker', 'diamond', 'MarkerSize', 16);
+    
+    hold off;
+    xlabel('Time [s]');
+    ylabel('Zpos [um]');
+    legend('zpos', 'UsedInFit', 'fit', 'max deriv.', 'midrange', 'Location','northwest');
+    title(['trackerID = ' num2str(myID), ', CVerr= ', num2str(CVerr,'%2.2f')]);
+    xlim([min(frames_for_fitting)-40*mean(diff(frames)) max(frames_for_fitting)+mean(diff(frames))]);
+return
+
