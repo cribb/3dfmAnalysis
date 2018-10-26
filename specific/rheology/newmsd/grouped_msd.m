694caade69e15b1190705c6facde2e85eeb98dc6
Jeremy Cribb <cribb@cs.unc.edu>
2018-10-26 14:34:13 -0400

    Better group handling.

--- specific/rheology/newmsd/grouped_msd.m
+++ specific/rheology/newmsd/grouped_msd.m
@@ -1,53 +1,103 @@
-function MsdTable = grouped_msd(DataIn, taulist, group_names)
+function MsdTable = grouped_msd(DataIn, group_names)
 
-    if nargin < 3 || isempty(group_names)
-        group_names = {'SampleName', 'SampleInstance', 'FovID'};
+    if nargin < 2 || isempty(group_names)
+        group_names = {'SampleName', 'SampleInstance', 'FovID', 'Tau'};
     end
 
+    
     v = DataIn.VidTable(:,{'Fid', 'SampleName', 'SampleInstance', 'FovID', 'Fps', 'Calibum'});
-    m = DataIn.TrackingTable;
+    m = DataIn.MsdTable(:,{'Fid', 'ID', 'Tau', 'MsdX', 'MsdY', 'MsdXo', 'MsdYo', 'N'});
 
     b = join(m, v);
-
-    [g, foo] = findgroups(b(:,group_names));
-
-    [g,gFid,gID] = findgroups(TrackingTable.Fid, TrackingTable.ID);
     
-%     taulist = repmat(taulist, length(g), 1);
-    msdset = splitapply(@(x1,x2,x3){split_apply_msd(x1,x2,x3,taulist)}, ...
-                                         TrackingTable.Fid, ...
-                                         TrackingTable.ID, ...
-                                         [TrackingTable.X, TrackingTable.Y TrackingTable.Xo TrackingTable.Yo], ...
+    b.MsdX = b.MsdX .* (b.Calibum).^2;
+    b.MsdY = b.MsdY .* (b.Calibum).^2;
+    b.MsdXo = b.MsdXo .* (b.Calibum).^2;
+    b.MsdYo = b.MsdYo .* (b.Calibum).^2;    
+    b.Properties.VariableUnits{'MsdX'} = '[um^2]';
+    b.Properties.VariableUnits{'MsdXo'} = '[um^2]';
+    b.Properties.VariableUnits{'MsdY'} = '[um^2]';
+    b.Properties.VariableUnits{'MsdYo'} = '[um^2]';
+    
+    b.Tau = b.Tau ./ b.Fps;
+    b.Properties.VariableUnits{'Tau'} = '[s]';
+    
+    b.Calibum = [];
+    b.Fps = [];
+    
+    [g, mygroups] = findgroups(b(:,group_names));
+
+%     [g,gFid,gID] = findgroups(MsdTable.Fid, MsdTable.ID);        
+    
+    msdset = splitapply(@(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11){split_apply_groupmsd(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11)}, ...
+                                         b.Fid, ...
+                                         b.ID, ...
+                                         b.Tau, ...
+                                         b.MsdX, ...
+                                         b.MsdY, ...
+                                         b.MsdXo, ...
+                                         b.MsdYo, ...                                         
+                                         b.N, ...
+                                         b.SampleName, ...
+                                         b.SampleInstance, ...
+                                         b.FovID, ...
                                          g);    
+                                     
+%     msdset = splitapply(@split_apply_groupmsd, blah, g);    
     
-    mymsd = cell2mat(msdset);
+    msdset = cell2mat(msdset);
     
-    MsdTable.Fid = mymsd(:,1);
-    MsdTable.ID = mymsd(:,2);
-    MsdTable.Tau = mymsd(:,3);
-    MsdTable.MsdX = mymsd(:,4);
-    MsdTable.MsdY = mymsd(:,5);    
-    MsdTable.MsdXo = mymsd(:,6);
-    MsdTable.MsdYo = mymsd(:,7);
-    MsdTable.N = mymsd(:,8);
+    msdout = table(msdset(:,1), msdset(:,2), msdset(:,3), msdset(:,4), msdset(:,5), msdset(:,6), msdset(:,7), msdset(:,8), msdset(:,9), msdset(:,10));
+    msdout.Properties.VariableNames = {'MsdX', 'MsdY', 'MsdXo', 'MsdYo', 'varMsdX', 'varMsdY', 'varMsdXo', 'VarMsdYo', 'Ntrackers', 'Nestimates'};
+    msdout.Properties.VariableUnits = {'um^2', 'um^2', 'um^2', 'um^2', 'um^4', 'um^4', 'um^4', 'um^4', '', ''};
+           
+    MsdTable = [mygroups, msdout];
+
+%     MsdTable.Fid = mymsd(:,1);
+%     MsdTable.ID = mymsd(:,2);
+%     MsdTable.Tau = mymsd(:,3);
+%     MsdTable.MsdX = mymsd(:,4);
+%     MsdTable.MsdY = mymsd(:,5);    
+%     MsdTable.MsdXo = mymsd(:,6);
+%     MsdTable.MsdYo = mymsd(:,7);
+%     MsdTable.N = mymsd(:,8);
         
-    MsdTable = struct2table(MsdTable);
+%     MsdTable = struct2table(MsdTable);
     
 return
    
    
-function outs = split_apply_msd(fid, id, data, taulist)
+function outs = split_apply_groupmsd(Fid, ID, Tau, MsdX, MsdY, MsdXo, MsdYo, N, SampleName, SampleInstance, FovID)
 
-    taulist = taulist(:);
-    Ntaus = length(taulist);
+    aggN = sum(N);
+    weights = N./aggN;
+    count = sum(~isnan(MsdX));
+    
+    MsdX = log10(MsdX);
+    MsdY = log10(MsdY);
+    MsdXo = log10(MsdXo);
+    MsdYo = log10(MsdYo);
+    
+    % Setting NaNs to zero because a NaN now has zero weight and won't
+    % matter anyway.
+    MsdX(isnan(MsdX)) = 0;
+    MsdY(isnan(MsdY)) = 0;
+    MsdXo(isnan(MsdXo)) = 0;
+    MsdYo(isnan(MsdYo)) = 0;
 
-    fid = repmat(fid(1), Ntaus, 1);
-    id  = repmat(id(1), Ntaus, 1);
+    aggMsdX = sum(MsdX .* weights, [], 'omitnan');
+    aggMsdY = sum(MsdY .* weights, [], 'omitnan');
+    aggMsdXo = sum(MsdXo .* weights, [], 'omitnan');
+    aggMsdYo = sum(MsdYo .* weights, [], 'omitnan');
     
-    [mymsd, Nest] = msd1d(data, taulist);    
+    V1 = aggN ./ (aggN-1);
+%     varMsdX = V1 * sum(weights .*(MsdX(:) - repmat(aggMsdX,size(MsdX,1),1)).^2, 1);
+    varMsdX = sum(weights .*(MsdX(:) - repmat(aggMsdX,size(MsdX,1),1)).^2, 1);
+    varMsdY = sum(weights .*(MsdY(:) - repmat(aggMsdY,size(MsdY,1),1)).^2, 1);
+    varMsdXo = sum(weights .*(MsdXo(:) - repmat(aggMsdXo,size(MsdXo,1), 1)).^2, 1);
+    varMsdYo = sum(weights .*(MsdYo(:) - repmat(aggMsdYo,size(MsdYo,1), 1)).^2, 1);
     
-    outs = [fid, id, taulist, mymsd, Nest];
-    
+    outs = [aggMsdX, aggMsdY, aggMsdXo, aggMsdYo, varMsdX, varMsdY, varMsdXo, varMsdYo, count, aggN];
 return
 
 
