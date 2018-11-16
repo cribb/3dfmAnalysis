5417c60626c8e8b1e616eb1ec4628e26a3c8e82f
Jeremy Cribb <cribb@cs.unc.edu>
2018-11-16 16:01:41 -0500

    Updated configuration/setup for running spot tracker from matlab.

--- tracking/vst_run_from_matlab.m
+++ tracking/vst_run_from_matlab.m
@@ -1,67 +1,75 @@
-function v = vst_run_from_matlab(filetotrack, ins)
+function v = vst_run_from_matlab(filetotrack, logfile, configfile, ins)
 
-filetotrack = 'D:\Dropbox\prof\Lab\Superfine Lab\expts\bead_adhesion_assay\sample_video\MucusBeads1\frame00001.pgm';
+% filetotrack = 'D:\Dropbox\prof\Lab\Superfine Lab\expts\bead_adhesion_assay\sample_video\MucusBeads1\frame00001.pgm';
 
-ins.VSTdir = 'C:\Program Files (x86)\CISMM\Video_Spot_Tracker_v8.13.0\';
-ins.VSTcuda = 'video_spot_tracker_local.bat';
-ins.VSTexe = 'video_spot_tracker.exe';
-% ins.VSTexe = 'video_spot_tracker_nogui.exe';
-ins.burstimage = '';
-ins.firstframe = '';
-ins.absfile = '';
-ins.configFind = 'D:\Dropbox\prof\Lab\Superfine Lab\expts\bead_adhesion_assay\optimize_z_test.cfg';
-ins.configTrack = 'D:\Dropbox\prof\Lab\Superfine Lab\expts\bead_adhesion_assay\optimize_z_test.cfg';
-ins.os = '';
-ins.logdir = 'D:\Dropbox\prof\Lab\Superfine Lab\expts\bead_adhesion_assay\';
+if nargin < 3 || isempty(ins)
+    error('Video Spot Tracker configuration empty or not found.');
+end
 
-tclpath = join([ins.VSTdir, 'tcl8.3'], '');
- tkpath = join([ins.VSTdir, 'tk8.3'], '');
-command = join([ins.VSTdir, filesep, ins.VSTexe], '');
-logfile = join([ins.logdir, 'test_tmp'], '');
+if nargin < 1 || isempty(filetotrack) || ~exist(filetotrack, 'file')
+    error('File empty or not found.');
+end
+
+startpath = pwd;
+
+vst_check_config(ins);
+
+% logfile = join([ins.logdir, 'test_tmp'], '');
 
 commandopts = ['-enable_internal_values ' ...
                '-lost_all_colliding_trackers ' ...
-               '-load_state "' ins.configFind '" ' ...
-               '-tracker 0 0 50 ' ...
-               '-outfile "' logfile '" ' ...
-               '"' filetotrack '" '];
-           
-fullcommand = ['"' command '" ' commandopts];
+               '-load_state "' configfile '" ' ...
+               '-tracker 0 0 50 '];
+
+if isfield(ins, 'continue_from')
+    tmp = dir(ins.continue_from);
+    disp(['Continuing from: ' tmp.name]);
+    commandopts = [commandopts '-continue_from "' ins.continue_from '" '];
+end
+
+if nargin < 2 || isempty(logfile)
+    warning('No logfile defined.');
+else
+    commandopts = [commandopts ' -outfile "' logfile '" '];
+end
+
+
+commandopts = [commandopts '"' filetotrack '" '];
+
+% disp(commandopts);     
+
+copyfile([ins.VSTdir filesep 'russ_widgets.tcl'], startpath);
+copyfile([ins.VSTdir filesep 'video_spot_tracker.tcl'], startpath);
+
+% fullcommand = ['"' ins.command '" ' commandopts];
+fullcommand = ['"' ins.VSTexe '" ' commandopts];
+
 systemSTR = join(fullcommand);
 disp(systemSTR);
 
+oldpath = getenv('PATH');
+if ~contains(oldpath, ins.VSTdir)
+    newpath = [oldpath pathsep ins.VSTdir];
+else
+    newpath = oldpath;
+end
+
 % setenv('PATH', mypath);
-setenv('TCL_LIBRARY', tclpath);
-setenv('TK_LIBRARY', tkpath);
+setenv('TCL_LIBRARY', ins.tclpath);
+setenv('TK_LIBRARY', ins.tkpath);
+setenv('PATH', newpath);
 
-
-cd(ins.VSTdir);
+% cd(ins.VSTdir);
 system(systemSTR);
+% cd(startpath);
+
+delete('russ_widgets.tcl');
+delete('video_spot_tracker.tcl');
+
+v = 0;
 
 return
  
-% 
-% function v = create_vstbat(ins)
-% 
-% 
-% 
-% cmd = ".\video_spot_tracker.exe";
-% 
-% fprintf('set PATH=%s;\%PATH\% \n',mypath);
-% fprintf('set TCL_LIBRARY=%s\n', tclpath);
-% fprintf('set TK_LIBRARY=%s\n', tkpath);
-% 
-% % REM By default, call the non-CUDA version of spot tracker.
-% fprintf('set CMD="%s"\n', cmd);
-% 
-% % REM If the first command-line argument is CUDA, then set the command name to run the
-% % REM CUDA version and shift the command-line arguments to remove it from the ones sent
-% % REM to the command we call.
-% % IF NOT [%1]==[CUDA] GOTO NOCUDA
-% % set CMD=".\video_spot_tracker_CUDA.exe"
-% % SHIFT
-% % :NOCUDA
-% % 
-% fprintf('\%CMD\% \%1 \%2 \%3 \%4 \%5 \%6 \%7 \n');
-% 
-% % if not errorlevel 0 pause
+
+
+
