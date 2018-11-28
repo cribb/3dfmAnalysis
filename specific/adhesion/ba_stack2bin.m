2c7fa16219c627054242fbb52f1ea9d05ef588b2
Jeremy Cribb <cribb@cs.unc.edu>
2018-11-28 10:49:15 -0500

    Can back-convert a stack of pgm images to a bin file.

---
+++ specific/adhesion/ba_stack2bin.m
@@ -0,0 +1,17 @@
+startdir = pwd;
+filelist = dir('*_uint16');
+for k = 1:length(filelist)
+    stackdir = filelist(k).name; 
+    fid = fopen([stackdir '.bin'], 'w');
+    cd(stackdir)
+    imlist = dir('frame*.pgm');
+    for m = 1:length(imlist)
+        im = imread(imlist(m).name);
+        fwrite(fid, im, 'uint16');
+        if ~mod(m,100)
+            disp(['Binning ' num2str(m) ' of ' num2str(length(imlist)) ' frames.']);
+        end
+    end
+    fclose(fid);
+    cd(startdir);
+end
