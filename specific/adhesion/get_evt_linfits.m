614fdecab0fbe2924876cd6dbd912fcb020566ad
Jeremy Cribb <cribb@cs.unc.edu>
2018-11-30 10:00:50 -0500

    Expanded output.

--- specific/adhesion/get_evt_linfits.m
+++ specific/adhesion/get_evt_linfits.m
@@ -8,8 +8,12 @@ t = d.FrameNumber ./ d.Fps;
 
 foo = splitapply(@(x,y)mylinfit(x,y,1), t, d.Z, g);
 
+mb = cell2mat(foo(:,3));
+
 m.SpotID = ID;
-m.Velocity = foo(:,1);
+m.Pulloff_time = cell2mat(foo(:,1));
+m.Mean_time = cell2mat(foo(:,2));
+m.Mean_vel = mb(:,1);
 
 m = struct2table(m);
 
@@ -17,6 +21,9 @@ return
 
 function outs = mylinfit(x,y,order)
 
-    outs = polyfit(x,y,order);
-
+    mb = polyfit(x,y,order);
+    outs{1,1} = x(1);
+    outs{1,2} = mean(x);
+    outs{1,3} = mb;
+    
 return
