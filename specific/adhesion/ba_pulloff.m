1b9003e9a9ed3ff02f168fa395c045e2bfc1b9ca
Jeremy Cribb <jcribb@email.unc.edu>
2019-05-14 14:08:01 -0400

    Major update to camera polling and file-saving. Now requires the number of frames in the upcoming video instead of leaving it freeform.

--- specific/adhesion/ba_pulloff.m
+++ specific/adhesion/ba_pulloff.m
@@ -1,4 +1,4 @@
-function ba_pulloff(filename, exptime)
+function ba_pulloff(h, filename, exptime)
 % Prior to starting experiment, make sure the magnet is centered.  Lower
 % the magnet to 0 and use the vertical micrometer to ensure the tips of the
 % magnet will touch the top of a glass slide (to apply maximum force to
@@ -15,112 +15,162 @@ function ba_pulloff(filename, exptime)
 % each other or an edge will probably not work well when tracking. Focus the region, then
 % close image acquisition again.  Run the script.
 
-
-% clear all
-% close all
-% clc
-% filename='2umPSbeads_fluidtop01_4.11.19';
-
-if nargin < 2 || isempty(exptime)
-    exptime = 4;
+if nargin < 1 || isempty(h)
+    disp('No z-controller object. Connecting to z-motor now...');
+    try
+        h = ba_initz;
+    catch
+        error('No connection. Is motor connected and running?');
+    end
+    disp('Connected.');
 end
 
-% This global variable serves as a handle to the Thorlabs z-motor
-global h;           
+if nargin < 2 || isempty(filename)
+    error('Need filename.');
+end
 
-%--- Create ActiveX Controller for the ThorLabs z-motor
-% This section is mostly taken from Thorlabs APT Matlab Guide, located at:
-% https://www.thorlabs.com/tutorials/APTProgramming.cfm (bottom link under
-% "Matlab" section). More information can be found here: 
-% http://www.mathworks.com/help/imaq/basic-image-acquisition-procedure.html
-
-% The z-motor GUI gets docked into a matlab figure that's explicitly 
-% constructed using the Matlab's default figure properties (i.e. figure '0').
-fpos=get(0,'DefaultFigurePosition');
-fpos(3)=650; % Figure width
-fpos(4)= 450;% Figure height
-f = figure('Position', fpos, 'Menu', 'None', 'Name', 'APT GUI');
-
-% Initialize Control GUI for Thorlabs z-motor control
-% !regsvr64 /s C:\Program Files (x86)\Thorlabs\APT\APT Server\MG17Motor.ocx
-h = actxcontrol('MGMOTOR.MGMotorCtrl.1',[20 20 600 400 ], f);
-h.StartCtrl;
-
-% The serial number for the motor is 83829797. Must set it here and
-% register/identify our connection to the motor.
-set(h,'HWSerialNum', 83829797);
-h.Identify; 
-pause(5); 
+if nargin < 3 || isempty(exptime)
+    exptime = 8; % [ms]
+end
 
 
-%% Set video parameters, start camera
+cnt = 1;
+starting_height = 3;
+abstime{1,1} = [];
+framenumber{1,1} = [];
+TotalFrames = 0;
+motor_velocity = 0.2; % [mm/sec]
+
+Nsec = starting_height/motor_velocity + 1;
+Fps = 1 / (exptime/1000);
+NFrames = Fps * Nsec;
+
+height = NaN(NFrames,1);
+height(1,1) = starting_height;
+
 vid = videoinput('pointgrey', 1,'F7_Raw16_1024x768_Mode2');
 vid.ReturnedColorspace = 'grayscale';
-myprops.vid = propinfo(vid);
+triggerconfig(vid, 'manual');
+vid.FramesPerTrigger = NFrames;
 
 % Following code found in apps -> image acquisition
+% More info here: http://www.mathworks.com/help/imaq/basic-image-acquisition-procedure.html
 src = getselectedsource(vid); 
 src.ExposureMode = 'off'; 
 src.FrameRateMode = 'off';
 src.ShutterMode = 'manual';
 src.Gain = 10;
-src.Gamma=1.15;
-src.Brightness=5.8594;
+src.Gamma = 1.15;
+src.Brightness = 5.8594;
 src.Shutter = exptime;
 
-triggerconfig(vid, 'manual');
-vid.FramesPerTrigger = Inf;
-pause(2)
+% preview(vid);
+
+% ----------------
+% Controlling the Hardware and running the experiment
+%
+
+pause(2);
+logentry('Starting video...');
 start(vid);
 pause(2);
 
+NFramesAvailable = 0;
 
-% Control the Z-motor and collect the video
-
-% Initial height of magnet
-height = 12; 
+fid = fopen('tempvid.bin', 'w');
 
 % set motor velocity to .2 mm/sec
-h.SetVelParams(0,0,1,.2); 
+h.SetVelParams(0, 0, 1, motor_velocity); 
 
-% move from 8 mm position to 0 mm
-h.SetAbsMovePos(0,0); 
-pause(2)
-h.MoveAbsolute(0,1==0);
+% move from starting position in mm down to 0 mm
+h.SetAbsMovePos(0, 0); 
+pause(2);
+logentry('Moving motor...');
+h.MoveAbsolute(0, 1==0);
+
+
+logentry('Triggering video collection...');
 trigger(vid);
 
-% Start timer
+% start timer for video timestamps
 t1=tic; 
 
-% This loop runs until motor position reaches 0
-while height > 0 
+% Check and store the motor position every 100 ms until it reaches zero. 
+pause(1/Fps);
+while(vid.FramesAvailable > 0)
+    cnt = cnt + 1;
+    height(cnt) = h.GetPosition_Position(0); 
     
-    height=h.GetPosition_Position(0); %returns position of motor and stores in ztvector
-   
-    pause(.1)
-    %flushdata(vid)
-   
+    NFramesAvailable(cnt,1) = vid.FramesAvailable;
+    [data, ~, meta] = getdata(vid, NFramesAvailable(cnt,1));    
+    
+    abstime{cnt,1} = vertcat(meta(:).AbsTime);
+    framenumber{cnt,1} = meta(:).FrameNumber;
+
+%     TotalFrames = TotalFrames + NFramesAvailable;   
+    [rows, cols, rgb, frames] = size(data);
+
+    datatype = class(data);
+    fwrite(fid, data, datatype);
+    logentry(['Wrote ' num2str(NFramesAvailable(cnt,1)) ' frames.']);
 end
 
-if height<.001
-    t2=toc(t1);
-    % stoppreview(vid);
-    stop(vid)
-    pause(1)
-%     h.MoveHome(0,0); %rehome the motor, then return to the 6mm position
-%     pause(10)
-    h.SetVelParams(0,0,4,2); %last two numbers describe acceleration and velocity (mm/sec)
-    h.SetAbsMovePos(0,12); 
-    h.MoveAbsolute(0,1==0);  
-end
-%% Saving data
-[data time meta ] = getdata(vid);
-time=zeros(length(data(1,1,1,:)),1);
-for k=1:length(meta)
+elapsed_time = toc(t1);
 
-    time(k)=seconds(days(datenum(meta(k).AbsTime)));
-end
-save_bin_on_nvme(data,filename)
-xlswrite([filename,'.xlsx'],time');
+logentry('Stopping video collection...');
+stop(vid);
+pause(1);
+% 
+% NFramesAvailable = vid.FramesAvailable;
+% [data, ~, meta] = getdata(vid, NFramesAvailable);    
+% logentry(['Wrote ' num2str(NFramesAvailable) ' final frames.']);
+%     
+% TotalFrames = TotalFrames + NFramesAvailable;   
+% 
+% datatype = class(data);
+% fwrite(fid, data, datatype);
+    
+% Last two numbers describe acceleration and velocity [mm/sec]
+h.SetVelParams(0, 0, 4, 2); 
+h.SetAbsMovePos(0, starting_height); 
+h.MoveAbsolute(0, 1==0);  
 
 
+fclose(fid);
+
+height(cnt+1:end) = [];
+NFramesCollected = sum(NFramesAvailable);
+AbsFrameNumber = cumsum(NFramesAvailable);
+
+logentry(['Total Frame count: ' num2str(NFramesCollected)]);
+
+interp_heights(:,1) = interp1(AbsFrameNumber, height, 0:NFramesCollected-1);
+
+
+time = cellfun(@(x)seconds(days(datenum(x))), abstime, 'UniformOutput', false);
+time = vertcat(time{:});
+
+TimeHeightTable = table(time, interp_heights);
+
+delete(vid);
+clear vid
+
+logentry('Done!');
+
+return
+
+
+% function for writing out stderr log messages
+function logentry(txt)
+    logtime = clock;
+    logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
+                   num2str(logtime(2),        '%02i') '.' ...
+                   num2str(logtime(3),        '%02i') ', ' ...
+                   num2str(logtime(4),        '%02i') ':' ...
+                   num2str(logtime(5),        '%02i') ':' ...
+                   num2str(floor(logtime(6)), '%02i') ') '];
+     headertext = [logtimetext 'ba_pulloff: '];
+     
+     fprintf('%s%s\n', headertext, txt);
+     
+     return;    
