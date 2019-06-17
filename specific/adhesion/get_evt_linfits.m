6486f780519ef80f7815b1a6a9c05c6ac7ed6214
Jeremy Cribb <jcribb@email.unc.edu>
2019-06-17 14:32:12 -0400

    Added external parameters for viscosity, bead size, etc.

--- specific/adhesion/get_evt_linfits.m
+++ specific/adhesion/get_evt_linfits.m
@@ -1,4 +1,4 @@
-function m = get_evt_linfits(evtfile)
+function m = get_evt_linfits(evtfile, calibum, visc_Pas, bead_diameter_um)
 
 d = load_evtfile(evtfile);
 
@@ -10,10 +10,12 @@ foo = splitapply(@(x,y)mylinfit(x,y,1), t, d.Z, g);
 
 mb = cell2mat(foo(:,3));
 
+m.Filename = repmat(string(evtfile), size(foo,1), 1);
 m.SpotID = ID;
 m.Pulloff_time = cell2mat(foo(:,1));
 m.Mean_time = cell2mat(foo(:,2));
-m.Mean_vel = mb(:,1);
+m.Mean_vel = mb(:,1) * calibum * 1e-6;
+m.Force = 6 * pi * visc_Pas * bead_diameter_um/2 * 1e-6 * m.Mean_vel;
 
 m = struct2table(m);
 
