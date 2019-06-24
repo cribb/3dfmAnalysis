db8d45746c293edbca589fa2094b8f640932d3a4
Jeremy Cribb <cribb@cs.unc.edu>
2019-06-24 14:18:20 -0400

    Added start position and FileID to the parameters passed in.

--- specific/adhesion/get_evt_linfits.m
+++ specific/adhesion/get_evt_linfits.m
@@ -1,4 +1,4 @@
-function m = get_evt_linfits(evtfile, calibum, visc_Pas, bead_diameter_um)
+function m = get_evt_linfits(evtfile, calibum, visc_Pas, bead_diameter_um, Fid)
 
 d = load_evtfile(evtfile);
 
@@ -7,11 +7,14 @@ t = d.FrameNumber ./ d.Fps;
 [g, ID] = findgroups(d.SpotID);
 
 foo = splitapply(@(x,y)mylinfit(x,y,1), t, d.Z, g);
+sp = splitapply(@(x,y)get_startpos(x,y), d.X, d.Y, g);
 
 mb = cell2mat(foo(:,3));
 
+m.Fid = repmat(Fid, size(foo,1), 1);
 m.Filename = repmat(string(evtfile), size(foo,1), 1);
 m.SpotID = ID;
+m.StartPosition = cell2mat(sp);
 m.Pulloff_time = cell2mat(foo(:,1));
 m.Mean_time = cell2mat(foo(:,2));
 m.Mean_vel = mb(:,1) * calibum * 1e-6;
@@ -21,11 +24,14 @@ m = struct2table(m);
 
 return
 
-function outs = mylinfit(x,y,order)
+function outs = get_startpos(x,y)
+    outs{1,1} = x(1);
+    outs{1,2} = y(1);
+return
 
+function outs = mylinfit(x,y,order)
     mb = polyfit(x,y,order);
     outs{1,1} = x(1);
     outs{1,2} = mean(x);
     outs{1,3} = mb;
-    
 return
