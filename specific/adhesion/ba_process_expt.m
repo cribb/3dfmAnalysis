3a77d24d01d7f1209479e324bab4d6fb1df7185b
Jeremy Cribb <cribb@cs.unc.edu>
2019-08-16 14:59:58 -0400

    Updated to better use evolved metadata structure.

--- specific/adhesion/ba_process_expt.m
+++ specific/adhesion/ba_process_expt.m
@@ -1,32 +1,34 @@
 function outs = ba_process_expt(filepath)
+% This function begins the process of analyzing the output of the bead
+% adhesion experiment where the bead detaches from the surface and moves
+% through z while being tracked by vst. The z-velocity is then used to back
+% out the detachment force
 
 rootdir = pwd;
 
 cd(filepath);
-
-calibum = 0.346;
-% visc_Pas = 0.010;
-bead_diameter_um = 24;
 
 search_radius_low = 10;
 search_radius_high = 26;
 
 evtfilelist = dir('*.vrpn.evt.mat');
 
-m = cell(length(evtfilelist),1);
-b = cell(length(evtfilelist),1);
-
 for k = 1:length(evtfilelist)
 
    basename = strrep(evtfilelist(k).name, '.vrpn.evt.mat', '');
    
-   % load data from metadata files (ultimately use this as indexing file)
+   % Load data from metadata file. Ultimately, use this as indexing file 
+   % when combining data for an entire experiment
    metadata = load([basename '.meta.mat']);
    
    Fid = metadata.File.Fid;
    visc_Pas = metadata.Medium.Viscosity;
-   firstframe = imread([basename '.00001.pgm']);
-   lastframe = imread([basename '.07625.pgm']);
+   calibum = metadata.Scope.Calibum;   
+   bead_diameter_um = metadata.Bead.Diameter;   
+   Ztable = metadata.Results.TimeHeightVidStatsTable;
+   Ztable.Time = (Ztable.Time - Ztable.Time(1));
+   firstframe = metadata.Results.FirstFrame;
+   lastframe = metadata.Results.LastFrame;
    
   
    FileTable{k} = shorten_metadata(metadata);
@@ -36,14 +38,14 @@ for k = 1:length(evtfilelist)
    origtracks = load_video_tracking([basename '.csv']);
    FileTable{k}.FirstCount = length(unique(origtracks.id(origtracks.frame == 1)));
    
-   BeadInfoTable{k} = ba_discoverbeads(firstframe, lastframe, search_radius_low, search_radius_high, Fid);
-   
+   BeadInfoTable{k} = ba_discoverbeads(firstframe, lastframe, search_radius_low, search_radius_high, Fid);   
       
    ForceTable{k} = get_evt_linfits(evtfilelist(k).name, calibum, visc_Pas, bead_diameter_um, Fid);
+   ForceTable{k}.ZmotorPos = interp1(Ztable.Time, Ztable.ZHeight, ForceTable{k}.Mean_time);
 end
 
 ForceTable = vertcat(ForceTable{:});
-ForceTable.Filename = [];
+% ForceTable.Filename = [];
 
 FileTable = vertcat(FileTable{:});
 BeadInfoTable = vertcat(BeadInfoTable{:});
@@ -57,25 +59,14 @@ T = join(T, NStartingBeadsT);
 
 [g, grpT] = findgroups(T(:,{'BeadChemistry', 'Media'}));
 
-% T = splitapply(@(x1,x2)sortrows(x1,x2,'descending'), T, T.Force, g);
-
 fracleft = splitapply(@(x1,x2){sa_fracleft(x1,x2)},T.Force,T.NStartingBeads,g);
 fracleft = cell2mat(fracleft);
 ForceTable.FractionLeft = fracleft;
 T.FractionLeft = fracleft;
 
-figure;
-gscatter(ForceTable.Force*1e9, ForceTable.FractionLeft*100, g);
-set(gca, 'XScale', 'log');
-xlabel('Force, F [nN]');
-ylabel('Percent Left [%]');
 
-% figure;
-% semilogx(ForceTable.Force*1e9, ForceTable.FractionLeft*100, '.');
-% xlabel('Force, F [N]');
-% ylabel('Percent Left [%]');
-% cd(rootdir);
-
+cd(rootdir);
+ 
 outs.FileTable = FileTable;
 outs.ForceTable = ForceTable;
 outs.BeadInfoTable = BeadInfoTable;
@@ -85,6 +76,7 @@ end
 
 function sm = shorten_metadata(metadata)
     sm.Fid = metadata.File.Fid;
+    sm.FullFilename = string(fullfile(metadata.File.Binpath, metadata.File.Binfile));
     sm.StartTime = metadata.Results.TimeHeightVidStatsTable.Time(1);
     sm.MeanFps  = 1 ./ mean(diff(metadata.Results.TimeHeightVidStatsTable.Time*86400));
     sm.ExposureTime = metadata.Video.ExposureTime;
@@ -95,6 +87,10 @@ function sm = shorten_metadata(metadata)
     sm.Media = string(metadata.Medium.Name);
     sm.MediumViscosity = metadata.Medium.Viscosity;
     sm.Calibum = metadata.Scope.Calibum;    
+    
+    if isfield(metadata.Substrate, 'LotNumber')
+        sm.SubstrateLotNumber = string(metadata.Substrate.LotNumber);
+    end
     
     sm = struct2table(sm);
     
@@ -115,3 +111,7 @@ function outs = sa_fracleft(force, startCount)
     outs = 1-(FRank ./ startCount);
 
 end
+
+
+
+
