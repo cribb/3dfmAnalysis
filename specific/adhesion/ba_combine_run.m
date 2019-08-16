2865cac636f7125e46044d06243a3cd68bcbf8f7
Jeremy Cribb <cribb@cs.unc.edu>
2019-08-16 14:57:20 -0400

    New function that can combine two datasets. Should extend to merging multiple datasets at once. Is not forgiving when it comes to variabilities in metadata structure.

---
+++ specific/adhesion/ba_combine_run.m
@@ -0,0 +1,11 @@
+function DataOut = ba_combine_run(DataInA, DataInB)
+
+repeatFID = intersect(DataInA.FileTable.Fid, DataInB.FileTable.Fid);
+
+if isempty(repeatFID)
+    DataOut.FileTable = [DataInA.FileTable; DataInB.FileTable];
+    DataOut.ForceTable = [DataInA.ForceTable; DataInB.ForceTable];
+    DataOut.BeadInfoTable = [DataInA.BeadInfoTable; DataInB.BeadInfoTable];
+else
+    error('Repeat FID. Likely the data are already combined. Data cannot be further combined.');
+end
