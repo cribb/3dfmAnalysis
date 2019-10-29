3b62909452f3d53e69535004934d168868c6c440
Jeremy Cribb <cribb@cs.unc.edu>
2019-10-29 14:46:52 -0400

    Forked adhesion assay code to new project. Stripping it out of 3dfmAnalysis now.

--- specific/adhesion/analysis/ba_normbymax.m
+++
@@ -1,6 +0,0 @@
-function outs = ba_normbymax(ins)
-% BA_NORMBYMAX normalizes input such that the maximum value is one.
-%
-
-outs = ins(:) ./ max(ins(:));
-end
--- specific/adhesion/hw/zmotor/ba_thorgui.m
+++
@@ -1,7 +0,0 @@
-function h = ba_thorgui
-% BA_THORGUI initializes the Thorlabs z-motor and opens the ThorLabs UI.
-%
-
-    h = ba_initz(true);
-
-    
