7924600243a192edef3a8ca3254b51eb3823c017
Jeremy Cribb <jcribb@email.unc.edu>
2020-06-17 16:07:58 -0400

    Trying to make the generic logentry function work for all of the function calls. It's still not right when it comes to UI calls.

--- utils/logentry.m
+++ utils/logentry.m
@@ -1,16 +1,21 @@
 function logentry(txt)
 % function for writing out stderr log messages
 
-    [ST,I] = dbstack;
-    logtime = clock;
+logtime = clock;
+    
+    % dbstack pulls the stack of function calls that got us here
+    [stk,I] = dbstack;
+    
+    % This function is stk(1).name and its calling function is stk(2).name
+    calling_func = stk(2).name;
+    
     logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
                    num2str(logtime(2),        '%02i') '.' ...
                    num2str(logtime(3),        '%02i') ', ' ...
                    num2str(logtime(4),        '%02i') ':' ...
                    num2str(logtime(5),        '%02i') ':' ...
-                   num2str(floor(logtime(6)), '%02i') ') '];
-     headertext = [logtimetext ST(end).name ': '];
+                   num2str(round(logtime(6)), '%02i') ') '];
+     headertext = [logtimetext calling_func ': '];
      
      fprintf('%s%s\n', headertext, txt);
-     
-     return;    
+    
