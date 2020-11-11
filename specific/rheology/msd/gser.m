47f32fef9362829429947a8e8c5c600cd3ddb4fc
Jeremy Cribb <jcribb@email.unc.edu>
2020-11-11 15:04:19 -0500

    Finished pulling the gser functionality to its own file.

--- specific/rheology/msd/gser.m
+++ specific/rheology/msd/gser.m
@@ -1,40 +1,37 @@
-function v = gser(tau, msd, Ntrackers, bead_radius)
-    k = 1.3806e-23;
+function v = gser(tau, msd, bead_radius)
+% GSER computes complex modulus and viscosity from Generalized Stokes-Einstein Relation
+%
+
+    kb = 1.3806e-23;
     T = 298;
-
-%     A = tau(1:end-1,:);
-%     B = tau(2:end,:);
-%     C = msd(1:end-1,:);
-%     D = msd(2:end,:);
-
-%     alpha = log10(D./C)./log10(B./A);
-
-    
-    % trim out NaNs
-    mymsd = msd( ~isnan(tau));
-    mytau = tau( ~isnan(tau));
-
     timeblur_decade_fraction = .3;
-    [myalpha, tau_evenspace, msd_evenspace] = getMSDSlope(mymsd, mytau, timeblur_decade_fraction);
 
-    alpha = NaN(size(msd));
-    alpha(1:length(myalpha)) = myalpha;
+    % trim out NaNs   
+    tau = tau( ~isnan(tau));
+    msd = msd( ~isnan(tau), :);
     
+    logtau = log10(tau);
+    logmsd = log10(msd);
+
+    % preseed alpha matrix
+    alpha = NaN(size(msd));
+    MYgamma = NaN(size(msd));
+    
+    for k = 1:size(msd,2)
+
+        [myalpha, ~, ~] = getMSDSlope(logtau, logmsd(:,k), timeblur_decade_fraction);
+
+        alpha(1:length(myalpha),k) = myalpha;
+
+    end
+
     MYgamma = gamma(1 + abs(alpha));
-    % gamma = 0.457*(1+alpha).^2-1.36*(1+alpha)+1.9;
-
-    % because of the first-difference equation used to compute alpha, we have
-    % to delete the last row of f, tau, and msd values computed.
-    % msd = msd(1:end-1,:);
-    % tau = tau(1:end-1,:);
-    % Ntrackers = Ntrackers(1:end-1,:);
-
     % get frequencies all worked out from timing (tau)
     f = 1 ./ tau;
     w = 2*pi*f;
 
     % compute shear and viscosity
-    gstar = (2/3) * (k*T) ./ (pi * bead_radius .* msd .* MYgamma);
+    gstar = (2/3) * (kb*T) ./ (pi * bead_radius .* msd .* MYgamma);
     gp = gstar .* cos(pi/2 .* alpha);
     gpp= gstar .* sin(pi/2 .* alpha);
     nstar = gstar .* tau;
