23e58f2c52d9d7b11b5cacc498be340b95a2b764
Jeremy Cribb <jcribb@email.unc.edu>
2020-11-11 15:05:34 -0500

    Updated plots. Added documentation text. Improved some variable names.

--- specific/rheology/msd/getMSDSlope.m
+++ specific/rheology/msd/getMSDSlope.m
@@ -1,28 +1,22 @@
 function [alpha, tau_evenspace, msd_evenspace] = ...
-                            getMSDSlope (msd, tau, timeblur_decade_fraction)
-
-% % deal with NaN inputs
-% if sum(isnan(msd)) == length(msd) || sum(isnan(tau)) == length(tau)
-%     alpha = NaN(size(tau));
-%     tau_evenspace = NaN;
-%     msd_evenspace = NaN;
-%     logentry('No data on which to compute the MSD Slope. Exiting now with NaN');
-%     return;
-% end
-
-%Get into log space
-tau_exponents = log10(tau(:, 1));
-msd_exponents = log10(msd);
+                            getMSDSlope(logtau, logmsd, timeblur_decade_fraction, debugPlotTF)
+% GETMSDSLOPE calculates slope over a smoothed window for msd curves already in log space
+%
+%
+                        
+if nargin < 4 || isempty(debugPlotTF)
+    debugPlotTF = false;
+end
 
 
 %Find ranges of tau, prepare for interpolation
-min_tau         = min(tau_exponents);
-tau_interp_step = min(diff(tau_exponents))/2;
-max_tau         = max(tau_exponents);
+min_tau         = min(logtau);
+tau_interp_step = min(diff(logtau))/2;
+max_tau         = max(logtau);
 tau_evenspace   = (min_tau : tau_interp_step : max_tau)';
 
 % handle case where empty dataset is sent
-if isempty(tau) || (isnan(min_tau) && isnan(max_tau))
+if isempty(logtau) || (isnan(min_tau) && isnan(max_tau))
     alpha = NaN;
     tau_evenspace = NaN;
     msd_evenspace = NaN;
@@ -31,15 +25,15 @@ end
 
 %Interpolate MSD to get evenly spaced MSD data
 warning('OFF', 'MATLAB:interp1:NaNinY');
-msd_evenspace = interp1(tau_exponents,...
-                        msd_exponents,...
+msd_evenspace = interp1(logtau,...
+                        logmsd,...
                         tau_evenspace ...
                         );
 
 %Create some important constants
 derivative_blur = timeblur_decade_fraction / tau_interp_step;
 [m_interp,n_interp] = size(msd_evenspace);
-[m, n] = size(msd);
+[m, n] = size(logmsd);
 
 %Take derivative of each bead's MSD (loop over beads)
 alpha_evenspace = nan(m_interp,n_interp); %initialize alpha matrix
@@ -49,7 +43,7 @@ for i = 1:n_interp
 end
 
 % Map indicies back to the original (uninterpolated) taus
-tau_matrix = repmat(tau_exponents', [m_interp 1]);
+tau_matrix = repmat(logtau', [m_interp 1]);
 tau_evenspace_matrix = repmat(tau_evenspace, [1 m]);
 diffmat = abs(tau_matrix - tau_evenspace_matrix);
 C = min(diffmat, [], 2);
@@ -59,40 +53,39 @@ bar = sortrows(foo, 2);
 tau_map_indicies = sortrows(bar(1:m)');
 
 %Populate alpha matrix
-alpha = msd * NaN; %Initialize
+alpha = NaN(size(logmsd)); %Initialize
 alpha(:,:) = alpha_evenspace(tau_map_indicies,:);
 
 alpha_evenspace(tau_evenspace < min(tau_evenspace)+3*timeblur_decade_fraction,:) = NaN;
 
-if(0)
-    figure;
-    hold on
+if debugPlotTF
+    h = figure;
+    subplot(1,3,1);
     plot(tau_evenspace, msd_evenspace(:, 1:4:end), '.',...
-         tau_exponents, msd_exponents(:, 1:4:end), '-');
+         logtau, logmsd(:, 1:4:end), '-');
     xlabel('log(tau)');
     ylabel('log(msd)');
     pretty_plot;
-    hold off
     
-    figure;
-    hold on
+    figure(h);
+    subplot(1,3,2);
     plot(10.^tau_evenspace, alpha_evenspace, '-b');
     plot(tau, alpha, '.r');
     xlabel('tau');
     ylabel('alpha');
     set(gca, 'YLim', [0 1.5], 'XScale', 'Log');
-    hold off
+    pretty_plot;
 
-    %Plot MSDs colored by slope
+    % Plot MSDs colored by slope
     idx1 = find(alpha < 0.05 & alpha > -0.05);
     idx2 = find(alpha < 1.05 & alpha >  0.95);
-    figure;
-    hold on
+    figure(h);
+    subplot(1,3,3);
     plot(log10(tau(idx1)), log10(msd(idx1)), '.b');
     plot(log10(tau(idx2)), log10(msd(idx2)), '.r');
     xlabel('tau (s)');
-    ylabel('msd')
-    hold off
+    ylabel('msd');
+    pretty_plot;
 end
 
 return;
