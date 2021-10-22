eefaf0e8450db5a6a16b3c3bb6840fc5901f344d
Jeremy Cribb <cribb@cs.unc.edu>
2021-10-22 12:28:13 -0400

    Updated for 'omitnan' argument in later versions of matlab.

--- math/stderr.m
+++ math/stderr.m
@@ -1,4 +1,10 @@
-function v = stderr(X, flag, dim)
+function v = stderr(X, flag, dim, nanflag)
+
+if nargin < 4 || isempty(nanflag)
+    nanflag = 'includenan';
+else
+    nanflag = 'omitnan';
+end
 
 if nargin < 3 || isempty(dim)
     dim = 1;
@@ -13,7 +19,16 @@ if (length(size(X)) == 2)  && (size(X,1) == 1)
     X = X(:);
 end
 
-    v = std(X, flag, dim) ./ sqrt(size(X,dim));
+%     v = std(X, flag, dim, nanflag) ./ sqrt(size(X,dim));
 
+    sd = std(X, flag, dim, nanflag);
+    
+    if contains(nanflag,'includenan')
+        sz = size(X,dim);
+    else
+        sz = sum(~isnan(X),dim, nanflag);
+    end
+    
+    v = sd ./ sqrt(sz);
 return;
 
