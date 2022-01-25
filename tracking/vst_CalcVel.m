cc37ee2e6ced59f98c07a26377227e0fe1dc1521
Jeremy Cribb <jcribb@email.unc.edu>
2022-01-25 16:26:46 -0500

    Added Fid to the velocity calc in an effort to expand beyond one dataset at a time.

--- tracking/vst_CalcVel.m
+++ tracking/vst_CalcVel.m
@@ -1,9 +1,9 @@
-function outs = vst_CalcVel(id, frame, xyz, sfactor)    
+function outs = vst_CalcVel(fid, id, frame, xyz, sfactor)    
     if isempty(frame)
         outs = zeros(0,3);
         return
     end
     
     dxyz = CreateGaussScaleSpace(xyz,1,sfactor);
-    outs = [id, frame, dxyz];
+    outs = [fid, id, frame, dxyz];
 return
